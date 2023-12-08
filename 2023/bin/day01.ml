ae3f7d401140f4a660c4585cbfa39f145b336dc7
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 01:47:39 +0300

    bin/day01: ocamlformat

--- 2023/bin/day01.ml
+++ 2023/bin/day01.ml
@@ -4,15 +4,15 @@ let ( let* ) = Option.bind
 
 let rec next_digit = function
   | [] -> (None, [])
-  | '1' :: s | 'o' :: (('n' :: 'e' :: _) as s) -> (Some 1, s)
-  | '2' :: s | 't' :: (('w' :: 'o' :: _) as s) -> (Some 2, s)
-  | '3' :: s | 't' :: (('h' :: 'r' :: 'e' :: 'e' :: _) as s) -> (Some 3, s)
-  | '4' :: s | 'f' :: (('o' :: 'u' :: 'r' :: _) as s) -> (Some 4, s)
-  | '5' :: s | 'f' :: (('i' :: 'v' :: 'e' :: _) as s) -> (Some 5, s)
-  | '6' :: s | 's' :: (('i' :: 'x' :: _) as s) -> (Some 6, s)
-  | '7' :: s | 's' :: (('e' :: 'v' :: 'e' :: 'n' :: _) as s) -> (Some 7, s)
-  | '8' :: s | 'e' :: (('i' :: 'g' :: 'h' :: 't' :: _) as s) -> (Some 8, s)
-  | '9' :: s | 'n' :: (('i' :: 'n' :: 'e' :: _) as s) -> (Some 9, s)
+  | '1' :: s | 'o' :: ('n' :: 'e' :: _ as s) -> (Some 1, s)
+  | '2' :: s | 't' :: ('w' :: 'o' :: _ as s) -> (Some 2, s)
+  | '3' :: s | 't' :: ('h' :: 'r' :: 'e' :: 'e' :: _ as s) -> (Some 3, s)
+  | '4' :: s | 'f' :: ('o' :: 'u' :: 'r' :: _ as s) -> (Some 4, s)
+  | '5' :: s | 'f' :: ('i' :: 'v' :: 'e' :: _ as s) -> (Some 5, s)
+  | '6' :: s | 's' :: ('i' :: 'x' :: _ as s) -> (Some 6, s)
+  | '7' :: s | 's' :: ('e' :: 'v' :: 'e' :: 'n' :: _ as s) -> (Some 7, s)
+  | '8' :: s | 'e' :: ('i' :: 'g' :: 'h' :: 't' :: _ as s) -> (Some 8, s)
+  | '9' :: s | 'n' :: ('i' :: 'n' :: 'e' :: _ as s) -> (Some 9, s)
   | _ :: s -> next_digit s
 
 let rec digits chars =

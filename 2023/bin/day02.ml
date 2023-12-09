9087952fb3fde0af2d435bf2a33565ec9fccb49d
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 12:34:05 +0300

    bin/day02: part2 function

--- 2023/bin/day02.ml
+++ 2023/bin/day02.ml
@@ -66,4 +66,12 @@ let part1 () =
   in
   print_endline (string_of_int sum)
 
+let part2 () =
+  let sum =
+    input_lines stdin |> Seq.map Game.of_string
+    |> Seq.map (fun Game.{ draws; _ } -> List.reduce Draw.max_elts draws)
+    |> Seq.map Draw.power |> Seq.reduce ( + )
+  in
+  print_endline (string_of_int sum)
+
 let () = part1 ()

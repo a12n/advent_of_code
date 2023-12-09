3d989ccd993ae04f3221814ab635d6fbe69a02db
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 18:33:00 +0300

    bin/day04: part1 function

--- 2023/bin/day04.ml
+++ 2023/bin/day04.ml
@@ -1,5 +1,50 @@
 open Advent
 
-let part1 () = ()
+module Numbers : sig
+  include module type of Set.Make (Int)
+
+  val of_string : string -> t
+end = struct
+  include Set.Make (Int)
+
+  let of_string s =
+    String.split_on_char ' ' s
+    |> List.filter (( <> ) "")
+    |> List.map int_of_string |> of_list
+end
+
+module Card : sig
+  type t = int * Numbers.t * Numbers.t
+
+  val of_string : string -> t
+end = struct
+  type t = int * Numbers.t * Numbers.t
+
+  let of_string s =
+    match String.split_on_char ':' s with
+    | [ header; numbers ] -> (
+        match String.split_on_char ' ' header |> List.filter (( <> ) "") with
+        | [ "Card"; id ] -> (
+            match String.split_on_char '|' numbers with
+            | [ winning; have ] ->
+                ( int_of_string id,
+                  Numbers.of_string winning,
+                  Numbers.of_string have )
+            | _ -> invalid_arg __FUNCTION__)
+        | _ -> invalid_arg __FUNCTION__)
+    | _ -> invalid_arg __FUNCTION__
+end
+
+let part1 () =
+  let sum =
+    input_lines stdin |> Seq.map Card.of_string
+    |> Seq.map (fun (_, winning, have) ->
+           match Numbers.(cardinal (inter winning have)) with
+           | 0 -> 0
+           | n -> 1 lsl (n - 1))
+    |> Seq.reduce ( + )
+  in
+  print_endline (string_of_int sum)
+
 let part2 () = ()
 let () = (parse_args Sys.argv [| part1; part2 |]) ()

682a1a7f79df867ef1a66e25a0b126c9e82660f7
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 23:45:56 +0300

    bin/day04: part2

--- 2023/bin/day04.ml
+++ 2023/bin/day04.ml
@@ -43,5 +43,21 @@ let part1 () =
   in
   print_endline (string_of_int sum)
 
-let part2 () = ()
+let part2 () =
+  let matching =
+    input_lines stdin |> Seq.map Card.of_string
+    |> Seq.mapi (fun i (id, matching) ->
+           assert (i = id - 1);
+           Numbers.cardinal matching)
+    |> Array.of_seq
+  in
+  let n = Array.length matching in
+  let copies = Array.make n 1 in
+  for i = 0 to n - 1 do
+    for j = 1 to matching.(i) do
+      copies.(i + j) <- copies.(i + j) + copies.(i)
+    done
+  done;
+  print_endline (string_of_int (Array.reduce ( + ) copies))
+
 let () = (parse_args Sys.argv [| part1; part2 |]) ()

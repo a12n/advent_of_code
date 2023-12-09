c6b61083bb9c0784e63cb4b2b6d8953c97e4ef44
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 13:07:14 +0300

    lib/advent: add input_chars function

--- 2023/lib/advent.ml
+++ 2023/lib/advent.ml
@@ -2,6 +2,9 @@ module List = Advent__List
 module Seq = Advent__Seq
 module String = Advent__String
 
-let input_lines ch =
-  Seq.of_dispenser (fun () ->
-      match input_line ch with l -> Some l | exception End_of_file -> None)
+let input_chars, input_lines =
+  let input_seq read ch =
+    Seq.of_dispenser (fun () ->
+        match read ch with x -> Some x | exception End_of_file -> None)
+  in
+  (input_seq input_char, input_seq input_line)

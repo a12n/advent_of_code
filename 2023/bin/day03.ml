2d8d660cb2b8d566862a41ddc52b867cecfc9f5a
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 15:24:49 +0300

    bin/day03: part2 function

--- 2023/bin/day03.ml
+++ 2023/bin/day03.ml
@@ -116,4 +116,20 @@ let part1 () =
   in
   print_endline (string_of_int sum)
 
+let part2 () =
+  let State.{ parts; symbols; _ } =
+    input_chars stdin
+    |> Seq.fold_left State.update State.initial
+    |> State.finish
+  in
+  let sum =
+    List.filter (fun (gear, _) -> gear) symbols
+    |> List.filter_map (fun sym ->
+           match List.filter ((Fun.flip Part.is_adjacent) sym) parts with
+           | Part.[ { num = a; _ }; { num = b; _ } ] -> Some (a * b)
+           | _ -> None)
+    |> List.reduce ( + )
+  in
+  print_endline (string_of_int sum)
+
 let () = part1 ()

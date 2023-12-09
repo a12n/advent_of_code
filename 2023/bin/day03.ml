546047dfacd93491e85e13dab02269baa7585664
Anton Yabchinskiy <arn@bestmx.net>
2023-12-09 14:54:37 +0300

    bin/day03: part1 function

--- 2023/bin/day03.ml
+++ 2023/bin/day03.ml
@@ -93,16 +93,21 @@ module State = struct
         }
 end
 
-let () =
+let part1 () =
   let State.{ parts; symbols; _ } =
     input_chars stdin
     |> Seq.fold_left State.update State.initial
     |> State.finish
   in
-  List.iter prerr_endline (List.map Part.to_string parts);
-  List.iter prerr_endline (List.map Symbol.to_string symbols);
-  let parts =
-    List.filter (fun part -> List.exists (Part.is_adjacent part) symbols) parts
+  (* List.iter prerr_endline (List.map Part.to_string parts); *)
+  (* List.iter prerr_endline (List.map Symbol.to_string symbols); *)
+  let sum =
+    List.filter
+      (fun part -> List.exists (Part.is_adjacent part) symbols)
+      parts (* All parts adjacent to at least one symbol. *)
+    |> List.map (fun Part.{ num; _ } -> num) (* Extract the part numbers. *)
+    |> List.reduce ( + ) (* Sum the part numbers. *)
   in
-  let sum = List.map (fun Part.{ num; _ } -> num) parts |> List.reduce ( + ) in
   print_endline (string_of_int sum)
+
+let () = part1 ()

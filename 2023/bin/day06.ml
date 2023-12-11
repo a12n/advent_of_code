d7e57b30465a8c46386f0ed2bd7eedbcfe6dd979
Anton Yabchinskiy <arn@bestmx.net>
2023-12-11 17:21:56 +0400

    bin/day06: fix parsing

--- 2023/bin/day06.ml
+++ 2023/bin/day06.ml
@@ -5,17 +5,34 @@ let distance race_time hold_time =
   assert (hold_time >= 0 && hold_time <= race_time);
   (race_time - hold_time) * hold_time
 
-let part1 () =
-  let records =
-    let int_list s =
-      String.split_on_char ' ' s
-      |> List.filter (( <> ) "")
-      |> List.map int_of_string
-    in
-    let time = Scanf.scanf "Time: %[^\n]" int_list in
-    let distance = Scanf.scanf "Distance: %[^\n]" int_list in
-    List.combine time distance
+let int_list_of_string s =
+  String.split_on_char ' ' s
+  |> List.filter (( <> ) "")
+  |> List.map int_of_string
+
+let int_of_string' s =
+  String.trim s |> String.map (function ' ' -> '_' | c -> c) |> int_of_string
+
+let parse_input f ch =
+  let pair s =
+    match String.split_on_char ':' s with
+    | [ key; value ] -> String.(trim key, trim value)
+    | _ -> invalid_arg __FUNCTION__
   in
+  let time =
+    match pair (input_line ch) with
+    | "Time", numbers -> f numbers
+    | _ -> invalid_arg __FUNCTION__
+  in
+  let distance =
+    match pair (input_line ch) with
+    | "Distance", numbers -> f numbers
+    | _ -> invalid_arg __FUNCTION__
+  in
+  (time, distance)
+
+let part1 () =
+  let times, dists = parse_input int_list_of_string stdin in
   let product =
     List.map
       (fun (race_time, record_dist) ->
@@ -24,11 +41,13 @@ let part1 () =
         |> Seq.map (distance race_time)
         |> Seq.filter (fun dist -> dist > record_dist)
         |> Seq.length)
-      records
+      (List.combine times dists)
     |> List.reduce ( * )
   in
   print_endline (string_of_int product)
 
-let part2 () = ()
+let part2 () =
+  let time, dist = parse_input int_of_string' stdin in
+  Printf.printf "time %d, dist %d\n" time dist
 
 let () = (parse_args Sys.argv [| part1; part2 |]) ()

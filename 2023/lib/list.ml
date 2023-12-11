5786401356fb8fcdcac4e4542bcd8b51ceab8b8c
Anton Yabchinskiy <arn@bestmx.net>
2023-12-11 16:03:39 +0400

    lib/list: add pairs function

--- 2023/lib/list.ml
+++ 2023/lib/list.ml
@@ -1,5 +1,10 @@
 include Stdlib.List
 
+let rec pairs = function
+  | [] -> []
+  | x0 :: x1 :: xs -> (x0, x1) :: pairs xs
+  | _ -> invalid_arg __FUNCTION__
+
 let reduce f = function
   | x0 :: xs -> fold_left f x0 xs
   | [] -> invalid_arg __FUNCTION__
--- 2023/lib/list.mli
+++ 2023/lib/list.mli
@@ -1,3 +1,4 @@
 include module type of Stdlib.List
 
+val pairs : 'a list -> ('a * 'a) list
 val reduce : ('a -> 'a -> 'a) -> 'a list -> 'a

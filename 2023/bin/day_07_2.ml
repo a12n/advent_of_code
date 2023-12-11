3fed049587f0dd7444acc13d35d8e249fc421a89
Anton Yabchinskiy <arn@bestmx.net>
2023-12-12 01:31:03 +0300

    bin/day_07_2: the same as part 1 but with joker flag

--- 2023/bin/day_07_2.ml
+++ 2023/bin/day_07_2.ml
@@ -1,1 +1,16 @@
-let () = print_endline (string_of_int 0)
+open Advent
+open Day_07
+
+let () =
+  let joker = true in
+  let total_winnings =
+    List.of_seq (input stdin)
+    |> List.map (fun (hand, bid) ->
+           (Hand.Freq.to_kind ~joker (Hand.to_freq ~joker hand), hand, bid))
+    |> List.stable_sort (fun (kind1, hand1, _) (kind2, hand2, _) ->
+           let= _ = compare kind1 kind2 in
+           Hand.compare ~joker hand1 hand2)
+    |> List.mapi (fun i (_, _, bid) -> (i + 1) * bid)
+    |> List.reduce ( + )
+  in
+  print_endline (string_of_int total_winnings)

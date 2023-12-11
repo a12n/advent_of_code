9e5fa1317f23707acb76f28f8da29f58581d112f
Anton Yabchinskiy <arn@bestmx.net>
2023-12-11 18:32:32 +0400

    bin/day_07: Card.to_char, Hand module

--- 2023/bin/day_07.ml
+++ 2023/bin/day_07.ml
@@ -5,7 +5,7 @@ module Card : sig
 
   val of_char : char -> t
   val pp : Format.formatter -> t -> unit [@@ocaml.toplevel_printer]
-  val to_string : t -> string
+  val to_char : t -> char
 end = struct
   type t = int
 
@@ -25,13 +25,36 @@ end = struct
     | 'A' | 'a' -> 14
     | _ -> invalid_arg __FUNCTION__
 
-  let to_string c =
-    Array.(
-      unsafe_get
-        [| "2"; "3"; "4"; "5"; "6"; "7"; "8"; "9"; "10"; "J"; "Q"; "K"; "A" |]
-        (c - 2))
+  let to_char c = String.unsafe_get "23456789TJQKA" (c - 2)
+  let pp fmt c = Format.pp_print_char fmt (to_char c)
+end
 
-  let pp fmt c = Format.pp_print_string fmt (to_string c)
+module Hand : sig
+  type t = Card.t * Card.t * Card.t * Card.t * Card.t
+
+  val of_string : string -> t
+  val to_string : t -> string
+end = struct
+  type t = Card.t * Card.t * Card.t * Card.t * Card.t
+
+  let of_string s =
+    let open String in
+    if length s <> 5 then invalid_arg __FUNCTION__;
+    ( Card.of_char (unsafe_get s 0),
+      Card.of_char (unsafe_get s 1),
+      Card.of_char (unsafe_get s 2),
+      Card.of_char (unsafe_get s 3),
+      Card.of_char (unsafe_get s 4) )
+
+  let to_string (c0, c1, c2, c3, c4) =
+    let open Bytes in
+    let buf = create 5 in
+    unsafe_set buf 0 (Card.to_char c0);
+    unsafe_set buf 1 (Card.to_char c1);
+    unsafe_set buf 2 (Card.to_char c2);
+    unsafe_set buf 3 (Card.to_char c3);
+    unsafe_set buf 4 (Card.to_char c4);
+    unsafe_to_string buf
 end
 
 let part1 () = print_endline (string_of_int 0)

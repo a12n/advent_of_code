831a35fd4aed063c84b0ee42fc8f5840c12381a0
Anton Yabchinskiy <arn@bestmx.net>
2023-12-11 23:33:10 +0300

    bin/day_07_1: initial solution

--- 2023/bin/day_07_1.ml
+++ 2023/bin/day_07_1.ml
@@ -1,3 +1,14 @@
+open Advent
+open Day_07
+
 let () =
-  let _ = Day_07.input stdin in
-  print_endline (string_of_int 0)
+  let total_winnings =
+    List.of_seq (input stdin)
+    |> List.map (fun (hand, bid) ->
+           (* XXX: Don't sort by bid. *)
+           (Hand.Freq.to_kind (Hand.to_freq hand), hand, bid))
+    |> List.stable_sort compare
+    |> List.mapi (fun i (_, _, bid) -> (i + 1) * bid)
+    |> List.reduce ( + )
+  in
+  print_endline (string_of_int total_winnings)

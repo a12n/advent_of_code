4d3e4c21421d84df000885e118c40c8681c57375
Anton Yabchinskiy <arn@bestmx.net>
2023-12-12 18:20:43 +0400

    bin/day_09_1: missing List.rev

--- 2023/bin/day_09_1.ml
+++ 2023/bin/day_09_1.ml
@@ -12,6 +12,6 @@ let () =
     |> Seq.map (String.split_on_char ' ')
     |> Seq.map (List.filter (( <> ) ""))
     |> Seq.map (List.map int_of_string)
-    |> Seq.map rev_extrapolate |> Seq.reduce ( + )
+    |> Seq.map List.rev |> Seq.map rev_extrapolate |> Seq.reduce ( + )
   in
   print_endline (string_of_int sum)

261813b2d7efc07943b82b7523509cfbfd5abb0d
Anton Yabchinskiy <arn@bestmx.net>
2023-12-12 12:46:40 +0400

    bin/day_08_1: initial solution

--- 2023/bin/day_08_1.ml
+++ 2023/bin/day_08_1.ml
@@ -1,3 +1,13 @@
+open Day_08
+
 let () =
-  (* TODO *)
-  ()
+  let dirs, network = input stdin in
+  Printf.eprintf "dirs %d\n%!" (Array.length dirs);
+  let dest = ID.of_string "ZZZ" in
+  let rec loop node i =
+    if node <> dest then
+      loop (Network.next network node dirs.(i mod Array.length dirs)) (i + 1)
+    else i
+  in
+  let num_steps = loop (ID.of_string "AAA") 0 in
+  print_endline (string_of_int num_steps)

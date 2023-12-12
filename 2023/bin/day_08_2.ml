716852338465e483f6650ea3a5c3c4a8dc07a8ce
Anton Yabchinskiy <arn@bestmx.net>
2023-12-12 19:06:59 +0400

    bin/day_08_2: import some WIP code

--- 2023/bin/day_08_2.ml
+++ 2023/bin/day_08_2.ml
@@ -1,3 +1,20 @@
+open Day_08
+
 let () =
-  (* TODO *)
-  ()
+  let dirs, nodes = input stdin in
+  let srcs, network =
+    Seq.fold_left
+      (fun (srcs, network) (from, dest) ->
+         ( (if ID.is_part_source from then from :: srcs else srcs),
+           Network.add network from dest ))
+      ([], Network.make ())
+      nodes
+  in
+  let rec loop node i =
+    if List.for_all ID.is_part_dest node then i
+    else
+      let dir = dirs.(i mod Array.length dirs) in
+      let node' = List.map (Network.next network dir) node in
+      loop node' (i + 1)
+  in
+  print_endline (string_of_int (loop srcs 0))

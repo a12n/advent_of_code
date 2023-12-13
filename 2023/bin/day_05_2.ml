deb4bf5af5fb47135809536e2f8f7a15fae1fdc6
Anton Yabchinskiy <arn@bestmx.net>
2023-12-13 18:42:56 +0400

    bin/day_05_2: initial implementation (WIP)

--- 2023/bin/day_05_2.ml
+++ 2023/bin/day_05_2.ml
@@ -1,3 +1,11 @@
+open Advent
+open Day_05
+
 let () =
-  (* TODO *)
-  ()
+  let (Almanac.{ seeds; _ } as almanac) =
+    Almanac.of_lines (input_lines stdin)
+  in
+  let seeds = List.map (fun (min, n) -> Segment.make min (`Length n)) seeds in
+  let locations = Almanac.seed_to_location_segment almanac seeds in
+  let Segment.{ min = min_location; _ } = List.reduce Stdlib.min locations in
+  print_endline (string_of_int min_location)

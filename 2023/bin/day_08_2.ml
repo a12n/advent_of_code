4fdf31da8219bcc8ab9faa2ed62f2cd1165232c5
Anton Yabchinskiy <arn@bestmx.net>
2023-12-14 16:41:50 +0400

    bin/day_08_2: solution with LCM

--- 2023/bin/day_08_2.ml
+++ 2023/bin/day_08_2.ml
@@ -1,3 +1,4 @@
+open Advent
 open Day_08
 
 let () =
@@ -5,16 +6,20 @@ let () =
   let srcs, network =
     Seq.fold_left
       (fun (srcs, network) (from, dest) ->
-         ( (if ID.is_part_source from then from :: srcs else srcs),
-           Network.add network from dest ))
+        ( (if ID.is_part_source from then from :: srcs else srcs),
+          Network.add network from dest ))
       ([], Network.make ())
       nodes
   in
-  let rec loop node i =
-    if List.for_all ID.is_part_dest node then i
-    else
-      let dir = dirs.(i mod Array.length dirs) in
-      let node' = List.map (Network.next network dir) node in
-      loop node' (i + 1)
-  in
-  print_endline (string_of_int (loop srcs 0))
+  (* XXX: Assume it's a bijection from source nodes to destination
+     nodes. It's the case for the puzzle input, but may not be true in
+     some other case. *)
+  (* For each source node, find the number of steps to a first destination. *)
+  List.map (Network.path network ID.is_part_dest dirs 0) srcs
+  (* Ignore destination identifiers, keep only number of steps. *)
+  |> List.map fst
+  (* Find least common multiple of these number of steps. *)
+  |> List.reduce Int.lcm
+  (* Print the result. *)
+  |> string_of_int
+  |> print_endline

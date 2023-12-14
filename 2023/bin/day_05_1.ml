a5731cb42d99d5ffffddc784dcdb93de4abb6b55
Anton Yabchinskiy <arn@bestmx.net>
2023-12-14 12:52:45 +0400

    bin/day_05_1: rewrite similar to day_05_2, use degenerate intervals for seeds

--- 2023/bin/day_05_1.ml
+++ 2023/bin/day_05_1.ml
@@ -5,9 +5,12 @@ let () =
   let (Almanac.{ seeds; _ } as almanac) =
     Almanac.of_lines (input_lines stdin)
   in
-  let min_location, _seed =
-    List.unpair seeds
-    |> List.map (fun seed -> (Almanac.seed_to_location almanac seed, seed))
-    |> List.fold_left min (max_int, 0)
+  let seeds =
+    List.flatten
+      (List.map
+         (fun (s0, s1) -> Segment.[ make s0 (`Length 0); make s1 (`Length 0) ])
+         seeds)
   in
+  let locations = Almanac.seed_to_location_segment almanac seeds in
+  let Segment.{ min = min_location; _ } = List.reduce Stdlib.min locations in
   print_endline (string_of_int min_location)

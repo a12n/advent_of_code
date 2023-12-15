629c08d85e89d7a98d0647ed33800ff1ff0ca5fa
Anton Yabchinskiy <arn@bestmx.net>
2023-12-15 15:55:42 +0300

    bin/day_11_1: solution

--- 2023/bin/day_11_1.ml
+++ 2023/bin/day_11_1.ml
@@ -2,6 +2,7 @@ open Advent
 open Day_11
 
 let () =
-  let img = Image.of_chars (input_chars stdin) in
-  (* TODO *)
-  List.iter (fun (row, col) -> Printf.printf "%d %d\n" row col) img
+  Image.of_chars (input_chars stdin)
+  |> Image.expand |> List.combine_tl
+  |> List.map (fun (g, gs) -> List.map (Image.dist g) gs)
+  |> List.concat |> List.reduce ( + ) |> string_of_int |> print_endline

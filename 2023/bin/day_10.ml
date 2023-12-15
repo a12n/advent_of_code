d0f29ee0987fcb245809c94fcdeb78e7d7b883c4
Anton Yabchinskiy <arn@bestmx.net>
2023-12-15 12:01:13 +0300

    bin/day_10: work in progress on Grid.cycles

--- 2023/bin/day_10.ml
+++ 2023/bin/day_10.ml
@@ -1,77 +1,94 @@
+open Advent
+
+module Dir = struct
+  type t = North | East | South | West
+
+  let neg = function North -> South | East -> West | South -> North | West -> East
+
+  let add_pos dir (row, column) =
+    match dir with
+    | North -> (row - 1, column)
+    | East -> (row, column + 1)
+    | South -> (row + 1, column)
+    | West -> (row, column - 1)
+end
+
 module Pipe = struct
-  type t = Vert | Horiz | North_East | North_West | South_West | South_East
+  type t = Start | North_south | East_west | North_east | North_west | South_west | South_east
 
   let of_char = function
-    | '|' -> Vert
-    | '-' -> Horiz
-    | 'L' -> North_East
-    | 'J' -> North_West
-    | '7' -> South_West
-    | 'F' -> South_East
+    | 'S' -> Start
+    | '|' -> North_south
+    | '-' -> East_west
+    | 'L' -> North_east
+    | 'J' -> North_west
+    | '7' -> South_west
+    | 'F' -> South_east
     | _ -> invalid_arg __FUNCTION__
+
+  let has_conn pipe dir =
+    match pipe with
+    | Start -> true
+    | North_south -> Dir.(dir = North || dir = South)
+    | East_west -> Dir.(dir = East || dir = West)
+    | North_east -> Dir.(dir = North || dir = East)
+    | North_west -> Dir.(dir = North || dir = West)
+    | South_west -> Dir.(dir = South || dir = West)
+    | South_east -> Dir.(dir = South || dir = East)
 
   let of_char_opt = function '.' -> None | c -> Some (of_char c)
 end
 
 module Grid : sig
-  type t = private { start : int * int; pipes : Pipe.t option array array }
+  type t = Pipe.t option array array
 
-  (* val get : t -> int -> int -> Pipe.t option *)
   val size : t -> int * int
-  val start : t -> int * int
-  val loops : t -> (int * int) list list
+  val start : t -> (int * int) option
+  val get : t -> int * int -> Pipe.t option
   val of_lines : string Seq.t -> t
+  val cycles : t -> (int * int) list list
 end = struct
-  type t = { start : int * int; pipes : Pipe.t option array array }
+  type t = Pipe.t option array array
 
-  let size g = Array.(length g.pipes, length g.pipes.(0))
-  let start g = g.start
+  let size pipes =
+    Array.(match length pipes with 0 -> (0, 0) | n -> (n, length (unsafe_get pipes 0)))
 
-  (* let get g row column = *)
-  (*   if row >= 0 && column >= 0 then *)
-  (*     let n, m = size g in *)
-  (*     if row < n && column < m then g.pipes.(row).(column) else None *)
-  (*   else None *)
+  let start pipes =
+    Array.(
+      find_mapi
+        (fun row pipes ->
+          find_mapi
+            (fun column pipe -> if pipe = Some Pipe.Start then Some (row, column) else None)
+            pipes)
+        pipes)
 
-  let loops g =
-    let n, m = size g in
+  let get pipes (row, column) =
+    if row >= 0 && column >= 0 then
+      let n, m = size pipes in
+      if row < n && column < m then Array.(unsafe_get (unsafe_get pipes row) column) else None
+    else None
+
+  let conns pipes pos =
+    List.filter_map
+      (fun dir ->
+        let*? pipe = get pipes pos in
+        let pos' = Dir.add_pos dir pos in
+        let*? pipe' = get pipes pos' in
+        if Pipe.(has_conn pipe dir && has_conn pipe' (Dir.neg dir)) then Some pos' else None)
+      Dir.[ North; East; South; West ]
+
+  (* TODO *)
+  let cycles pipes =
+    let n, m = size pipes in
+    let start = Option.get (start pipes) in
     let visited = Array.make_matrix n m false in
     let rec dfs path ((row, column) as pos) =
-      if visited.(row).(column) then
-        if (row, column) = g.start then [ pos :: path ] else []
+      if visited.(row).(column) then if List.last path = pos then [ path ] else []
       else (
         visited.(row).(column) <- true;
-        (* All possible directions from this position. *)
-        (match g.pipes.(row).(column) with
-        | None -> []
-        | Some Pipe.Vert -> [ (row - 1, column); (row + 1, column) ]
-        | Some Pipe.Horiz -> [ (row, column - 1); (row, column + 1) ]
-        | Some Pipe.North_East -> [ (row - 1, column); (row, column + 1) ]
-        | Some Pipe.North_West -> [ (row - 1, column); (row, column - 1) ]
-        | Some Pipe.South_West -> [ (row + 1, column); (row, column - 1) ]
-        | Some Pipe.South_East -> [ (row + 1, column); (row, column + 1) ])
-        (* Filter invalid directions. *)
-        |> List.filter (fun (row, column) ->
-               row >= 0 && row < n && column >= 0 && column < m)
-        (* Search all valid directions and combine the results. *)
-        |> List.map (dfs (pos :: path))
-        |> List.concat)
+        List.concat (List.map (dfs (pos :: path)) (conns pipes pos)))
     in
-    dfs [] g.start
+    dfs [] start
 
-  let of_lines lines =
-    let start = ref (-1, -1) in
-    let pipes =
-      lines
-      |> Seq.mapi (fun row line ->
-             String.to_seq line
-             |> Seq.mapi (fun column c ->
-                    if c = 'S' then (
-                      start := (row, column);
-                      None)
-                    else Pipe.of_char_opt c)
-             |> Array.of_seq)
-      |> Array.of_seq
-    in
-    { start = !start; pipes }
+  let of_lines = Array.of_seq % Seq.map (Array.of_seq % Seq.map Pipe.of_char_opt % String.to_seq)
 end

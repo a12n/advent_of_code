432cb9a17338fdf06506efafcd91917854d9e49b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-15 10:36:01 +0300

    lib/advent: move operators to the top of the module

--- 2023/lib/advent.ml
+++ 2023/lib/advent.ml
@@ -4,6 +4,10 @@ module List = Advent__List
 module Seq = Advent__Seq
 module String = Advent__String
 
+(** Binding operator to short-circuit chains of [compare] calls for
+    tuples. *)
+let ( let= ) ord f = if ord <> 0 then ord else f ord
+
 let input_chars, input_lines =
   let input_seq read ch =
     (* XXX: The module Seq is an alias for module Advent__Seq, which
@@ -11,10 +15,6 @@ let input_chars, input_lines =
     Seq.of_dispenser (fun () -> match read ch with x -> Some x | exception End_of_file -> None)
   in
   (input_seq input_char, input_seq input_line)
-
-(** Binding operator to short-circuit chains of [compare] calls for
-    tuples. *)
-let ( let= ) ord f = if ord <> 0 then ord else f ord
 
 (** Choose one of the elements based on the index in args. *)
 let parse_args args elts =

3ab20c4b282d57ec4b307d624b78fa18457786ad
Anton Yabchinskiy <arn@bestmx.net>
2023-12-15 14:05:45 +0300

    bin/day_11: Image.of_chars

---
+++ 2023/bin/day_11.ml
@@ -0,0 +1,16 @@
+open Advent
+
+module Image = struct
+  type t = (int * int) list
+
+  let of_chars =
+    snd
+    % Seq.fold_left
+        (fun ((row, col), image) c ->
+          match c with
+          | '.' -> ((row, col + 1), image)
+          | '\n' -> ((row + 1, 0), image)
+          | '#' -> ((row, col + 1), (row, col) :: image)
+          | _ -> invalid_arg __FUNCTION__)
+        ((0, 0), [])
+end

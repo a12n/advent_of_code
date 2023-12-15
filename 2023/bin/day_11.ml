132f6b216ce3782e0473113859befe5d1e880901
Anton Yabchinskiy <arn@bestmx.net>
2023-12-15 14:43:40 +0300

    bin/day_11: add Image.expand

--- 2023/bin/day_11.ml
+++ 2023/bin/day_11.ml
@@ -3,6 +3,21 @@ open Advent
 module Image = struct
   type t = (int * int) list
 
+  let expand img =
+    let diff dim img =
+      0
+      :: (List.map dim img |> List.diffs ( - )
+         |> List.map (function 0 -> 0 | 1 -> 0 | d -> d - 1)
+         |> List.fold_left_map (fun sum d -> (sum + d, sum + d)) 0
+         |> snd)
+    in
+    let by dim a b = Int.compare (dim a) (dim b) in
+    let img = List.stable_sort (by snd) img in
+    let img = List.map2 (fun (row, col) dx -> (row, col + dx)) img (diff snd img) in
+    let img = List.stable_sort (by fst) img in
+    let img = List.map2 (fun (row, col) dy -> (row + dy, col)) img (diff fst img) in
+    img
+
   let of_chars =
     List.rev % snd
     % Seq.fold_left

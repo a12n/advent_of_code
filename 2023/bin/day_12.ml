213029075e4f9ffbd0a2f1c799583beccb3a5769
Anton Yabchinskiy <arn@bestmx.net>
2023-12-16 00:08:56 +0300

    bin/day_12: add Spring_row type

---
+++ 2023/bin/day_12.ml
@@ -0,0 +1,22 @@
+module Spring_row : sig
+  type t = private bool option list * int list
+
+  val of_string : string -> t
+end = struct
+  type t = bool option list * int list
+
+  let of_string s =
+    match String.split_on_char ' ' s |> List.map String.trim |> List.filter (( <> ) "") with
+    | [ springs; numbers ] ->
+        let springs =
+          List.init (String.length springs) (fun i ->
+              match springs.[i] with
+              | '.' -> Some true
+              | '#' -> Some false
+              | '?' -> None
+              | _ -> invalid_arg __FUNCTION__)
+        in
+        let numbers = String.split_on_char ',' numbers |> List.map int_of_string in
+        (springs, numbers)
+    | _ -> invalid_arg __FUNCTION__
+end

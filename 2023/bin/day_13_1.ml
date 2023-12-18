0582c74ca6ff4faf1f1cf82d8bed1abc03d6820f
Anton Yabchinskiy <arn@bestmx.net>
2023-12-18 18:23:45 +0400

    bin/day_13_1: rename line of simmetry index to k

--- 2023/bin/day_13_1.ml
+++ 2023/bin/day_13_1.ml
@@ -3,5 +3,5 @@ open Advent
 let () =
   Day_13.input stdin
   |> Seq.filter_map Day_13.Grid.reflection
-  |> Seq.map (function `Horiz i -> 100 * i | `Vert i -> i)
+  |> Seq.map (function `Horiz k -> 100 * k | `Vert k -> k)
   |> Seq.reduce ( + ) |> string_of_int |> print_endline

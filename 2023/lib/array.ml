035ec75e59e5efbacc0cc860cdbaabb4c2ccd77f
Anton Yabchinskiy <arn@bestmx.net>
2023-12-19 01:54:39 +0300

    lib/array: add symmetry function

--- 2023/lib/array.ml
+++ 2023/lib/array.ml
@@ -11,20 +11,41 @@ let reduce f a =
   done;
   !r
 
-let symmetric eq a pos len =
-  Int.is_even len
-  &&
-  try
-    for i = 0 to (len / 2) - 1 do
-      let j = len - 1 - i in
-      if not (eq (get a (pos + i)) (get a (pos + j))) then raise Exit
-    done;
-    true
-  with Exit -> false
-
-let symmetry _eq _a =
-  (* TODO *)
-  None
+(* FIXME: Complicated. *)
+let symmetry eq a =
+  let n = length a in
+  let is_sym pos len =
+    try
+      for i = 0 to (len / 2) - 1 do
+        if not (eq (get a (pos + i)) (get a (pos + len - 1 - i))) then raise Exit
+      done;
+      true
+    with Exit -> false
+  in
+  let rec loop = function
+    (* Skip [m] last/first items from the array. Are remaining
+       elements symmetrical around the middle? *)
+    | `No_Last, m when m >= n ->
+        (* Skipped all the elements, no line of symmetry found. *)
+        None
+    | `No_Last, m when is_sym 0 (n - m) ->
+        (* Skipped last [m] elements and elements from 0 up to (n - m)
+           are symmetrical. *)
+        Some ((n - m) / 2)
+    | `No_Last, m ->
+        (* Skipped last [m], no symmetry. Try to skip from the
+           beginning. *)
+        loop (`No_First, m)
+    | `No_First, m when is_sym m (n - m) ->
+        (* No first [m] elements and elements from m up to n are
+           symmetrical. *)
+        Some (m + ((n - m) / 2))
+    | `No_First, m ->
+        (* Skipped first [m] elements, no symmetry. Skip from the end
+           again, but this time skip more elements. *)
+        loop (`No_Last, m + 2)
+  in
+  if Int.is_even n then loop (`No_Last, 0) else loop (`No_Last, 1)
 
 let transpose a =
   let n_rows = length a in
--- 2023/lib/array.mli
+++ 2023/lib/array.mli
@@ -2,4 +2,5 @@ include module type of Stdlib.Array
 
 val equal : ('a -> 'a -> bool) -> 'a t -> 'a t -> bool
 val reduce : ('a -> 'a -> 'a) -> 'a t -> 'a
+val symmetry : ('a -> 'a -> bool) -> 'a t -> int option
 val transpose : 'a t t -> 'a t t

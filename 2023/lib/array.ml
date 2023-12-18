464215f16d203d1baf51164218a202965be4e7d5
Anton Yabchinskiy <arn@bestmx.net>
2023-12-18 20:18:30 +0400

    lib/array: add transpose function

--- 2023/lib/array.ml
+++ 2023/lib/array.ml
@@ -8,3 +8,8 @@ let reduce f a =
     r := f !r (unsafe_get a i)
   done;
   !r
+
+let transpose a =
+  let n_rows = Array.length a in
+  let n_cols = if n_rows > 0 then Array.(length (unsafe_get a 0)) else 0 in
+  Array.init n_cols (fun col -> Array.init n_rows (fun row -> a.(row).(col)))

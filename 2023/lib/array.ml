625dc528d24dec6f30c6a084c357345d6726b771
Anton Yabchinskiy <arn@bestmx.net>
2023-12-18 20:23:29 +0400

    lib/array: no need for fully-qualified function names in transpose, Array already included

--- 2023/lib/array.ml
+++ 2023/lib/array.ml
@@ -12,6 +12,6 @@ let reduce f a =
   !r
 
 let transpose a =
-  let n_rows = Array.length a in
-  let n_cols = if n_rows > 0 then Array.(length (unsafe_get a 0)) else 0 in
-  Array.init n_cols (fun col -> Array.init n_rows (fun row -> a.(row).(col)))
+  let n_rows = length a in
+  let n_cols = if n_rows > 0 then length (unsafe_get a 0) else 0 in
+  init n_cols (fun col -> init n_rows (fun row -> a.(row).(col)))

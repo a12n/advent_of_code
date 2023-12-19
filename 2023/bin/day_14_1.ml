3f3deed8e2577c352d4941e4f2597f95781bb44b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-19 19:58:35 +0400

    bin/day_14_1: fix call to Platform.tilt

--- 2023/bin/day_14_1.ml
+++ 2023/bin/day_14_1.ml
@@ -2,5 +2,6 @@ open Advent
 open Day_14
 
 let () =
-  input_lines stdin |> Platform.of_lines |> Platform.tilt |> Platform.load |> string_of_int
-  |> print_endline
+  input_lines stdin |> Platform.of_lines
+  |> (Fun.flip Platform.tilt) Dir.North
+  |> Platform.load |> string_of_int |> print_endline

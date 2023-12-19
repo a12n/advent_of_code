abc43ed8204188fccb0583e18df2fe15da4f07f2
Anton Yabchinskiy <arn@bestmx.net>
2023-12-20 00:50:58 +0300

    bin/day_14_2: solution

--- 2023/bin/day_14_2.ml
+++ 2023/bin/day_14_2.ml
@@ -1,16 +1,22 @@
 open Advent
 open Day_14
-
 module Platform_Hash = Hashtbl.Make (Platform)
 
 let () =
-  let p = Platform.of_lines (input_lines stdin) in
-  let seen = Platform_Hash.create 100 in (* TODO *)
-  for cycles = 1 to 1_000_000_000 do
-    Platform.tilt_cycle_inplace p;
-    Printf.eprintf "cycles %d, hash %d\n%!" cycles (Platform.hash p);
-    match Platform_Hash.find_opt seen p with
-    | Some k -> Printf.eprintf "After %d cycles is the same as after %d cycles\n%!" cycles k
-    | None -> Platform_Hash.replace seen p cycles
-  done;
-  print_endline (string_of_int (Platform.load p))
+  let plat = Platform.of_lines (input_lines stdin) in
+  let seen = Platform_Hash.create 256 in
+  let rec run i n =
+    if i < n then (
+      Printf.eprintf "@ %d ~ %d\n%!" i n;
+      Platform.tilt_cycle_inplace plat;
+      match Platform_Hash.find_opt seen plat with
+      | Some k ->
+        Printf.eprintf "@ %d = @ %d ~ n%!" i k;
+        Platform_Hash.reset seen;
+        run (n - ((n - i) mod (i - k)) + 1) n
+      | None ->
+        Platform_Hash.replace seen (Platform.copy plat) i;
+        run (i + 1) n)
+  in
+  run 0 1_000_000_000;
+  print_endline (string_of_int (Platform.load plat))

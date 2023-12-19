32e2ade996d7b067589bc857722908e3ace23c01
Anton Yabchinskiy <arn@bestmx.net>
2023-12-19 20:45:09 +0400

    bin/day_14_2: make Platform_Hash module

--- 2023/bin/day_14_2.ml
+++ 2023/bin/day_14_2.ml
@@ -1,9 +1,11 @@
 open Advent
 open Day_14
 
+module Platform_Hash = Hashtbl.Make (Platform)
+
 let () =
   let p = Platform.of_lines (input_lines stdin) in
-  let _seen = Hashtbl.create 8 in (* TODO *)
+  let _seen = Platform_Hash.create 10 in (* TODO *)
   for cycles = 1 to 1000000000 do
     Platform.tilt_cycle_inplace p;
     Printf.eprintf "cycles %d\n%!" cycles

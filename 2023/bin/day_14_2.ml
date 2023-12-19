e7463ff54685f66f61d0281802d8f086c39e9d40
Anton Yabchinskiy <arn@bestmx.net>
2023-12-19 20:45:29 +0400

    bin/day_14_2: number formatting

--- 2023/bin/day_14_2.ml
+++ 2023/bin/day_14_2.ml
@@ -6,7 +6,7 @@ module Platform_Hash = Hashtbl.Make (Platform)
 let () =
   let p = Platform.of_lines (input_lines stdin) in
   let _seen = Platform_Hash.create 10 in (* TODO *)
-  for cycles = 1 to 1000000000 do
+  for cycles = 1 to 1_000_000_000 do
     Platform.tilt_cycle_inplace p;
     Printf.eprintf "cycles %d\n%!" cycles
   done;

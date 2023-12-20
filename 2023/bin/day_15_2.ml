dfc5beff11ad3c06121d99c9ba5ae12b1815572c
Anton Yabchinskiy <arn@bestmx.net>
2023-12-20 12:36:39 +0400

    bin/day_15_2: perform steps, print total focusing power

--- 2023/bin/day_15_2.ml
+++ 2023/bin/day_15_2.ml
@@ -2,11 +2,7 @@ open Advent
 open Day_15
 
 let () =
-  let steps = read_line () |> String.split_on_char ',' |> List.map Step.of_string in
-  List.iter
-    (fun Step.{ label; op } ->
-      Printf.eprintf "\"%s\" : " label;
-      match op with
-      | `Remove -> Printf.eprintf ", remove\n"
-      | `Insert fd -> Printf.eprintf ", insert %d\n" fd)
-    steps
+  let boxes = Boxes.make () in
+  read_line () |> String.split_on_char ',' |> List.map Step.of_string
+  |> List.iter (Boxes.perform boxes);
+  print_endline (string_of_int (Boxes.focus_power boxes))

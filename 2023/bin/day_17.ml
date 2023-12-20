7d66fcd96009cca33e58c7847ef924291e4b20f8
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 01:32:25 +0300

    bin/day_17: work in progress

--- 2023/bin/day_17.ml
+++ 2023/bin/day_17.ml
@@ -1,9 +1,27 @@
 open Advent
 
+module Dir = struct
+  type t = Up | Left | Right | Down
+
+  let add_pos dir (row, col) =
+    match dir with
+    | Up -> (row - 1, col)
+    | Left -> (row, col - 1)
+    | Right -> (row, col + 1)
+    | Down -> (row + 1, col)
+end
+
+module Pos = struct
+  type t = int * int
+
+  let add (row1, col1) (row2, col2) = (row1 + row2, col1 + col2)
+  let add_dir (row, col) dir = Dir.add_pos dir (row, col)
+end
+
 module Grid : sig
   type t
 
-  val min_path : t -> (int * int) list * int
+  val min_path : t -> int
   val of_lines : string Seq.t -> t
   val size : t -> int * int
 end = struct
@@ -16,7 +34,23 @@ end = struct
   let size = Array.matrix_size
 
   let min_path grid =
-    (* TODO *)
-    ignore grid;
-    ([], 0)
+    let n_rows, n_cols = size grid in
+    (* let valid (row, col) = row >= 0 && row < n_rows && col >= 0 && col < n_cols in *)
+    let cells = Array.make_matrix n_rows n_cols None in
+    cells.(0).(0) <- Some (0, (0, 0));
+    for row = 0 to n_rows - 1 do
+      for col = 0 to n_cols - 1 do
+        List.iter
+          (fun off ->
+            (* Get other position. *)
+            (* Is it inside the grid? *)
+            (* Is there some state in other position? *)
+            (* Is not (offset = other offset = other other offset = other other other offset)? *)
+            (* Is this none or other is better? Then update this distance. *)
+            (* TODO *)
+            ignore off)
+          [ (0, -1); (-1, 0); (1, 0); (0, 1) ]
+      done
+    done;
+    Option.(get (map fst cells.(n_rows - 1).(n_cols - 1)))
 end
--- 2023/bin/day_17_1.ml
+++ 2023/bin/day_17_1.ml
@@ -3,5 +3,5 @@ open Day_17
 
 let () =
   let grid = Grid.of_lines (input_lines stdin) in
-  let _, heat_loss = Grid.min_path grid in
+  let heat_loss = Grid.min_path grid in
   print_endline (string_of_int heat_loss)

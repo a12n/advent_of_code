5e5edc8fa8db8f4298ce618a647e06d434ec4e66
Anton Yabchinskiy <arn@bestmx.net>
2023-12-20 08:20:33 +0300

    bin/day_15_1: \n as ,

--- 2023/bin/day_15_1.ml
+++ 2023/bin/day_15_1.ml
@@ -2,14 +2,11 @@ open Advent
 open Day_15
 
 let () =
-  let total, hash =
+  let total, _ =
     input_chars stdin
     |> Seq.fold_left
          (fun (total, hash) c ->
-           match c with
-           | '\n' -> (total, hash)
-           | ',' -> (total + hash, 0)
-           | c -> (total, update_hash hash c))
+           match c with ',' | '\n' -> (total + hash, 0) | c -> (total, update_hash hash c))
          (0, 0)
   in
-  print_endline (string_of_int (total + hash))
+  print_endline (string_of_int total)

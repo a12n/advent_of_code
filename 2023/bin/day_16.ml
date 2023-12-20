045f78cdde8474c3652fb821e84a9de64d599677
Anton Yabchinskiy <arn@bestmx.net>
2023-12-20 15:13:53 +0400

    bin/day_16: add Splitter.split

--- 2023/bin/day_16.ml
+++ 2023/bin/day_16.ml
@@ -48,6 +48,16 @@ end
 module Splitter = struct
   type t = Horiz | Vert
 
+  let split s to_dir beam =
+    Dir.(
+      match (to_dir, s) with
+      | Left, Horiz | Right, Horiz -> `Pass (to_dir, beam)
+      | Up, Vert | Down, Vert -> `Pass (to_dir, beam)
+      | Up, Horiz -> `Split ((Left, beam), (Right, Beam.split beam))
+      | Down, Horiz -> `Split ((Left, Beam.split beam), (Right, beam))
+      | Left, Vert -> `Split ((Down, beam), (Up, Beam.split beam))
+      | Right, Vert -> `Split ((Up, beam), (Down, Beam.split beam)))
+
   let of_char = function '-' -> Horiz | '|' -> Vert | _ -> invalid_arg __FUNCTION__
   let to_char = function Horiz -> '-' | Vert -> '|'
 end

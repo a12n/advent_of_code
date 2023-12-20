a0f47fbe9ba30a026e18e0712e4e9f47ad8fa26e
Anton Yabchinskiy <arn@bestmx.net>
2023-12-20 16:01:17 +0400

    bin/day_16_1: Grid.trace pos is the current position

--- 2023/bin/day_16_1.ml
+++ 2023/bin/day_16_1.ml
@@ -3,5 +3,5 @@ open Day_16
 
 let () =
   let grid = Grid.of_lines (input_lines stdin) in
-  let energized = Grid.trace grid (0, -1) Dir.Right in
+  let energized = Grid.trace grid (0, 0) Dir.Right in
   print_endline (string_of_int (Energized_Map.length energized))

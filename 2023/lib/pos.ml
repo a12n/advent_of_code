993bbf4d3ac856e3c2ed9f646623af9646b3ba42
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 15:44:57 +0400

    lib/pos: add is_horiz_aligned, is_vert_aligned and is_aligned functions

--- 2023/lib/pos.ml
+++ 2023/lib/pos.ml
@@ -3,3 +3,12 @@ type t = int * int
 let add (row1, col1) (row2, col2) = (row1 + row2, col1 + col2)
 let add_dir pos dir = add pos (Dir.to_pos dir)
 let sub (row1, col1) (row2, col2) = (row1 - row2, col1 - col2)
+
+(** Two points are on a horizontal line. *)
+let is_horiz_aligned (row1, _) (row2, _) = row1 = row2
+
+(** Two points are on a vertical line. *)
+let is_vert_aligned (_, col1) (_, col2) = col1 = col2
+
+(** Two points are on a horizontal or on a vertical line. *)
+let is_aligned p0 p1 = is_horiz_aligned p0 p1 || is_vert_aligned p0 p1

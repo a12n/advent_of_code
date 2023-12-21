330341a9266b01ac7522bfb2e168ee5c524eeb62
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 19:28:57 +0400

    lib/dir: add to_char

--- 2023/lib/dir.ml
+++ 2023/lib/dir.ml
@@ -1,3 +1,4 @@
 type t = Up | Left | Right | Down
 
+let to_char = function Up -> '^' | Left -> '<' | Right -> '>' | Down -> 'v'
 let to_pos = function Up -> (-1, 0) | Left -> (0, -1) | Right -> (0, 1) | Down -> (1, 0)

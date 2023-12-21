354faa14c9f3c8624a2191e235c3a22cbe8f79cb
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 22:47:01 +0300

    bin/day_18_1: remove debug output

--- 2023/bin/day_18_1.ml
+++ 2023/bin/day_18_1.ml
@@ -4,13 +4,5 @@ open Day_18
 let () =
   let plan = Plan.of_lines (input_lines stdin) in
   let grid = Grid.of_plan plan in
-  Format.(
-    pp_print_string Format.err_formatter "Before:\n";
-    Grid.pp err_formatter grid;
-  );
   Grid.dig_interior grid;
-  Format.(
-    pp_print_string Format.err_formatter "After:\n";
-    Grid.pp err_formatter grid;
-  );
   print_endline (string_of_int (Grid.trench_volume grid))

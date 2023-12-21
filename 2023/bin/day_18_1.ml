81a94361ead09d176b3246451536ce90fef968fe
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 19:18:42 +0400

    bin/day_18_1: print Grid.trench_volume

--- 2023/bin/day_18_1.ml
+++ 2023/bin/day_18_1.ml
@@ -3,5 +3,6 @@ open Day_18
 
 let () =
   let plan = Plan.of_lines (input_lines stdin) in
-  (* TODO *)
-  ignore plan
+  let grid = Grid.of_plan plan in
+  Grid.dig_interior grid;
+  print_endline (string_of_int (Grid.trench_volume grid))

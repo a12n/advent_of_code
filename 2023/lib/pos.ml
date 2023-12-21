27eff84e42ada26374a75a30e3b55776b090d7b3
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 18:43:45 +0400

    lib/pos: fix is_valid

--- 2023/lib/pos.ml
+++ 2023/lib/pos.ml
@@ -13,4 +13,4 @@ let is_vert_aligned (_, col1) (_, col2) = col1 = col2
 (** Two points are on a horizontal or on a vertical line. *)
 let is_aligned p0 p1 = is_horiz_aligned p0 p1 || is_vert_aligned p0 p1
 
-let is_valid (n_rows, n_cols) (row, col) = row >= 0 && col >= n_cols && row < n_rows && col < n_cols
+let is_valid (n_rows, n_cols) (row, col) = row >= 0 && col >= 0 && row < n_rows && col < n_cols

69d12cbbc4c3df8c13d0fa47a0e60b2da78b3558
Anton Yabchinskiy <arn@bestmx.net>
2023-12-22 02:33:13 +0300

    lib/polygon: work in progress

--- 2023/lib/polygon.ml
+++ 2023/lib/polygon.ml
@@ -38,12 +38,13 @@ let area =
 let straight_joint (r0, c0) (r1, c1) (r2, c2) = (r0 = r1 && r1 = r2) || (c0 = c1 && c1 = c2)
 
 let right_turn_joint (r0, c0) (r1, c1) (r2, c2) =
-  (r0 = r1 && c1 = c2 && r1 < r2) || (c0 = c1 && r1 = r2 && c1 < c2)
+  (r0 = r1 && c1 = c2 && r1 > r2) || (c0 = c1 && r1 = r2 && c1 < c2)
 
 let left_turn_joint (r0, c0) (r1, c1) (r2, c2) =
-  (r0 = r1 && c1 = c2 && r1 > r2) || (c0 = c1 && r1 = r2 && c1 > c2)
+  (r0 = r1 && c1 = c2 && r1 < r2) || (c0 = c1 && r1 = r2 && c1 > c2)
 
-let excessive_area =
+(* Excessive area in line joints. *)
+let joint_area =
   let print_joint i pi j pj k pk =
     Printf.eprintf "p%d (%d, %d) - p%d (%d, %d) - p%d (%d, %d) = %c%c%c\n%!" i (fst pi) (snd pi) j
       (fst pj) (snd pj) k (fst pk) (snd pk)
@@ -51,25 +52,55 @@ let excessive_area =
       (if straight_joint pi pj pk then '|' else '.')
       (if right_turn_joint pi pj pk then '>' else '.')
   in
+  let part_area pi pj pk =
+    if straight_joint pi pj pk then (50, 50)
+    else if left_turn_joint pi pj pk then (25, 75)
+    else if right_turn_joint pi pj pk then (75, 25)
+    else assert false
+  in
   function
   | p0 :: p1 :: p2 :: ps ->
       let rec do_area (total1, total2) i = function
         | pi :: pj :: pk :: ps ->
             (* pi pj pk *)
             print_joint i pi (i + 1) pj (i + 2) pk;
-            do_area (total1, total2) (i + 1) (pj :: pk :: ps)
+            let part1, part2 = part_area pi pj pk in
+            do_area (total1 + part1, total2 + part2) (i + 1) (pj :: pk :: ps)
         | [ pi; pj ] ->
             (* pi pj p0 *)
             print_joint i pi (i + 1) pj 0 p0;
-            do_area (total1,total2) (i + 1) [ pj ]
+            let part1, part2 = part_area pi pj p0 in
+            do_area (total1 + part1, total2 + part2) (i + 1) [ pj ]
         | [ pi ] ->
             (* pi p0 p1 *)
             print_joint i pi 0 p0 1 p1;
-            (total1, total2)
+            let part1, part2 = part_area pi p0 p1 in
+            (total1 + part1, total2 + part2)
         | [] -> assert false
       in
-      do_area (0,0) 0 (p0 :: p1 :: p2 :: ps)
-  | _ -> (0,0)
+      do_area (0, 0) 0 (p0 :: p1 :: p2 :: ps)
+  | _ -> (0, 0)
+
+(* Excessive area in straight parts of the lines connecting points. *)
+let lines_area =
+  let part_area (ri, ci) (rj, cj) =
+    if ri = rj then 50 * (Int.abs (cj - ci) - 1)
+    else if ci = cj then 50 * (Int.abs (rj - ri) - 1)
+    else assert false
+  in
+  function
+  | p0 :: ps ->
+      let rec do_area total = function
+        | pi :: pj :: ps ->
+            let part = part_area pi pj in
+            do_area (total + part) (pj :: ps)
+        | [ pi ] ->
+            let part = part_area pi p0 in
+            total + part
+        | [] -> assert false
+      in
+      do_area 0 (p0 :: ps)
+  | [] -> 0
 
 let test1 =
   (* 4 *)

c7a4975be067b64ce422d4975cf55b3665f99a7a
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 18:17:49 +0400

    bin/day_18: add Plan.size, add Grid.of_plan

--- 2023/bin/day_18.ml
+++ 2023/bin/day_18.ml
@@ -22,4 +22,43 @@ module Plan = struct
   type t = Dig.t list
 
   let of_lines = List.of_seq % Seq.map Dig.of_string
+
+  let size plan =
+    let _, (max_row, max_col) =
+      List.fold_left
+        (fun (pos, max) Dig.{ dir; length; _ } ->
+          let v = Dir.to_pos dir in
+          let pos = Pos.(add pos (mul_int v length)) in
+          (pos, Stdlib.max max pos))
+        ((0, 0), (0, 0))
+        plan
+    in
+    (max_row + 1, max_col + 1)
+end
+
+module Grid = struct
+  type t = [ `Ground | `Trench ] option array array
+
+  let of_plan plan =
+    let n_rows, n_cols = Plan.size plan in
+    let grid = Array.make_matrix n_rows n_cols None in
+    let rec do_dig ((row, col) as pos) = function
+      | [] -> ()
+      | Dig.{ length = 0; _ } :: plan -> do_dig pos plan
+      | (Dig.{ dir; length; _ } as dig) :: plan ->
+          grid.(row).(col) <- Some `Trench;
+          do_dig (Pos.add pos (Dir.to_pos dir)) (Dig.{ dig with length = length - 1 } :: plan)
+    in
+    do_dig (0, 0) plan;
+    grid
+
+  let pp fmt =
+    Array.iter (fun line ->
+        Array.iter
+          (fun elt ->
+            Format.pp_print_char fmt
+              (match elt with None -> '?' | Some `Ground -> '.' | Some `Trench -> '#'))
+          line;
+        Format.pp_print_newline fmt ())
+  [@@ocaml.toplevel_printer]
 end

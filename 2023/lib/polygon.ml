ad706c64ab2af8b82c91eb2a60cffe8326836d91
Anton Yabchinskiy <arn@bestmx.net>
2023-12-21 23:59:41 +0300

    lib/polygon: fix do_area, i parameter

--- 2023/lib/polygon.ml
+++ 2023/lib/polygon.ml
@@ -1,6 +1,5 @@
 (** Gauss's area formula adjusted for integer 2D grids. Polygon is a
     list of points on a grid (the input) connected with implicit lines. *)
-
 let area =
   let check_aligned p0 p1 =
     (* All lines in a polygon must be axis-aligned. *)
@@ -11,17 +10,21 @@ let area =
     (ci * rj) - (cj * ri)
   in
   function
-  | (r0, c0) :: p1 :: ps ->
+  | p0 :: p1 :: ps ->
       (* TODO: adjust for excessive area. Compute area in 1/100's,
          take into account 1/2 (on sides), 1/4 and 3/4 (external and
          internal angles). *)
-      let rec do_area total = function
+      let rec do_area total i = function
         | [] -> total
         | pi :: pj :: ps ->
             check_aligned pi pj;
-            do_area (total + (100 * shoelace pi pj)) (pj :: ps)
-        | [ pn ] -> do_area total [ pn; (r0, c0) ]
+            let part = 100 * shoelace pi pj in
+            do_area (total + part) (i + 1) (pj :: ps)
+        | [ pi ] ->
+            check_aligned pi p0;
+            let part = 100 * shoelace pi p0 in
+            total + part
       in
-      let total = do_area 0 ((r0, c0) :: p1 :: ps) in
+      let total = do_area 0 0 (p0 :: p1 :: ps) in
       Int.abs total / (2 * 100)
   | [ _ ] | [] -> 0

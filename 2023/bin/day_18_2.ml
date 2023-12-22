2c28e6219aa2546110579fd40d85aec5768a0f32
Anton Yabchinskiy <arn@bestmx.net>
2023-12-22 19:10:59 +0400

    bin/day_18_2: Plan.of_lines2

--- 2023/bin/day_18_2.ml
+++ 2023/bin/day_18_2.ml
@@ -2,5 +2,5 @@ open Advent
 open Day_18
 
 let () =
-  let plan = Plan.of_lines2 (input_lines stdin) in
+  Plan.of_lines2 (input_lines stdin)
   |> Plan.to_list_pos |> Polygon.area |> string_of_int |> print_endline

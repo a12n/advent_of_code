7421a813c6d9f6581fb56536d87f67b93681520e
Anton Yabchinskiy <arn@bestmx.net>
2023-12-22 21:28:24 +0300

    bin/day_18_2: Polygon.interior_area was renamed

--- 2023/bin/day_18_2.ml
+++ 2023/bin/day_18_2.ml
@@ -2,5 +2,7 @@ open Advent
 open Day_18
 
 let () =
+  (* FIXME: Must include interior_area and boundary_area. As for now
+     interior_area erroneously includes boundary area in the result. *)
   Plan.of_lines2 (input_lines stdin)
-  |> Plan.to_list_pos |> Polygon.area |> string_of_int |> print_endline
+  |> Plan.to_list_pos |> Polygon.interior_area |> string_of_int |> print_endline

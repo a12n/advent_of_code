1ba37a547b901a078352fd8750ec1b946c4e1a94
Anton Yabchinskiy <arn@bestmx.net>
2023-12-22 20:04:17 +0400

    bin/day_10_2: debug output

--- 2023/bin/day_10_2.ml
+++ 2023/bin/day_10_2.ml
@@ -5,7 +5,16 @@ let () =
   let grid = Grid.of_lines (input_lines stdin) in
   Grid.pp Format.err_formatter grid;
   let cycle = Option.get (Grid.cycle grid) in
-  Printf.eprintf "cycle %d\n%!" (List.length cycle);
-  let cycle = Polygon.compact cycle in
-  Printf.eprintf "compact cycle %d\n%!" (List.length cycle);
+  let pp_sep fmt () = Format.pp_print_string fmt "; " in
+  Format.(
+    pp_print_string err_formatter "Cycle: ";
+    pp_print_list ~pp_sep Pos.pp Format.err_formatter cycle;
+    pp_print_newline err_formatter ()
+  );
+  let cycle = List.rev (Polygon.compact cycle) in
+  Format.(
+    pp_print_string err_formatter "Compact Rev Cycle: ";
+    pp_print_list ~pp_sep Pos.pp Format.err_formatter cycle;
+    pp_print_newline err_formatter ()
+  );
   Polygon.area cycle |> string_of_int |> print_endline

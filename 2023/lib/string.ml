6bf5a77d7e06a3018f06d7f8b7a49c15ad2aa3d1
Anton Yabchinskiy <arn@bestmx.net>
2023-12-23 18:32:05 +0300

    lib/string: add split_on_chars

--- 2023/lib/string.ml
+++ 2023/lib/string.ml
@@ -1,3 +1,8 @@
 include Stdlib.String
 
+let rec split_on_chars sep s =
+  match sep with
+  | [] -> [ s ]
+  | c :: cs -> List.(concat (map (split_on_chars cs) (split_on_char c s)))
+
 let to_list s = List.init (length s) (unsafe_get s)
--- 2023/lib/string.mli
+++ 2023/lib/string.mli
@@ -1,3 +1,4 @@
 include module type of Stdlib.String
 
+val split_on_chars : char list -> string -> string list
 val to_list : string -> char list

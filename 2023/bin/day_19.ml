b3ef39b7fae697bc02370842a3c5c98b54eb6af1
Anton Yabchinskiy <arn@bestmx.net>
2023-12-24 00:22:12 +0300

    bin/day_19: add Part.Range

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -11,4 +11,21 @@ module Part = struct
   let rating { x; m; a; s } = x + m + a + s
   let of_string s = Scanf.sscanf s "{x=%d,m=%d,a=%d,s=%d}" (fun x m a s -> { x; m; a; s })
 
+  module Range = struct
+    let full = ({ x = 1; m = 1; a = 1; s = 1 }, { x = 4000; m = 4000; a = 4000; s = 4000 })
+
+    let merge (min1, max1) (min2, max2) =
+      ( {
+          x = Int.max min1.x min2.x;
+          m = Int.max min1.m min2.m;
+          a = Int.max min1.a min2.a;
+          s = Int.max min1.s min2.s;
+        },
+        {
+          x = Int.min max1.x max2.x;
+          m = Int.min max1.m max2.m;
+          a = Int.min max1.a max2.a;
+          s = Int.min max1.s max2.s;
+        } )
+  end
 end

bfbd7a4e283f5c3f34dc59b683a6a36658f3d4d7
Anton Yabchinskiy <arn@bestmx.net>
2023-12-24 00:19:10 +0300

    bin/day_19: move Workflow and System to Day_19_1

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -1,5 +1,3 @@
-open Advent
-
 module Part = struct
   type t = { x : int; m : int; a : int; s : int }
 
@@ -12,60 +10,5 @@ module Part = struct
 
   let rating { x; m; a; s } = x + m + a + s
   let of_string s = Scanf.sscanf s "{x=%d,m=%d,a=%d,s=%d}" (fun x m a s -> { x; m; a; s })
-end
 
-module Rule = struct
-  type t = Part.t -> (string, bool) result option
-
-  let of_string s =
-    let action_of_string = function "A" -> Error true | "R" -> Error false | name -> Ok name in
-    let pred_of_char = function
-      | '<' -> Stdlib.( < )
-      | '>' -> Stdlib.( > )
-      | _ -> invalid_arg __FUNCTION__
-    in
-    match String.split_on_char ':' s |> List.filter (( <> ) "") with
-    | [ action ] ->
-        let action = action_of_string action in
-        fun _ -> Some action
-    | [ cond; action ] ->
-        let get = Part.get cond.[0] in
-        let pred = pred_of_char cond.[1] in
-        let value = int_of_string String.(sub cond 2 (length cond - 2)) in
-        let action = action_of_string action in
-        fun p -> if pred (get p) value then Some action else None
-    | _ -> invalid_arg __FUNCTION__
-end
-
-module Workflow = struct
-  type t = Rule.t list
-
-  let eval wf p = List.find_map (fun r -> r p) wf
-  let of_string = List.map Rule.of_string % String.split_on_char ','
-end
-
-module System = struct
-  type t = (string, Workflow.t) Hashtbl.t
-
-  let eval sys =
-    let rec loop id p =
-      match Workflow.eval (Hashtbl.find sys id) p with
-      | Some (Ok id') -> loop id' p
-      | Some (Error rejected) -> rejected
-      | _ -> failwith __FUNCTION__
-    in
-    loop "in"
-
-  let of_lines =
-    Seq.fold_left
-      (fun sys line ->
-        (match String.split_on_char '{' line with
-        | [ id; workflow' ] -> (
-            match String.split_on_char '}' workflow' with
-            | [ workflow; "" ] -> Hashtbl.replace sys id (Workflow.of_string workflow)
-            | _ -> invalid_arg __FUNCTION__)
-        | _ -> invalid_arg __FUNCTION__);
-        sys)
-      (Hashtbl.create 100)
-    % Seq.take_while (( <> ) "")
 end
--- 2023/bin/day_19_1.ml
+++ 2023/bin/day_19_1.ml
@@ -1,6 +1,76 @@
 open Day_19
 open Advent
 
+module Rule : sig
+  type t = Part.t -> (string, bool) result option
+
+  val of_string : string -> t
+end = struct
+  type t = Part.t -> (string, bool) result option
+
+  let of_string s =
+    let action_of_string = function "A" -> Error true | "R" -> Error false | name -> Ok name in
+    let pred_of_char = function
+      | '<' -> Stdlib.( < )
+      | '>' -> Stdlib.( > )
+      | _ -> invalid_arg __FUNCTION__
+    in
+    match String.split_on_char ':' s |> List.filter (( <> ) "") with
+    | [ action ] ->
+        let action = action_of_string action in
+        fun _ -> Some action
+    | [ cond; action ] ->
+        let get = Part.get cond.[0] in
+        let pred = pred_of_char cond.[1] in
+        let value = int_of_string String.(sub cond 2 (length cond - 2)) in
+        let action = action_of_string action in
+        fun p -> if pred (get p) value then Some action else None
+    | _ -> invalid_arg __FUNCTION__
+end
+
+module Workflow : sig
+  type t = Rule.t list
+
+  val eval : t -> Rule.t
+  val of_string : string -> t
+end = struct
+  type t = Rule.t list
+
+  let eval wf p = List.find_map (fun r -> r p) wf
+  let of_string = List.map Rule.of_string % String.split_on_char ','
+end
+
+module System : sig
+  type t
+
+  val eval : t -> Part.t -> bool
+  val of_lines : string Seq.t -> t
+end = struct
+  type t = (string, Workflow.t) Hashtbl.t
+
+  let eval sys =
+    let rec loop id p =
+      match Workflow.eval (Hashtbl.find sys id) p with
+      | Some (Ok id') -> loop id' p
+      | Some (Error rejected) -> rejected
+      | _ -> failwith __FUNCTION__
+    in
+    loop "in"
+
+  let of_lines =
+    Seq.fold_left
+      (fun sys line ->
+        (match String.split_on_char '{' line with
+        | [ id; workflow' ] -> (
+            match String.split_on_char '}' workflow' with
+            | [ workflow; "" ] -> Hashtbl.replace sys id (Workflow.of_string workflow)
+            | _ -> invalid_arg __FUNCTION__)
+        | _ -> invalid_arg __FUNCTION__);
+        sys)
+      (Hashtbl.create 100)
+    % Seq.take_while (( <> ) "")
+end
+
 let () =
   let lines = input_lines stdin in
   let sys = System.of_lines lines in

cb3c734c122866d71277fbd36c5cbb0e9d3ab368
Anton Yabchinskiy <arn@bestmx.net>
2023-12-23 14:31:49 +0300

    lib/dir: add of_pos

--- 2023/lib/dir.ml
+++ 2023/lib/dir.ml
@@ -4,3 +4,10 @@ let neg = function Up -> Down | Left -> Right | Right -> Left | Down -> Up
 let to_char = function Up -> '^' | Left -> '<' | Right -> '>' | Down -> 'v'
 let to_pos = function Up -> (-1, 0) | Left -> (0, -1) | Right -> (0, 1) | Down -> (1, 0)
 let to_string = function Up -> "↑" | Left -> "←" | Down -> "↓" | Right -> "→"
+
+let of_pos = function
+  | 0, n when n < 0 -> Left
+  | 0, n when n > 0 -> Right
+  | n, 0 when n < 0 -> Down
+  | n, 0 when n > 0 -> Up
+  | _, _ -> invalid_arg __FUNCTION__

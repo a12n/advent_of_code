c2d223f4cd55a20d3363e4b3862a4f6c7a81836a
Anton Yabchinskiy <arn@bestmx.net>
2023-12-23 23:29:31 +0300

    bin/day_19: add Part.rating, Part.of_string, char parameter in Part.get

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -1,12 +1,17 @@
+open Advent
+
 module Part = struct
   type t = { x : int; m : int; a : int; s : int }
 
   let get = function
-    | "x" -> fun { x; _ } -> x
-    | "m" -> fun { m; _ } -> m
-    | "a" -> fun { a; _ } -> a
-    | "s" -> fun { s; _ } -> s
+    | 'x' -> fun { x; _ } -> x
+    | 'm' -> fun { m; _ } -> m
+    | 'a' -> fun { a; _ } -> a
+    | 's' -> fun { s; _ } -> s
     | _ -> invalid_arg __FUNCTION__
+
+  let rating { x; m; a; s } = x + m + a + s
+  let of_string s = Scanf.sscanf s "{x=%d,m=%d,a=%d,s=%d}" (fun x m a s -> { x; m; a; s })
 end
 
 module Rule = struct

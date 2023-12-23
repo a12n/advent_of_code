1db6515471aec1a64a47d67ce30424d9a7f1678b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-23 18:20:23 +0300

    bin/day_19: add Rule.t

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -1,3 +1,16 @@
 module Part = struct
   type t = { x : int; m : int; a : int; s : int }
 end
+
+module Rule = struct
+  type t = Part.t -> (string, bool) result
+
+  let of_string s =
+    match String.split_on_char ':' s |> List.filter (( <> ) "") with
+    | [ "A" ] -> fun _ -> Error true
+    | [ "R" ] -> fun _ -> Error false
+    | [ cond; act ] ->
+        (* TODO *)
+        fun _ -> Ok ""
+    | _ -> invalid_arg (__FUNCTION__ ^ ": \"" ^ s ^ "\"")
+end

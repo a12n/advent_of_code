b41e353e43d4a1e59e8d4a3dd29912e3b3dc22da
Anton Yabchinskiy <arn@bestmx.net>
2023-12-24 00:29:15 +0300

    bin/day_19_2: work in progress on range variants of Rule, Workflow and System

--- 2023/bin/day_19_2.ml
+++ 2023/bin/day_19_2.ml
@@ -1,6 +1,58 @@
 open Day_19
 open Advent
 
+module Rule : sig
+  (* TODO *)
+  type t = Part.Range.t -> unit
+
+  val of_string : string -> t
+end = struct
+  type t = Part.Range.t -> unit
+
+  let of_string s =
+    (* TODO *)
+    ignore s;
+    failwith __FUNCTION__
+end
+
+module Workflow : sig
+  type t = Rule.t list
+
+  val eval : t -> Part.Range.t -> (string * Part.Range.t) list * Part.Range.t * Part.Range.t
+  val of_string : string -> t
+end = struct
+  type t = Rule.t list
+
+  let eval wf p =
+    (* TODO *)
+    ignore wf;
+    ([], p, p)
+
+  let of_string s =
+    (* TODO *)
+    ignore s;
+    []
+end
+
+module System : sig
+  type t
+
+  val eval : t -> Part.Range.t * Part.Range.t
+  val of_lines : string Seq.t -> t
+end = struct
+  type t = (string, Workflow.t) Hashtbl.t
+
+  let eval sys =
+    (* TODO *)
+    ignore sys;
+    (Part.Range.full, Part.Range.full)
+
+  let of_lines lines =
+    (* TODO *)
+    ignore lines;
+    Hashtbl.create 100
+end
+
 let () =
   let lines = input_lines stdin in
   let sys = System.of_lines lines in

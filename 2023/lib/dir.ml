15c19bf28800eae00b4ff6740ce2b33fd006fb8e
Anton Yabchinskiy <arn@bestmx.net>
2023-12-23 14:31:34 +0300

    lib/dir: add to_string

--- 2023/lib/dir.ml
+++ 2023/lib/dir.ml
@@ -3,3 +3,4 @@ type t = Up | Left | Right | Down
 let neg = function Up -> Down | Left -> Right | Right -> Left | Down -> Up
 let to_char = function Up -> '^' | Left -> '<' | Right -> '>' | Down -> 'v'
 let to_pos = function Up -> (-1, 0) | Left -> (0, -1) | Right -> (0, 1) | Down -> (1, 0)
+let to_string = function Up -> "↑" | Left -> "←" | Down -> "↓" | Right -> "→"

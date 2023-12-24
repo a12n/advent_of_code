927450876b53a114f11c8b2ec64b451f6ee6e40a
Anton Yabchinskiy <arn@bestmx.net>
2023-12-25 01:10:44 +0300

    bin/day_19: add Part.Set.t

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -25,6 +25,50 @@ module Part = struct
   let rating { x; m; a; s } = x + m + a + s
   let of_string s = Scanf.sscanf s "{x=%d,m=%d,a=%d,s=%d}" (fun x m a s -> { x; m; a; s })
 
+  module Set = struct
+    module Int_Set = Set.Make (Int)
+
+    type t = { x : Int_Set.t; m : Int_Set.t; a : Int_Set.t; s : Int_Set.t }
+
+    let get = function
+      | 'x' -> fun { x; _ } -> x
+      | 'm' -> fun { m; _ } -> m
+      | 'a' -> fun { a; _ } -> a
+      | 's' -> fun { s; _ } -> s
+      | _ -> invalid_arg __FUNCTION__
+
+    let set = function
+      | 'x' -> fun p x -> { p with x }
+      | 'm' -> fun p m -> { p with m }
+      | 'a' -> fun p a -> { p with a }
+      | 's' -> fun p s -> { p with s }
+      | _ -> invalid_arg __FUNCTION__
+
+    let empty = { x = Int_Set.empty; m = Int_Set.empty; a = Int_Set.empty; s = Int_Set.empty }
+
+    let full =
+      let x = Int_Set.of_seq Seq.(take 4000 (ints 1)) in
+      { x; m = x; a = x; s = x }
+
+    let inter s1 s2 =
+      {
+        x = Int_Set.inter s1.x s2.x;
+        m = Int_Set.inter s1.m s2.m;
+        a = Int_Set.inter s1.a s2.a;
+        s = Int_Set.inter s1.s s2.s;
+      }
+
+    let union s1 s2 =
+      {
+        x = Int_Set.union s1.x s2.x;
+        m = Int_Set.union s1.m s2.m;
+        a = Int_Set.union s1.a s2.a;
+        s = Int_Set.union s1.s s2.s;
+      }
+
+    let cardinal { x; m; a; s } = Int_Set.(cardinal x * cardinal m * cardinal a * cardinal s)
+  end
+
   module Range = struct
     type t = part * part
 

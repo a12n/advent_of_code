c7672af77edbf2cff16ba3e51b896c1f755f34a1
Anton Yabchinskiy <arn@bestmx.net>
2023-12-24 19:05:52 +0300

    bin/day_19: add Part.map2_{min,max}, Part.empty, rename Part.inter

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -2,6 +2,12 @@ module Part = struct
   type t = { x : int; m : int; a : int; s : int }
   and part = t
 
+  let map2_min p1 p2 =
+    { x = Int.min p1.x p2.x; m = Int.min p1.m p2.m; a = Int.min p1.a p2.a; s = Int.min p1.s p2.s }
+
+  let map2_max p1 p2 =
+    { x = Int.max p1.x p2.x; m = Int.max p1.m p2.m; a = Int.max p1.a p2.a; s = Int.max p1.s p2.s }
+
   let get = function
     | 'x' -> fun { x; _ } -> x
     | 'm' -> fun { m; _ } -> m
@@ -22,20 +28,9 @@ module Part = struct
   module Range = struct
     type t = part * part
 
+    let empty = ({ x = 1; m = 1; a = 1; s = 1 }, { x = 0; m = 0; a = 0; s = 0 })
     let full = ({ x = 1; m = 1; a = 1; s = 1 }, { x = 4000; m = 4000; a = 4000; s = 4000 })
 
-    let merge (min1, max1) (min2, max2) =
-      ( {
-          x = Int.max min1.x min2.x;
-          m = Int.max min1.m min2.m;
-          a = Int.max min1.a min2.a;
-          s = Int.max min1.s min2.s;
-        },
-        {
-          x = Int.min max1.x max2.x;
-          m = Int.min max1.m max2.m;
-          a = Int.min max1.a max2.a;
-          s = Int.min max1.s max2.s;
-        } )
+    let inter (min1, max1) (min2, max2) = (map2_max min1 min2, map2_min max1 max2)
   end
 end

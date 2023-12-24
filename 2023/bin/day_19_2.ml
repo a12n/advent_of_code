8454a141e58e557dd00a343238567f116625ac53
Anton Yabchinskiy <arn@bestmx.net>
2023-12-25 01:12:01 +0300

    bin/day_19_2: try with sets

--- 2023/bin/day_19_2.ml
+++ 2023/bin/day_19_2.ml
@@ -3,10 +3,13 @@ open Advent
 
 module Rule : sig
   type t = Part.Range.t -> ([ `Accept | `Reject | `Send of string ] * Part.Range.t) * Part.Range.t
+  type t2 = Part.Set.t -> ([ `Accept | `Reject | `Send of string ] * Part.Set.t) * Part.Set.t
 
   val of_string : string -> t
+  val of_string2 : string -> t2
 end = struct
   type t = Part.Range.t -> ([ `Accept | `Reject | `Send of string ] * Part.Range.t) * Part.Range.t
+  type t2 = Part.Set.t -> ([ `Accept | `Reject | `Send of string ] * Part.Set.t) * Part.Set.t
 
   let of_string s =
     let action_of_string = function "A" -> `Accept | "R" -> `Reject | name -> `Send name in
@@ -32,48 +35,100 @@ end = struct
               ((action, (min, max')), (min', max))
         | _ -> invalid_arg __FUNCTION__)
     | _ -> invalid_arg __FUNCTION__
+
+  let of_string2 s =
+    let action_of_string = function "A" -> `Accept | "R" -> `Reject | name -> `Send name in
+    match String.split_on_char ':' s |> List.filter (( <> ) "") with
+    | [ action ] ->
+        let action = action_of_string action in
+        fun parts -> ((action, parts), Part.Set.empty)
+    | [ cond; action ] -> (
+        let get = Part.Set.get cond.[0] in
+        let set = Part.Set.set cond.[0] in
+        let value = int_of_string String.(sub cond 2 (length cond - 2)) in
+        let action = action_of_string action in
+        let pred =
+          match cond.[1] with
+          | '>' -> (fun n -> n > value)
+          | '<' -> (fun n -> n < value)
+          | _ -> invalid_arg __FUNCTION__ in
+        fun parts ->
+          let send, pass = Part.Set.Int_Set.partition pred (get parts) in
+          ((action, set parts send), set parts pass)
+      )
+    | _ -> invalid_arg __FUNCTION__
 end
 
 module Workflow : sig
   type t = Rule.t list
+  type t2 = Rule.t2 list
 
-  val eval : t -> Part.Range.t -> (string * Part.Range.t) list * Part.Range.t * Part.Range.t
+  val eval : t -> Part.Range.t -> (string * Part.Range.t) list * Part.Range.t list
+  val eval2 : t2 -> Part.Set.t -> (string * Part.Set.t) list * Part.Set.t
   val of_string : string -> t
+  val of_string2 : string -> t2
 end = struct
   type t = Rule.t list
+  type t2 = Rule.t2 list
 
   let eval wf p =
-    (* TODO *)
-    ignore wf;
-    ([], p, p)
+    let sent, accepted, _ =
+      List.fold_left
+        (fun (sent, accepted, p) r ->
+          match r p with
+          | (`Accept, accept), pass -> (sent, accept :: accepted, pass)
+          | (`Reject, _), pass -> (sent, accepted, pass)
+          | (`Send id, send), pass -> ((id, send) :: sent, accepted, pass))
+        ([], [], p) wf
+    in
+    (sent, accepted)
 
-  let of_string s =
-    (* TODO *)
-    ignore s;
-    []
+  let eval2 wf parts =
+    let send, accept, _ =
+      List.fold_left
+        (fun (send, accept, parts) rule ->
+           match rule parts with
+           | (`Accept, a), pass -> (send, Part.Set.union accept a, pass)
+           | (`Reject, _), pass -> (send, accept, pass)
+           | (`Send id, s), pass -> ((id,s)::send, accept, pass)) ([], Part.Set.empty, parts) wf in
+    (send, accept)
+
+  let of_string = List.map Rule.of_string % String.split_on_char ','
+  let of_string2 = List.map Rule.of_string2 % String.split_on_char ','
 end
 
 module System : sig
   type t
 
-  val eval : t -> Part.Range.t * Part.Range.t
+  (* Returns accepted part range. *)
+  val eval : t -> Part.Set.t
   val of_lines : string Seq.t -> t
 end = struct
-  type t = (string, Workflow.t) Hashtbl.t
+  type t = (string, Workflow.t2) Hashtbl.t
 
   let eval sys =
-    (* TODO *)
-    ignore sys;
-    (Part.Range.full, Part.Range.full)
+    let rec loop id parts =
+      let send, accept = Workflow.eval2 (Hashtbl.find sys id) parts in
+      List.fold_left (fun accept (id, parts) ->
+          Part.Set.union accept (loop id parts)) accept send in
+    loop "in" Part.Set.full
 
-  let of_lines lines =
-    (* TODO *)
-    ignore lines;
-    Hashtbl.create 100
+  let of_lines =
+    Seq.fold_left
+      (fun sys line ->
+        (match String.split_on_char '{' line with
+        | [ id; workflow' ] -> (
+            match String.split_on_char '}' workflow' with
+            | [ workflow; "" ] -> Hashtbl.replace sys id (Workflow.of_string2 workflow)
+            | _ -> invalid_arg __FUNCTION__)
+        | _ -> invalid_arg __FUNCTION__);
+        sys)
+      (Hashtbl.create 100)
+    % Seq.take_while (( <> ) "")
 end
 
 let () =
   let lines = input_lines stdin in
   let sys = System.of_lines lines in
-  (* TODO *)
-  ignore sys
+  let accept = System.eval sys in
+  print_endline (string_of_int (Part.Set.cardinal accept ))

b1387857535bb8dd373fe20d077028404a160ba7
Anton Yabchinskiy <arn@bestmx.net>
2023-12-25 14:10:10 +0400

    bin/day_18_2: make polygon CCW, interior_area + boundary_area

--- 2023/bin/day_18_2.ml
+++ 2023/bin/day_18_2.ml
@@ -2,7 +2,5 @@ open Advent
 open Day_18
 
 let () =
-  (* FIXME: Must include interior_area and boundary_area. As for now
-     interior_area erroneously includes boundary area in the result. *)
-  Plan.of_lines2 (input_lines stdin)
-  |> Plan.to_list_pos |> Polygon.interior_area |> string_of_int |> print_endline
+  let points = Plan.of_lines2 (input_lines stdin) |> Plan.to_list_pos |> List.rev in
+  print_endline (string_of_int Polygon.(interior_area points + boundary_area points))

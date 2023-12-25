55027f7af3b3bc4ea8bfa02b57ace6a12dbf3087
Anton Yabchinskiy <arn@bestmx.net>
2023-12-25 17:34:25 +0400

    lib/string: ?pos and ?len args for find_index, remove other find functions

--- 2023/lib/string.mli
+++ 2023/lib/string.mli
@@ -1,8 +1,5 @@
 include module type of Stdlib.String
 
-val find_index : string -> (char -> bool) -> int
-val find_index_from : string -> int -> (char -> bool) -> int
-val rfind_index : string -> (char -> bool) -> int
-val rfind_index_from : string -> int -> (char -> bool) -> int
+val find_index : ?pos:int -> ?len:int -> string -> (char -> bool) -> int option
 val split_on_chars : char list -> string -> string list
 val to_list : string -> char list

50d6f6ae09d7467d7e4c5169e338e0a4285bfe2b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-25 17:23:12 +0400

    lib/string: add find_index functions

--- 2023/lib/string.ml
+++ 2023/lib/string.ml
@@ -1,5 +1,17 @@
 include Stdlib.String
 
+let find_index_from s i pred =
+  let n = length s in
+  let rec loop i = if i = n then raise Not_found else if pred (get s i) then i else loop (i + 1) in
+  loop i
+
+let rfind_index_from s i pred =
+  let rec loop i = if i = -1 then raise Not_found else if pred (get s i) then i else loop (i - 1) in
+  loop i
+
+let find_index s = find_index_from s 0
+let rfind_index s = rfind_index_from s (length s - 1)
+
 let rec split_on_chars sep s =
   match sep with
   | [] -> [ s ]
--- 2023/lib/string.mli
+++ 2023/lib/string.mli
@@ -1,4 +1,8 @@
 include module type of Stdlib.String
 
+val find_index : string -> (char -> bool) -> int
+val find_index_from : string -> int -> (char -> bool) -> int
+val rfind_index : string -> (char -> bool) -> int
+val rfind_index_from : string -> int -> (char -> bool) -> int
 val split_on_chars : char list -> string -> string list
 val to_list : string -> char list

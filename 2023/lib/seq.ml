d1812f1218142daf7c58f23566c30ef28666d026
Anton Yabchinskiy <arn@bestmx.net>
2023-12-26 13:54:12 +0400

    lib/seq: add symmetric

--- 2023/lib/seq.ml
+++ 2023/lib/seq.ml
@@ -1,6 +1,10 @@
 include Stdlib.Seq
 
 let reduce f s =
-  match uncons s with
-  | Some (x0, xs) -> fold_left f x0 xs
-  | None -> invalid_arg __FUNCTION__
+  match uncons s with Some (x0, xs) -> fold_left f x0 xs | None -> invalid_arg __FUNCTION__
+
+let symmetric pos len =
+  if len < 0 then invalid_arg __FUNCTION__;
+  if pos < 0 || pos >= len then invalid_arg __FUNCTION__;
+  let mid = len / 2 in
+  unfold (fun i -> if i < mid then Some ((pos + i, pos + len - 1 - i), i + 1) else None) 0
--- 2023/lib/seq.mli
+++ 2023/lib/seq.mli
@@ -1,3 +1,8 @@
 include module type of Stdlib.Seq
 
 val reduce : ('a -> 'a -> 'a) -> 'a t -> 'a
+
+val symmetric : int -> int -> (int * int) t
+(** [symmetric pos len] generates [len / 2] pairs of symmetric
+    indicies.  E.g., [symmetric 10 6] will produce pairs
+    [(10, 15); (11, 14); (12, 13)]. *)

a2bcd09d7a1e846b7008144929162b5384a1e4c5
Anton Yabchinskiy <arn@bestmx.net>
2023-12-26 14:34:46 +0400

    bin/day_13: implememt reflection2

--- 2023/bin/day_13.ml
+++ 2023/bin/day_13.ml
@@ -29,17 +29,23 @@ end = struct
         | None -> None)
 
   let reflection2 notes =
+    let aux matrix =
+      let n_rows = Array.length matrix in
+      Seq.find
+        (fun (row, len) ->
+          let total_dist =
+            Seq.Symmetric.ints row len
+            |> Seq.map (fun (i, j) -> Array.hamming_dist Stdlib.( = ) matrix.(i) matrix.(j))
+            |> Seq.reduce ( + )
+          in
+          total_dist < 2)
+        (Seq.Symmetric.windows n_rows)
+      |> Option.map (fun (row, len) -> row + (len / 2))
+    in
     match Array.symmetry (Array.equal ( = )) notes with
     | Some _ -> (
-        (* TODO: Allow one mismatch in equal *)
-        match Array.symmetry (Array.equal ( = )) (Array.transpose notes) with
-        | Some k -> Some (`Vert k)
-        | None -> None)
-    | None -> (
-        (* TODO: Allow one mismatch in equal *)
-        match Array.symmetry (Array.equal ( = )) notes with
-        | Some k -> Some (`Horiz k)
-        | None -> None)
+        match aux (Array.transpose notes) with Some col -> Some (`Vert col) | None -> None)
+    | None -> ( match aux notes with Some row -> Some (`Horiz row) | None -> None)
 end
 
 let input chan =

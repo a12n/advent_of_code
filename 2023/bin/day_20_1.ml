37b6d89cfb33f57c9cf976d3c35cfd0335e7e9c2
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 16:43:30 +0400

    bin/day_20_1: Seq.ints

--- 2023/bin/day_20_1.ml
+++ 2023/bin/day_20_1.ml
@@ -5,8 +5,8 @@ let () =
   let cfg = Config.of_lines (input_lines stdin) in
   Config.pp Format.err_formatter cfg;
   let Stats.{ low; high } =
-    Seq.(forever Fun.id)
+    Seq.(ints 1)
     |> Seq.take 1000
-    |> Seq.fold_left (fun stats () -> Stats.add stats (Config.push_button cfg)) Stats.zero
+    |> Seq.fold_left (fun stats _i -> Stats.add stats (Config.push_button cfg)) Stats.zero
   in
   print_endline (string_of_int (low * high))

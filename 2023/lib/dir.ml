cd000693e5d40d62e7745185e15cdb35f539eff4
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 00:11:21 +0300

    lib/dir: add turn_left and turn_right functions

--- 2023/lib/dir.ml
+++ 2023/lib/dir.ml
@@ -2,6 +2,8 @@ type t = Up | Left | Right | Down
 
 let neg = function Up -> Down | Left -> Right | Right -> Left | Down -> Up
 let to_char = function Up -> '^' | Left -> '<' | Right -> '>' | Down -> 'v'
+
+(* TODO: Move to Pos.of_dir *)
 let to_pos = function Up -> (-1, 0) | Left -> (0, -1) | Right -> (0, 1) | Down -> (1, 0)
 let to_string = function Up -> "↑" | Left -> "←" | Down -> "↓" | Right -> "→"
 
@@ -11,3 +13,6 @@ let of_pos = function
   | n, 0 when n < 0 -> Up
   | n, 0 when n > 0 -> Down
   | _, _ -> invalid_arg __FUNCTION__
+
+let turn_left = function Up -> Left | Left -> Down | Down -> Right | Right -> Up
+let turn_right = function Up -> Right | Left -> Up | Down -> Left | Right -> Down

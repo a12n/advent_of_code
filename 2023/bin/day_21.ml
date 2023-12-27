b726cef481be7a5cbd8b2e7a295fcf37af0d5d8b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 17:23:33 +0400

    bin/day_21: add Grid module

---
+++ 2023/bin/day_21.ml
@@ -0,0 +1,13 @@
+open Advent
+
+module Grid = struct
+  type t = char array array
+
+  let start =
+    Array.find_mapi (fun row line ->
+        Option.map
+          (fun col -> (row, col))
+          (Array.find_mapi (fun col plot -> if plot = 'S' then Some col else None) line))
+
+  let of_lines = Array.of_seq % Seq.map (Array.of_seq % String.to_seq)
+end

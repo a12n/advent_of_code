1a6d9a822726fa9d4ef5ecf011cb84d0c180049f
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 19:23:41 +0400

    bin/day_22_1: print format

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -2,4 +2,6 @@ open Day_22
 
 let () =
   let bricks = input stdin in
-  Format.(pp_print_list ~pp_sep:pp_print_space Brick.pp err_formatter bricks)
+  Format.(
+    pp_print_list ~pp_sep:pp_print_newline Brick.pp err_formatter bricks;
+    pp_print_newline err_formatter ())

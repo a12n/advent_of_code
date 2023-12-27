f12fdd71b14519d156d639842ea0e790cddaddff
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 19:55:43 +0400

    bin/day_22_1: print grid span

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -5,7 +5,10 @@ let () =
   let bricks = Snapshot.of_lines (input_lines stdin) in
   Format.(
     pp_print_string err_formatter "Input:\n";
-    Snapshot.pp err_formatter bricks);
+    Snapshot.pp err_formatter bricks;
+    pp_print_string err_formatter "Grid Span:\n";
+    Brick.pp err_formatter (Snapshot.grid_span bricks);
+    pp_print_newline err_formatter ());
   let bricks = Snapshot.sort bricks in
   Format.(
     pp_print_string err_formatter "Sorted:\n";

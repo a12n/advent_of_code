e4a0c89fe878b725f8501b3b657267ce902a09ba
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 18:14:54 +0400

    bin/day_21: add Plot module

--- 2023/bin/day_21.ml
+++ 2023/bin/day_21.ml
@@ -1,25 +1,31 @@
 open Advent
 
+module Plot = struct
+  type t = Garden | Rock | Start
+
+  let of_char = function
+    | '.' -> Garden
+    | '#' -> Rock
+    | 'S' -> Start
+    | _ -> invalid_arg __FUNCTION__
+
+  let to_string = function Garden -> "." | Rock -> "▓" | Start -> "@"
+end
+
 module Garden = struct
-  type t = char array array
+  type t = Plot.t array array
 
   let start =
     Array.find_mapi (fun row line ->
         Option.map
           (fun col -> (row, col))
-          (Array.find_mapi (fun col plot -> if plot = 'S' then Some col else None) line))
+          (Array.find_mapi (fun col plot -> if plot = Plot.Start then Some col else None) line))
 
-  let of_lines = Array.of_seq % Seq.map (Array.of_seq % String.to_seq)
+  let of_lines = Array.of_seq % Seq.map (Array.of_seq % Seq.map Plot.of_char % String.to_seq)
 
   let pp fmt =
     Format.(
       Array.iter (fun line ->
-          Array.iter
-            (pp_print_string fmt % function
-             | '.' -> "."
-             | '#' -> "▓"
-             | 'S' -> "@"
-             | _ -> failwith __FUNCTION__)
-            line;
+          Array.iter (pp_print_string fmt % Plot.to_string) line;
           pp_print_newline fmt ()))
 end

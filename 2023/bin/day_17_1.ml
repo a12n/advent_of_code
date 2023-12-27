04d2e4cc55fec9932d1920760ae7445d084704e3
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 00:12:36 +0300

    bin/day_17_1: use Grid.heat_loss

--- 2023/bin/day_17_1.ml
+++ 2023/bin/day_17_1.ml
@@ -4,6 +4,8 @@ open Day_17
 let () =
   let grid = Grid.of_lines (input_lines stdin) in
   let n_rows, n_cols = Grid.size grid in
-  let loss, path = Option.get (Grid.path grid (0, 0) (n_rows - 1, n_cols - 1)) in
+  (* let loss, path = Option.get (Grid.path grid (0, 0) (n_rows - 1, n_cols - 1)) in *)
+  let loss = Grid.heat_loss grid (0, 0) (n_rows - 1, n_cols - 1) in
+  let path = [] in
   Grid.pp ~path Format.err_formatter grid;
   print_endline (string_of_int loss)

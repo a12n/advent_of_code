13c2473247c6ebdda137209ccf3339f1807b7e07
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 16:50:53 +0400

    bin/day_20_2: try to find "rx"

--- 2023/bin/day_20_2.ml
+++ 2023/bin/day_20_2.ml
@@ -4,8 +4,7 @@ open Day_20
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
   Seq.(ints 1)
-  |> Seq.find (fun n ->
-         ignore (Config.push_button cfg);
-         ignore n;
-         false)
+  |> Seq.find (fun _i ->
+         let found, _ = Config.push_button cfg in
+         found)
   |> Option.get |> string_of_int |> print_endline

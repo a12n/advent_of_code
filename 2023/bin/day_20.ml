8138f8ab6c8b6614bad34287d7ee7a2b81bd2d8b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 14:26:25 +0400

    bin/day_20: WIP

--- 2023/bin/day_20.ml
+++ 2023/bin/day_20.ml
@@ -32,19 +32,88 @@ module Propagator = struct
 end
 
 module Config = struct
-  type t = unit
+  type t = {
+    outputs : (string, (string * int) list) Hashtbl.t;
+    flip_flops : (string, bool) Hashtbl.t;
+    conjunctions : (string, Pulse.t array) Hashtbl.t;
+  }
 
-  let make () = ()
+  let pp fmt { outputs; flip_flops; conjunctions } =
+    Format.(
+      pp_print_string fmt "outputs:\n";
+      Hashtbl.iter
+        (fun name dests ->
+          List.iter (fun (dest, input) -> fprintf fmt "\t%s -> %s %d\n" name dest input) dests)
+        outputs;
+      pp_print_string fmt "flip_flops:\n";
+      Hashtbl.iter (fun name state -> fprintf fmt "\t%s: %B\n" name state) flip_flops;
+      pp_print_string fmt "conjunctions:\n";
+      Hashtbl.iter
+        (fun name state ->
+          fprintf fmt "\t%s:" name;
+          Array.iter (fprintf fmt " %s" % Pulse.to_string) state;
+          pp_print_newline fmt ())
+        conjunctions)
 
-  let of_lines =
-    (* String right of "->" creates numbered and named connection nodes. E.g.,
-       broadcaster -> a, b, c
+  let make () =
+    {
+      outputs = Hashtbl.create 100;
+      flip_flops = Hashtbl.create 100;
+      conjunctions = Hashtbl.create 100;
+    }
 
-       Creates nodes a-0, b-0, c-0.
-    *)
-    Seq.fold_left (fun cfg line ->
-        (* TODO *)
-        ignore line;
-        cfg
-      ) (make ())
+  let of_lines lines =
+    let ({ outputs; flip_flops; conjunctions; _ } as cfg) = make () in
+    let num_inputs = Hashtbl.create 100 in
+    Seq.iter
+      (fun line ->
+        match Str.(split (regexp "[ ]*->[ ]*") line) with
+        | [ modul; dests ] -> (
+            let mod_type, mod_name =
+              match Str.string_before modul 1 with
+              | "%" -> ('%', Str.string_after modul 1)
+              | "&" -> ('&', Str.string_after modul 1)
+              | _ -> ('>', modul)
+            in
+            let dests =
+              List.map
+                (fun dest ->
+                  let i =
+                    match Hashtbl.find_opt num_inputs dest with
+                    | Some i ->
+                        Hashtbl.replace num_inputs dest (i + 1);
+                        i
+                    | None ->
+                        Hashtbl.replace num_inputs dest 1;
+                        0
+                  in
+                  (dest, i))
+                Str.(split (regexp "[ ]*,[ ]*") dests)
+            in
+            Hashtbl.replace outputs mod_name dests;
+            match mod_type with
+            | '%' -> Hashtbl.replace flip_flops mod_name false
+            | '&' -> Hashtbl.replace conjunctions mod_name [||]
+            | '>' -> ()
+            | _ -> failwith __FUNCTION__)
+        | _ -> invalid_arg __FUNCTION__)
+      lines;
+    (* No additional inputs in flip-flops and broadcasters. *)
+    Hashtbl.iter
+      (fun name dests ->
+        let dests =
+          List.map
+            (fun (dest, input) ->
+              if Hashtbl.mem conjunctions dest then (dest, input) else (dest, 0))
+            dests
+        in
+        Hashtbl.replace outputs name dests)
+      outputs;
+    (* Allocate conjunctions inputs. *)
+    Hashtbl.iter
+      (fun name _ ->
+        Hashtbl.replace conjunctions name (Array.make (Hashtbl.find num_inputs name) Pulse.Low))
+      conjunctions;
+    (* Done. *)
+    cfg
 end

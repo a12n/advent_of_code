29f5bfc0e9fff3c4a5eca31db44036b53361ba27
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 02:07:38 +0300

    bin/day_17_2: WIP

---
+++ 2023/bin/day_17_2.ml
@@ -0,0 +1,10 @@
+open Advent
+open Day_17
+
+let () =
+  let grid = Heat_Grid.of_lines (input_lines stdin) in
+  let n_rows, n_cols = Heat_Grid.size grid in
+  let loss =
+    Heat_Grid.heat_loss ~min_straight:4 ~max_straight:10 grid (0, 0) (n_rows - 1, n_cols - 1)
+  in
+  print_endline (string_of_int loss)

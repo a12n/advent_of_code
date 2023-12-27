cb02ec6bf3d0042de80502ace9914bd6b17b24d6
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 15:59:05 +0400

    bin/day_20_1: run 1000 times

--- 2023/bin/day_20_1.ml
+++ 2023/bin/day_20_1.ml
@@ -4,5 +4,9 @@ open Day_20
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
   Config.pp Format.err_formatter cfg;
-  let Stats.{ low; high } = Config.push_button cfg in
-  Printf.printf "low %d, high %d\n" low high
+  let Stats.{ low; high } =
+    Seq.(forever Fun.id)
+    |> Seq.take 1000
+    |> Seq.fold_left (fun stats () -> Stats.add stats (Config.push_button cfg)) Stats.zero
+  in
+  print_endline (string_of_int (low * high))

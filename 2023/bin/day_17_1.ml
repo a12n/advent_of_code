761f3e97a273108c1f27ff482e68f69c5673aac5
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 02:06:50 +0300

    bin/day_17_1: remove unused

--- 2023/bin/day_17_1.ml
+++ 2023/bin/day_17_1.ml
@@ -2,10 +2,7 @@ open Advent
 open Day_17
 
 let () =
-  let grid = Grid.of_lines (input_lines stdin) in
-  let n_rows, n_cols = Grid.size grid in
-  (* let loss, path = Option.get (Grid.path grid (0, 0) (n_rows - 1, n_cols - 1)) in *)
-  let loss = Grid.heat_loss grid (0, 0) (n_rows - 1, n_cols - 1) in
-  let path = [] in
-  Grid.pp ~path Format.err_formatter grid;
+  let grid = Heat_Grid.of_lines (input_lines stdin) in
+  let n_rows, n_cols = Heat_Grid.size grid in
+  let loss = Heat_Grid.heat_loss ~max_straight:3 grid (0, 0) (n_rows - 1, n_cols - 1) in
   print_endline (string_of_int loss)

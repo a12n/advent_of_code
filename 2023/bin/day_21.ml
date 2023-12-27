13c1521eb46dae96a0c51a2a8a55d91ab412fa4f
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 17:28:00 +0400

    bin/day_21: Garden.pp

--- 2023/bin/day_21.ml
+++ 2023/bin/day_21.ml
@@ -10,4 +10,14 @@ module Garden = struct
           (Array.find_mapi (fun col plot -> if plot = 'S' then Some col else None) line))
 
   let of_lines = Array.of_seq % Seq.map (Array.of_seq % String.to_seq)
+
+  let pp fmt =
+    Format.(
+      Array.iter
+        (Array.iter
+           (pp_print_string fmt % function
+            | '.' -> "░"
+            | '#' -> "█"
+            | 'S' -> "╳"
+            | _ -> failwith __FUNCTION__)))
 end

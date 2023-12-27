befb84eba19610e8f02784e6ac22c5b39cfff8b6
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 01:43:56 +0300

    lib/pos: alias for Grid.Pos

--- 2023/lib/pos.ml
+++ 2023/lib/pos.ml
@@ -1,9 +1,2 @@
-type t = int * int
-(** Position in a 2D grid. [(0, 0)] is the top-left corner and
-    [(n_rows - 1, n_cols - 1)] is the bottom-right corner. *)
-
-let add (row1, col1) (row2, col2) = (row1 + row2, col1 + col2)
-let mul_int (row, col) n = (row * n, col * n)
-let sub (row1, col1) (row2, col2) = (row1 - row2, col1 - col2)
-let is_valid (n_rows, n_cols) (row, col) = row >= 0 && col >= 0 && row < n_rows && col < n_cols
-let pp fmt (row, col) = Format.fprintf fmt "(%d, %d)" row col
+(** Use {!Grid.Pos}. *)
+include Grid.Pos

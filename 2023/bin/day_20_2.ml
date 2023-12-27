9dfd62a9f7d83e3f58b62306d9a2563cdddbcca5
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 17:02:24 +0400

    bin/day_20_2: pp_state

--- 2023/bin/day_20_2.ml
+++ 2023/bin/day_20_2.ml
@@ -4,7 +4,12 @@ open Day_20
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
   Seq.(ints 1)
-  |> Seq.find (fun _i ->
+  |> Seq.find (fun i ->
+         Format.(
+           pp_print_int err_formatter i;
+           pp_print_char err_formatter ':';
+           pp_print_newline err_formatter ();
+           Config.pp_state err_formatter cfg);
          let found, _ = Config.push_button cfg in
          found)
   |> Option.get |> string_of_int |> print_endline

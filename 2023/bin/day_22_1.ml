a9895fc65f878c72966f9b8e0cf42249a727b8ed
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 19:37:06 +0400

    bin/day_22_1: print sorted

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -1,7 +1,12 @@
+open Advent
 open Day_22
 
 let () =
   let bricks = Snapshot.of_lines (input_lines stdin) in
   Format.(
     pp_print_string err_formatter "Input:\n";
+    Snapshot.pp err_formatter bricks);
+  let bricks = Snapshot.sort bricks in
+  Format.(
+    pp_print_string err_formatter "Sorted:\n";
     Snapshot.pp err_formatter bricks)

67fac5d081a2f543998366eef6d82bd918c7a477
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 14:58:57 +0400

    bin/day_20: remove unused

--- 2023/bin/day_20.ml
+++ 2023/bin/day_20.ml
@@ -10,31 +10,6 @@ module Pulse = struct
   let to_string = function Low -> "L" | High -> "H"
 end
 
-module Flip_Flop = struct
-  type t = bool
-
-  let initial = false
-
-  let eval ff = function
-    | Pulse.High -> (ff, [])
-    | Pulse.Low when ff -> (false, [ Pulse.Low ])
-    | Pulse.Low -> (true, [ Pulse.High ])
-end
-
-module Conjunction = struct
-  type t = Pulse.t array
-
-  let make n = Array.make n Pulse.Low
-end
-
-module Propagator = struct
-  type t = (Pulse.t * string * string list) Queue.t
-
-  let button queue =
-    Queue.add (Pulse.Low, "button", [ "broadcaster" ]) queue;
-    queue
-end
-
 module Config = struct
   type t = {
     outputs : (string, (string * int) list) Hashtbl.t;

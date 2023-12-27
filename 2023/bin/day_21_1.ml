9e9f61fe2661a3d8a29b2e7e8ed9d4c81b414097
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 18:25:51 +0400

    bin/day_21_1: print grid with highlights

--- 2023/bin/day_21_1.ml
+++ 2023/bin/day_21_1.ml
@@ -3,4 +3,5 @@ open Day_21
 
 let () =
   let garden = Garden.of_lines (input_lines stdin) in
-  Garden.pp Format.err_formatter garden
+  let reachable = Garden.steps garden (Option.get (Garden.start garden)) 64 in
+  Garden.pp ~highlight:reachable Format.err_formatter garden

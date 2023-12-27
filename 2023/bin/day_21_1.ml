34d75376c19e6753f57ef6967473c1de67bb494c
Anton Yabchinskiy <arn@bestmx.net>
2023-12-27 18:28:59 +0400

    bin/day_21_1: print number of reachable positions

--- 2023/bin/day_21_1.ml
+++ 2023/bin/day_21_1.ml
@@ -4,4 +4,5 @@ open Day_21
 let () =
   let garden = Garden.of_lines (input_lines stdin) in
   let reachable = Garden.steps garden (Option.get (Garden.start garden)) 64 in
-  Garden.pp ~highlight:reachable Format.err_formatter garden
+  Garden.pp ~highlight:reachable Format.err_formatter garden;
+  print_endline (string_of_int (List.length reachable))

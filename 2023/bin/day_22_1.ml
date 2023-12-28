e3fe1d6d2a8e1fdbd96dfe9f1e35c76b7cab4c4b
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 21:45:27 +0300

    bin/day_22_1: print max point of span instead of grid size

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -3,8 +3,8 @@ open Day_22
 
 let () =
   let bricks = Snapshot.of_lines (input_lines stdin) in
-  let nx, ny, nz = Snapshot.grid_size bricks in
-  Printf.printf "%d %d %d\n" nx ny nz;
+  let _, Point.{ x = nx; y = ny; z = nz } = Snapshot.grid_span bricks in
+  Printf.printf "%d %d %d\n" (nx + 1) (ny + 1) (nz + 1);
   let bricks = Snapshot.sort bricks in
   let bricks, _ = Snapshot.settle bricks in
   List.iter

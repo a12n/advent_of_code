e7d3de4672bd2ff0b515286df0a51378001a574c
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 16:25:16 +0400

    bin/day_19: remove unused, adjust Part.Set.pp

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -42,8 +42,8 @@ module Part = struct
     let empty = { x = Int_Set.empty; m = Int_Set.empty; a = Int_Set.empty; s = Int_Set.empty }
 
     let full =
-      let x = Int_Set.of_seq Seq.(take 4000 (ints 1)) in
-      { x; m = x; a = x; s = x }
+      let s = Int_Set.of_seq Seq.(take 4000 (ints 1)) in
+      { x = s; m = s; a = s; s }
 
     let union s1 s2 =
       {
@@ -80,67 +80,18 @@ module Part = struct
           pp_print_int fmt prev));
       Format.pp_print_string fmt " }"
 
-    let pp fmt { x; m; a; s } =
-      Format.(
-        pp_print_string fmt "{";
-        pp_set fmt x;
-        pp_print_string fmt "; ";
-        pp_set fmt m;
-        pp_print_string fmt "; ";
-        pp_set fmt a;
-        pp_print_string fmt "; ";
-        pp_set fmt s;
-        pp_print_string fmt "} ";
-        pp_print_int fmt (cardinal { x; m; a; s }))
-  end
-
-  module Range = struct
-    type t = { x : Segment.t; m : Segment.t; a : Segment.t; s : Segment.t }
-
-    let get = function
-      | 'x' -> fun { x; _ } -> x
-      | 'm' -> fun { m; _ } -> m
-      | 'a' -> fun { a; _ } -> a
-      | 's' -> fun { s; _ } -> s
-      | _ -> invalid_arg __FUNCTION__
-
-    let set = function
-      | 'x' -> fun p x -> { p with x }
-      | 'm' -> fun p m -> { p with m }
-      | 'a' -> fun p a -> { p with a }
-      | 's' -> fun p s -> { p with s }
-      | _ -> invalid_arg __FUNCTION__
-
-    let empty =
-      let s = Segment.empty in
-      { x = s; m = s; a = s; s }
-
-    let full =
-      let s = Segment.of_endpoints 1 4000 in
-      { x = s; m = s; a = s; s }
-
-    let union p1 p2 =
-      {
-        x = Segment.union p1.x p2.x;
-        m = Segment.union p1.m p2.m;
-        a = Segment.union p1.a p2.a;
-        s = Segment.union p1.s p2.s;
-      }
-
-    let cardinal { x; m; a; s } = Segment.(length x * length m * length a * length s)
-
-    let pp fmt ({ x; m; a; s } as r) =
+    let pp fmt ({ x; m; a; s } as set) =
       Format.(
         pp_print_string fmt "{x=";
-        Segment.pp fmt x;
+        pp_set fmt x;
         pp_print_string fmt "; m=";
-        Segment.pp fmt m;
+        pp_set fmt m;
         pp_print_string fmt "; a=";
-        Segment.pp fmt a;
+        pp_set fmt a;
         pp_print_string fmt "; s=";
-        Segment.pp fmt s;
+        pp_set fmt s;
         pp_print_string fmt "; n=";
-        pp_print_int fmt (cardinal r);
+        pp_print_int fmt (cardinal set);
         pp_print_string fmt "}")
   end
 end

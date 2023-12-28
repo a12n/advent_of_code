344a09e521fabeb3f46a041b69cb1d0372eff004
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 13:45:27 +0400

    bin/day_19: rewrite Part.Range with Segment

--- 2023/bin/day_19.ml
+++ 2023/bin/day_19.ml
@@ -1,3 +1,5 @@
+open Advent
+
 module Part = struct
   type t = { x : int; m : int; a : int; s : int }
   and part = t
@@ -116,10 +118,59 @@ module Part = struct
   end
 
   module Range = struct
-    type t = part * part
+    type t = { x : Segment.t; m : Segment.t; a : Segment.t; s : Segment.t }
 
-    let empty = ({ x = 1; m = 1; a = 1; s = 1 }, { x = 0; m = 0; a = 0; s = 0 })
-    let full = ({ x = 1; m = 1; a = 1; s = 1 }, { x = 4000; m = 4000; a = 4000; s = 4000 })
-    let inter (min1, max1) (min2, max2) = (map2_max min1 min2, map2_min max1 max2)
+    let get = function
+      | 'x' -> fun { x; _ } -> x
+      | 'm' -> fun { m; _ } -> m
+      | 'a' -> fun { a; _ } -> a
+      | 's' -> fun { s; _ } -> s
+      | _ -> invalid_arg __FUNCTION__
+
+    let set = function
+      | 'x' -> fun p x -> { p with x }
+      | 'm' -> fun p m -> { p with m }
+      | 'a' -> fun p a -> { p with a }
+      | 's' -> fun p s -> { p with s }
+      | _ -> invalid_arg __FUNCTION__
+
+    let empty =
+      let s = Segment.empty in
+      { x = s; m = s; a = s; s }
+
+    let full =
+      let s = Segment.of_endpoints 1 4000 in
+      { x = s; m = s; a = s; s }
+
+    let inter p1 p2 =
+      {
+        x = Segment.inter p1.x p2.x;
+        m = Segment.inter p1.m p2.m;
+        a = Segment.inter p1.a p2.a;
+        s = Segment.inter p1.s p2.s;
+      }
+
+    let union p1 p2 =
+      {
+        x = Segment.union p1.x p2.x;
+        m = Segment.union p1.m p2.m;
+        a = Segment.union p1.a p2.a;
+        s = Segment.union p1.s p2.s;
+      }
+
+    let cardinal { x; m; a; s } = Segment.(length x * length m * length a * length s)
+
+    let pp fmt ({ x; m; a; s } as r) =
+      Format.(
+        pp_print_string fmt "|x ";
+        Segment.pp fmt x;
+        pp_print_string fmt " m ";
+        Segment.pp fmt m;
+        pp_print_string fmt " a ";
+        Segment.pp fmt a;
+        pp_print_string fmt " s ";
+        Segment.pp fmt s;
+        pp_print_string fmt "| = ";
+        pp_print_int fmt (cardinal r))
   end
 end

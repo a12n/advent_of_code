95b48683619155a0231d263edfd5c88ce513cbae
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 21:45:04 +0300

    bin/day_22_1: call Snapshot.settle

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -6,6 +6,7 @@ let () =
   let nx, ny, nz = Snapshot.grid_size bricks in
   Printf.printf "%d %d %d\n" nx ny nz;
   let bricks = Snapshot.sort bricks in
+  let bricks, _ = Snapshot.settle bricks in
   List.iter
     (fun (Point.{ x = x1; y = y1; z = z1 }, Point.{ x = x2; y = y2; z = z2 }) ->
       Printf.printf "%d %d %d %d %d %d\n" x1 y1 z1 x2 y2 z2)

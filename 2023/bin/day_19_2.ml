845c330e0b656931ee5344a25d814b07a2aecc12
Anton Yabchinskiy <arn@bestmx.net>
2023-12-28 16:44:32 +0400

    bin/day_19_2: no special variants for Accept/Reject, handle "R" and "A"

--- 2023/bin/day_19_2.ml
+++ 2023/bin/day_19_2.ml
@@ -2,23 +2,19 @@ open Day_19
 open Advent
 
 module Rule : sig
-  type t = Part.Set.t -> ([ `Accept | `Reject | `Send of string ] * Part.Set.t) * Part.Set.t
+  type t = Part.Set.t -> (string * Part.Set.t) * Part.Set.t
 
   val of_string : string -> t
 end = struct
-  type t = Part.Set.t -> ([ `Accept | `Reject | `Send of string ] * Part.Set.t) * Part.Set.t
+  type t = Part.Set.t -> (string * Part.Set.t) * Part.Set.t
 
   let of_string s =
-    let action_of_string = function "A" -> `Accept | "R" -> `Reject | name -> `Send name in
     match String.split_on_char ':' s |> List.filter (( <> ) "") with
-    | [ action ] ->
-        let action = action_of_string action in
-        fun parts -> ((action, parts), Part.Set.empty)
+    | [ action ] -> fun parts -> ((action, parts), Part.Set.empty)
     | [ cond; action ] ->
         let get = Part.Set.get cond.[0] in
         let set = Part.Set.set cond.[0] in
         let value = int_of_string String.(sub cond 2 (length cond - 2)) in
-        let action = action_of_string action in
         let pred =
           match cond.[1] with
           | '>' -> fun n -> n > value
@@ -34,22 +30,20 @@ end
 module Workflow : sig
   type t = Rule.t list
 
-  val eval : t -> Part.Set.t -> (string * Part.Set.t) list * Part.Set.t
+  val eval : t -> Part.Set.t -> (string * Part.Set.t) list
   val of_string : string -> t
 end = struct
   type t = Rule.t list
 
   let eval wf parts =
-    let send, accept, _ =
+    let send, _ =
       List.fold_left
-        (fun (send, accept, parts) rule ->
-          match rule parts with
-          | (`Accept, a), pass -> (send, Part.Set.union accept a, pass)
-          | (`Reject, _), pass -> (send, accept, pass)
-          | (`Send id, s), pass -> ((id, s) :: send, accept, pass))
-        ([], Part.Set.empty, parts) wf
+        (fun (send, parts) rule ->
+          let (id, s), pass = rule parts in
+          ((id, s) :: send, pass))
+        ([], parts) wf
     in
-    (List.rev send, accept)
+    List.rev send
 
   let of_string = List.map Rule.of_string % String.split_on_char ','
 end
@@ -57,36 +51,39 @@ end
 module System : sig
   type t
 
-  (* Returns accepted part range. *)
-  val eval : t -> string -> Part.Set.t -> int
+  val eval : t -> int
   val of_lines : string Seq.t -> t
 end = struct
-  type t = (string, Workflow.t ) Hashtbl.t
+  type t = (string, Workflow.t) Hashtbl.t
 
-  let rec eval sys id parts =
-    let send, accept = Workflow.eval (Hashtbl.find sys id) parts in
-    Format.(
-      fprintf err_formatter "eval \"%s\": " id;
-      Part.Set.pp err_formatter parts;
-      pp_print_string err_formatter " ->\n";
-      List.iter
-        (fun (id, parts) ->
-          fprintf err_formatter "- send \"%s\" " id;
-          Part.Set.pp err_formatter parts;
-          pp_print_newline err_formatter ())
-        send;
-      pp_print_string err_formatter "- accept ";
-      Part.Set.pp err_formatter accept;
-      pp_print_newline err_formatter ());
-    let accept = Part.Set.cardinal accept in
-    let accept =
-      List.fold_left (fun accept (id, parts) -> accept + eval sys id parts) accept send
+  let eval sys =
+    let rec loop id parts =
+      let send = Workflow.eval (Hashtbl.find sys id) parts in
+      Format.(
+        fprintf err_formatter "eval \"%s\": " id;
+        Part.Set.pp err_formatter parts;
+        pp_print_string err_formatter " ->\n";
+        List.iter
+          (fun (id, parts) ->
+            fprintf err_formatter "- send \"%s\" " id;
+            Part.Set.pp err_formatter parts;
+            pp_print_newline err_formatter ())
+          send;
+        pp_print_newline err_formatter ());
+      let accepted =
+        List.fold_left
+          (fun accepted (id, parts) ->
+            accepted
+            + match id with "A" -> Part.Set.cardinal parts | "R" -> 0 | _ -> loop id parts)
+          0 send
+      in
+      Format.(
+        fprintf err_formatter "accept after \"%s\": " id;
+        pp_print_int err_formatter accepted;
+        pp_print_newline err_formatter ());
+      accepted
     in
-    Format.(
-      fprintf err_formatter "accept after \"%s\": " id;
-      pp_print_int err_formatter accept;
-      pp_print_newline err_formatter ());
-    accept
+    loop "in" Part.Set.full
 
   let of_lines =
     Seq.fold_left
@@ -105,5 +102,5 @@ end
 let () =
   let lines = input_lines stdin in
   let sys = System.of_lines lines in
-  let accept = System.eval sys "in" Part.Set.full in
-  print_endline (string_of_int accept)
+  let accepted = System.eval sys in
+  print_endline (string_of_int accepted)

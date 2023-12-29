9e489abcfdbb5d42d43474c509604fb79e6fa23d
Anton Yabchinskiy <arn@bestmx.net>
2023-12-29 17:34:37 +0400

    bin/day_20_2: update for changes in Stats

--- 2023/bin/day_20_2.ml
+++ 2023/bin/day_20_2.ml
@@ -3,6 +3,7 @@ open Day_20
 
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
+  let (Stats.{ low; _ } as stats) = Stats.make () in
   Config.pp Format.err_formatter cfg;
   Seq.(ints 1)
   |> Seq.find (fun i ->
@@ -11,6 +12,6 @@ let () =
            pp_print_char err_formatter ':';
            pp_print_newline err_formatter ();
            Config.pp_state err_formatter cfg);
-         let found, _ = Config.push_button cfg in
-         found)
+         Config.push_button cfg stats;
+         Hashtbl.mem low "rx")
   |> Option.get |> string_of_int |> print_endline

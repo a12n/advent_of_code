f505b92e33a986239ef53f019b2781b6e3af37cc
Anton Yabchinskiy <arn@bestmx.net>
2023-12-29 17:53:13 +0400

    bin/day_20_2: solution

--- 2023/bin/day_20_2.ml
+++ 2023/bin/day_20_2.ml
@@ -4,26 +4,25 @@ open Day_20
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
   let (Stats.{ low; _ } as stats) = Stats.make () in
-  Config.pp Format.err_formatter cfg;
-  Seq.(ints 1)
-  |> Seq.find (fun i ->
-         Stats.clear stats;
-
-         Config.push_button cfg stats;
-
-         (* "rx" is low when all "&zh" inputs are high. "&zh" inputs
-            are wired to "&jt", "&kb", "&sx" and "&ks" outputs. All these
-            conjunctions (inverters) must see low pulse to set "&zh" to
-            high, and "rx" to low. *)
-         let check_low name =
-           match Hashtbl.find_opt low name with
-           | Some n -> Printf.eprintf "%d: %s %d\n%!" i name n
-           | None -> ()
-         in
-         check_low "jt";
-         check_low "kb";
-         check_low "sx";
-         check_low "ks";
-
-         Hashtbl.mem low "rx")
-  |> Option.get |> string_of_int |> print_endline
+  (* "rx" is low when all "&zh" inputs are high. "&zh" inputs are
+     wired to "&jt", "&kb", "&sx" and "&ks" outputs. All these
+     conjunctions (inverters) must see low pulse to set "&zh" to high,
+     and "rx" to low. It happens with some different period for each
+     inverter. LCM of these periods is the answer. *)
+  let multiples =
+    List.map
+      (fun name ->
+        Config.reset cfg;
+        Stats.clear stats;
+        Seq.find
+          (fun i ->
+            Config.push_button cfg stats;
+            if Hashtbl.mem low name then (
+              Printf.eprintf "%d: low on %s\n%!" i name;
+              true)
+            else false)
+          (Seq.ints 1)
+        |> Option.get)
+      [ "jt"; "kb"; "sx"; "ks" ]
+  in
+  print_endline (string_of_int (List.reduce Int.lcm multiples))

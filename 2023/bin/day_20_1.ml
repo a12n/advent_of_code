390c42a480ae64b2e34dadfc355936abc9bdd418
Anton Yabchinskiy <arn@bestmx.net>
2023-12-29 17:28:42 +0400

    bin/day_20_1: update for changes in Stats

--- 2023/bin/day_20_1.ml
+++ 2023/bin/day_20_1.ml
@@ -3,14 +3,8 @@ open Day_20
 
 let () =
   let cfg = Config.of_lines (input_lines stdin) in
+  let stats = Stats.make () in
   Config.pp Format.err_formatter cfg;
-  let Stats.{ low; high } =
-    Seq.(ints 1)
-    |> Seq.take 1000
-    |> Seq.fold_left
-         (fun total _i ->
-           let _, stats = Config.push_button cfg in
-           Stats.add total stats)
-         Stats.zero
-  in
+  Seq.(ints 1) |> Seq.take 1000 |> Seq.iter (fun _i -> Config.push_button cfg stats);
+  let low, high = Stats.total stats in
   print_endline (string_of_int (low * high))

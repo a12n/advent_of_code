94bd72e5f8c3679e241b4fd0f2ec09f5218e98fd
Anton Yabchinskiy <arn@bestmx.net>
2023-12-29 17:14:37 +0400

    lib/hashtbl: add modify function

--- 2023/lib/hashtbl.ml
+++ 2023/lib/hashtbl.ml
@@ -1,5 +1,11 @@
 include Stdlib.Hashtbl
 
+let modify ?default tbl f key =
+  match (find_opt tbl key, default) with
+  | Some value, _ -> replace tbl key (f value)
+  | None, Some default -> replace tbl key default
+  | None, None -> raise Not_found
+
 module Ops = struct
   let ( .%{} ) = find
   let ( .%%{} ) = find_opt

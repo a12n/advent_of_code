509adade06cebec26bf975d9d8e515a53dc9f3ab
Anton Yabchinskiy <arn@bestmx.net>
2023-12-30 13:38:27 +0300

    bin/day_17_2: print state matrices with Grid.pp

--- 2023/bin/day_17_2.ml
+++ 2023/bin/day_17_2.ml
@@ -8,6 +8,14 @@ let () =
   let src, dest = ((0, 0), (n_rows - 1, n_cols - 1)) in
   let costs, dirs = Heat_Grid.distance grid src in
   let path = Heat_Grid.backtrack dirs dest in
-  Heat_Grid.pp_dist ~path Format.err_formatter grid costs dirs;
-  Heat_Grid.pp ~path Format.err_formatter grid;
+  Format.(
+    pp_print_string err_formatter "==== Heat-loss ====\n";
+    Grid.pp ~highlight:path (fun fmt cost -> fprintf fmt " %3d" cost) err_formatter costs;
+    pp_print_string err_formatter "==== Direction ====\n";
+    Grid.pp ~highlight:path
+      (fun fmt dir ->
+        fprintf fmt " %s" (match dir with None -> "  ." | Some dir -> "  " ^ Dir.to_string dir))
+      err_formatter dirs;
+    pp_print_string err_formatter "==== Grid ====\n";
+    Heat_Grid.pp ~path Format.err_formatter grid);
   print_endline (string_of_int costs.@(dest))

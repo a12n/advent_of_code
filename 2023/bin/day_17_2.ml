3d081cecb1e263a6df6aaa5df348879125146bf9
Anton Yabchinskiy <arn@bestmx.net>
2023-12-30 13:15:25 +0300

    bin/day_17_2: print grid with path and the final cost

--- 2023/bin/day_17_2.ml
+++ 2023/bin/day_17_2.ml
@@ -1,10 +1,13 @@
 open Advent
 open Day_17
+open Grid.Ops
 
 let () =
   let grid = Heat_Grid.of_lines (input_lines stdin) in
   let n_rows, n_cols = Heat_Grid.size grid in
-  let loss =
-    Heat_Grid.heat_loss ~min_straight:4 ~max_straight:10 grid (0, 0) (n_rows - 1, n_cols - 1)
-  in
-  print_endline (string_of_int loss)
+  let src, dest = ((0, 0), (n_rows - 1, n_cols - 1)) in
+  let costs, dirs = Heat_Grid.distance grid src in
+  let path = Heat_Grid.backtrack dirs dest in
+  Heat_Grid.pp_dist ~path Format.err_formatter grid costs dirs;
+  Heat_Grid.pp ~path Format.err_formatter grid;
+  print_endline (string_of_int costs.@(dest))

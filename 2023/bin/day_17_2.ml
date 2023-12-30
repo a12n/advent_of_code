7dd2cf35c64aa542a5f34c1f516f08bc18b47e18
Anton Yabchinskiy <arn@bestmx.net>
2023-12-30 13:41:05 +0300

    bin/day_17_2: print straight steps matrix

--- 2023/bin/day_17_2.ml
+++ 2023/bin/day_17_2.ml
@@ -6,7 +6,7 @@ let () =
   let grid = Heat_Grid.of_lines (input_lines stdin) in
   let n_rows, n_cols = Heat_Grid.size grid in
   let src, dest = ((0, 0), (n_rows - 1, n_cols - 1)) in
-  let costs, dirs = Heat_Grid.distance grid src in
+  let costs, dirs, straight = Heat_Grid.distance grid src in
   let path = Heat_Grid.backtrack dirs dest in
   Format.(
     pp_print_string err_formatter "==== Heat-loss ====\n";
@@ -16,6 +16,8 @@ let () =
       (fun fmt dir ->
         fprintf fmt " %s" (match dir with None -> "  ." | Some dir -> "  " ^ Dir.to_string dir))
       err_formatter dirs;
+    pp_print_string err_formatter "==== Num Steps ====\n";
+    Grid.pp ~highlight:path (fun fmt steps -> fprintf fmt " %3d" steps) err_formatter straight;
     pp_print_string err_formatter "==== Grid ====\n";
     Heat_Grid.pp ~path Format.err_formatter grid);
   print_endline (string_of_int costs.@(dest))

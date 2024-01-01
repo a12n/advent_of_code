b47326c6f8d22f84325be51d70aa421eec72a46d
Anton Yabchinskiy <arn@bestmx.net>
2024-01-01 14:07:40 +0300

    bin/day_21_2: print Garden.num_steps

--- 2023/bin/day_21_2.ml
+++ 2023/bin/day_21_2.ml
@@ -4,6 +4,8 @@ open Day_21
 let () =
   let garden = Garden.of_lines (input_lines stdin) in
   let n = Option.(value ~default:26501365 (map int_of_string (Sys.getenv_opt "N"))) in
+  let start = Option.get (Garden.start garden) in
   Format.(
     fprintf err_formatter "N = %d\n%!" n;
-    Garden.pp err_formatter garden)
+    Garden.pp err_formatter garden);
+  print_endline (string_of_int (Garden.num_steps garden start n))

42487bc93d693c19caec4f3cd8cb443b96ce9044
Anton Yabchinskiy <arn@bestmx.net>
2024-01-01 14:07:22 +0300

    bin/day_21_1: start binding

--- 2023/bin/day_21_1.ml
+++ 2023/bin/day_21_1.ml
@@ -4,7 +4,8 @@ open Day_21
 let () =
   let garden = Garden.of_lines (input_lines stdin) in
   let n = Option.(value ~default:64 (map int_of_string (Sys.getenv_opt "N"))) in
-  let reachable = Garden.steps garden (Option.get (Garden.start garden)) n in
+  let start = Option.get (Garden.start garden) in
+  let reachable = Garden.steps garden start n in
   Format.(
     fprintf err_formatter "N = %d\n%!" n;
     Garden.pp ~highlight:reachable err_formatter garden);

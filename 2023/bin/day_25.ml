6c56f08e21e3e1e673882f2378f050689c0132a4
Anton Yabchinskiy <arn@bestmx.net>
2024-01-04 02:57:13 +0300

    bin/day_25: Config module

--- 2023/bin/day_25.ml
+++ 2023/bin/day_25.ml
@@ -1,17 +1,27 @@
 open Advent
 
-let of_lines lines =
-  let conns = Hashtbl.create 1000 in
-  Seq.iter
-    (fun line ->
-      match String.split_on_char ':' line with
-      | [ a; bs ] ->
-          String.split_on_char ' ' bs |> List.map String.trim
-          |> List.filter (( <> ) "")
-          |> List.iter (fun b ->
-                 Hashtbl.(
-                   add conns a b;
-                   add conns b a))
-      | _ -> invalid_arg __FUNCTION__)
-    lines;
-  conns
+module Config = struct
+  type t = (string, string) Hashtbl.t
+
+  let add cfg a b = Hashtbl.add cfg (min a b) (max a b)
+  let mem cfg a b = Hashtbl.find_all cfg (min a b) |> List.mem (max a b)
+
+  let of_lines lines =
+    let cfg = Hashtbl.create 1000 in
+    Seq.iter
+      (fun line ->
+        match String.split_on_char ':' line with
+        | [ a; bs ] ->
+            String.split_on_char ' ' bs |> List.map String.trim
+            |> List.filter (( <> ) "")
+            |> List.iter (add cfg a)
+        | _ -> invalid_arg __FUNCTION__)
+      lines;
+    cfg
+
+  let pp fmt cfg =
+    Format.(
+      pp_print_string fmt "graph {\n";
+      Hashtbl.iter (fun k v -> fprintf fmt "\t%s -- %s;\n" k v) cfg;
+      pp_print_string fmt "}\n")
+end

9760feea20846a32f207409a5ea14320faaa49c6
Anton Yabchinskiy <arn@bestmx.net>
2024-01-03 20:57:18 +0300

    bin/day_23_2: Trail_Map.hikes ~slippery:false

--- 2023/bin/day_23_2.ml
+++ 2023/bin/day_23_2.ml
@@ -4,7 +4,7 @@ open Day_23
 let () =
   let trails = Trail_Map.of_lines (input_lines stdin) in
   let start, finish = Trail_Map.(start trails, finish trails) in
-  let hikes = Trail_Map.hikes ~slippery:true trails start finish in
+  let hikes = Trail_Map.hikes ~slippery:false trails start finish in
   let longest =
     List.reduce
       (fun hike1 hike2 -> if Pos_Set.(cardinal hike2 > cardinal hike1) then hike2 else hike1)

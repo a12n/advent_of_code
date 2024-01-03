7ed1d070eee8c1ca243ec4a593cfcffe6a74b64b
Anton Yabchinskiy <arn@bestmx.net>
2024-01-03 20:05:01 +0300

    bin/day_23_1: initial solution

--- 2023/bin/day_23_1.ml
+++ 2023/bin/day_23_1.ml
@@ -6,5 +6,11 @@ let () =
   let start, finish = Trail_Map.(start trails, finish trails) in
   Printf.eprintf "start (%d, %d), finish (%d, %d)\n%!" (fst start) (snd start) (fst finish)
     (snd finish);
-  (* TODO *)
-  Trail_Map.pp Format.err_formatter trails
+  Trail_Map.pp Format.err_formatter trails;
+  let hikes = Trail_Map.hikes trails start finish in
+  let longest =
+    List.reduce
+      (fun hike1 hike2 -> if Pos_Set.(cardinal hike2 > cardinal hike1) then hike2 else hike1)
+      hikes
+  in
+  print_endline (string_of_int (Pos_Set.cardinal longest))

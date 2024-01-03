854dd21f8cd42bb96bdeba0951e58724b49c376c
Anton Yabchinskiy <arn@bestmx.net>
2024-01-03 19:53:21 +0400

    lib/dir: add of_char

--- 2023/lib/dir.ml
+++ 2023/lib/dir.ml
@@ -1,6 +1,14 @@
 type t = Up | Left | Right | Down
 
 let neg = function Up -> Down | Left -> Right | Right -> Left | Down -> Up
+
+let of_char = function
+  | '^' -> Up
+  | '<' -> Left
+  | '>' -> Right
+  | 'v' -> Down
+  | _ -> invalid_arg __FUNCTION__
+
 let to_char = function Up -> '^' | Left -> '<' | Right -> '>' | Down -> 'v'
 let to_string = function Up -> "↑" | Left -> "←" | Down -> "↓" | Right -> "→"
 let turn_left = function Up -> Left | Left -> Down | Down -> Right | Right -> Up

459232694b7814a8773ee2de17569a961df12e61
Anton Yabchinskiy <arn@bestmx.net>
2024-01-04 02:12:43 +0300

    bin/day_24_1: WIP

--- 2023/bin/day_24_1.ml
+++ 2023/bin/day_24_1.ml
@@ -12,22 +12,33 @@ let () =
     (Q.to_string max);
   List.combine_tl hailstones
   |> List.map (fun (((p0, v0) as h0), hs) ->
-         List.filter_map (Hailstone.intersect2 h0) hs
-         |> List.map (fun (tx, ty) ->
-                let p = Point.(Ops.(p0 + (unit_x *. tx) + (unit_y *. ty))) in
-                Format.(
-                  pp_print_string err_formatter "p0 ";
-                  Point.pp err_formatter p0;
-                  pp_print_string err_formatter ", v0 ";
-                  Vector.pp err_formatter v0;
-                  pp_print_string err_formatter ", tx ";
-                  Q.pp_print err_formatter tx;
-                  pp_print_string err_formatter ", ty ";
-                  Q.pp_print err_formatter ty;
-                  pp_print_string err_formatter ", p ";
-                  Point.pp err_formatter p;
-                  pp_print_newline err_formatter ());
-                p))
+         List.filter_map
+           (fun ((p1, v1) as h1) ->
+             let* tx, ty = Hailstone.intersect2 h0 h1 in
+             let p =
+               Point.(Ops.(p0 + ({ zero with x = v0.x } *. tx) + ({ zero with y = v0.y } *. ty)))
+             in
+             Format.(
+               Point.pp err_formatter p0;
+               pp_print_string err_formatter " @ ";
+               Vector.pp err_formatter v0;
+               pp_print_string err_formatter "; ";
+               Point.pp err_formatter p1;
+               pp_print_string err_formatter " @ ";
+               Vector.pp err_formatter v1;
+               pp_print_string err_formatter " = ";
+               Q.pp_print err_formatter tx;
+               pp_print_string err_formatter ", ";
+               Q.pp_print err_formatter ty;
+               pp_print_string err_formatter "; p ";
+               Point.pp err_formatter p;
+               pp_print_newline err_formatter ());
+             Some p)
+           hs)
   |> List.concat
-  |> List.filter (fun Point.{ x; y; _ } -> min <= x && x <= max && min <= y && y <= max)
+  |> List.filter (fun Point.{ x; y; _ } ->
+         Format.(
+           fprintf err_formatter "min %s, max %s; x %s, y %s\n%!" (Q.to_string min)
+             (Q.to_string max) (Q.to_string x) (Q.to_string y));
+         min <= x && x <= max && min <= y && y <= max)
   |> List.length |> string_of_int |> print_endline

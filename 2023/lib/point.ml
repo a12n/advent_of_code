d4912bafa105d69722e32719fcdfe832c1dd24c7
Anton Yabchinskiy <arn@bestmx.net>
2024-01-04 01:40:37 +0300

    lib/point: Make functor, default instantiation for Int.t

--- 2023/lib/point.ml
+++ 2023/lib/point.ml
@@ -1,19 +1,42 @@
 open Fun.Ops
 
-type t = { x : int; y : int; z : int }
+module Make (Elt : sig
+  type t
 
-let zero = { x = 0; y = 0; z = 0 }
-let map2 f p q = { x = f p.x q.x; y = f p.y q.y; z = f p.z q.z }
-let add = map2 ( + )
-let sub = map2 ( - )
-let of_tuple (x, y, z) = { x; y; z }
-let to_tuple { x; y; z } = (x, y, z)
+  val zero : t
+  val add : t -> t -> t
+  val sub : t -> t -> t
+  val mul : t -> t -> t
+  val of_string : string -> t
+  val to_string : t -> string
+end) =
+struct
+  type t = { x : Elt.t; y : Elt.t; z : Elt.t }
 
-let of_string s =
-  match
-    String.split_on_char ',' s |> List.filter (( <> ) "") |> List.map (int_of_string % String.trim)
-  with
-  | [ x; y; z ] -> { x; y; z }
-  | _ -> invalid_arg __FUNCTION__
+  let zero = { x = Elt.zero; y = Elt.zero; z = Elt.zero }
+  let map f p = { x = f p.x; y = f p.y; z = f p.z }
+  let map2 f p q = { x = f p.x q.x; y = f p.y q.y; z = f p.z q.z }
+  let add = map2 Elt.add
+  let sub = map2 Elt.sub
+  let mul_elt v x = map (Elt.mul x) v
+  let of_tuple (x, y, z) = { x; y; z }
+  let to_tuple { x; y; z } = (x, y, z)
 
-let pp fmt { x; y; z } = Format.fprintf fmt "(%d, %d, %d)" x y z
+  let of_string s =
+    match
+      String.split_on_char ',' s |> List.filter (( <> ) "") |> List.map (Elt.of_string % String.trim)
+    with
+    | [ x; y; z ] -> { x; y; z }
+    | _ -> invalid_arg __FUNCTION__
+
+  let to_string { x; y; z } =
+    "(" ^ Elt.to_string x ^ ", " ^ Elt.to_string y ^ ", " ^ Elt.to_string z ^ ")"
+
+  let pp fmt p = Format.pp_print_string fmt (to_string p)
+end
+
+include Make (struct
+  include Int
+
+  let of_string = int_of_string
+end)

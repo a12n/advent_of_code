6ae46f492885cf38b85a76db6410b02d547315c1
Anton Yabchinskiy <arn@bestmx.net>
2024-01-03 23:52:44 +0300

    bin/day_24: WIP on Hailstone.intersect

--- 2023/bin/day_24.ml
+++ 2023/bin/day_24.ml
@@ -8,6 +8,12 @@ module Hailstone = struct
     match String.split_on_char '@' s |> List.map String.trim with
     | [ p; v ] -> (Point.of_string p, Vector.of_string v)
     | _ -> invalid_arg __FUNCTION__
+
+  let intersect (p1, v1) (p2, v2) =
+    (* TODO *)
+    ignore (p1, v1);
+    ignore (p2, v2);
+    None
 end
 
 let of_lines = List.of_seq % Seq.map Hailstone.of_string

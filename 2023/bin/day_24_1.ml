84381445f3ac0197a2f0d832b3677132b6b6b9ba
Anton Yabchinskiy <arn@bestmx.net>
2024-01-04 18:09:12 +0400

    bin/day_24_1: rename {min,max}_value

--- 2023/bin/day_24_1.ml
+++ 2023/bin/day_24_1.ml
@@ -3,13 +3,13 @@ open Day_24
 
 let () =
   let hailstones = of_lines (input_lines stdin) in
-  let min_elt, max_elt =
+  let min_value, max_value =
     Option.
       ( value ~default:Q.(of_int 200000000000000) (map Q.of_string (Sys.getenv_opt "MIN")),
         value ~default:Q.(of_int 400000000000000) (map Q.of_string (Sys.getenv_opt "MAX")) )
   in
-  Printf.eprintf "%d hailstones, min %s, max %s\n%!" (List.length hailstones) (Q.to_string min_elt)
-    (Q.to_string max_elt);
+  Printf.eprintf "%d hailstones, min %s, max %s\n%!" (List.length hailstones)
+    (Q.to_string min_value) (Q.to_string max_value);
   List.combine_tl hailstones
   |> List.map (fun (((p0, v0) as h0), hs) ->
          List.filter_map
@@ -31,7 +31,7 @@ let () =
              Some (x, y))
            hs)
   |> List.concat
-  |> List.filter (fun (x, y) -> Q.(x >= min_elt && x <= max_elt && y >= min_elt && y <= max_elt))
+  |> List.filter (fun (x, y) -> Q.(x >= min_value && x <= max_value && y >= min_value && y <= max_value))
   |> List.map (fun (x, y) ->
          Format.(fprintf err_formatter "pass (%f, %f)\n%!" (Q.to_float x) (Q.to_float y));
          (x, y))

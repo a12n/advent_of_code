9297c186bd3f42a066dcbe24a9c0822a71411716
Anton Yabchinskiy <arn@bestmx.net>
2024-01-04 12:08:18 +0400

    bin/day_22_1: adjust for hashtbl from Snapshot.settle, generate set of unsafe bricks and use only it's cardinality in the answer

--- 2023/bin/day_22_1.ml
+++ 2023/bin/day_22_1.ml
@@ -4,22 +4,24 @@ open Day_22
 let () =
   (* List of settled bricks along with a set of brick numbers
      supporting each brick. *)
-  let bricks = Snapshot.(settle (of_lines (input_lines stdin))) in
+  let bricks, supports, _ = Snapshot.(settle (of_lines (input_lines stdin))) in
   List.iteri
-    (fun i ((Point.{ x = x1; y = y1; z = z1 }, Point.{ x = x2; y = y2; z = z2 }), support) ->
+    (fun i (Point.{ x = x1; y = y1; z = z1 }, Point.{ x = x2; y = y2; z = z2 }) ->
       Printf.eprintf "%d,%d,%d~%d,%d,%d\n%!" x1 y1 z1 x2 y2 z2;
       Printf.eprintf "# Brick %d supported by:" i;
-      Int_Set.iter (Printf.eprintf " %d") support;
+      List.iter (Printf.eprintf " %d") (Hashtbl.find_all supports i);
       Printf.eprintf "\n%!")
     bricks;
-  let disintegrate =
-    List.fold_left
-      (fun candidates (_, support) ->
-        if Int_Set.cardinal support = 1 then Int_Set.diff candidates support else candidates)
-      (Int_Set.of_list List.(init (length bricks) Fun.id))
-      bricks
+  let unsafe =
+    List.fold_lefti
+      (fun unsafe i _brick ->
+        match Hashtbl.find_all supports i with
+        | [ j ] ->
+            (* Brick [j] is the only brick supporting [i]. It isn't
+               safe to disintegrate it. *)
+            Int_Set.add j unsafe
+        | [] | _ :: _ :: _ -> unsafe)
+      Int_Set.empty bricks
   in
-  Printf.eprintf "# Disintegrate:";
-  Int_Set.iter (Printf.eprintf " %d") disintegrate;
-  Printf.eprintf "\n%!";
-  print_endline (string_of_int (Int_Set.cardinal disintegrate))
+  Printf.eprintf "# Unsafe bricks %d/%d\n%!" (Int_Set.cardinal unsafe) (List.length bricks);
+  print_endline (string_of_int (List.length bricks - Int_Set.cardinal unsafe))

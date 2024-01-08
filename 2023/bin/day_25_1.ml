1e6fdd6f91c33bee52affa399ba823d5fa2bf356
Anton Yabchinskiy <arn@bestmx.net>
2024-01-08 15:13:40 +0400

    bin/day_25_1: remove specific edges, count vertices in connected components

--- 2023/bin/day_25_1.ml
+++ 2023/bin/day_25_1.ml
@@ -2,8 +2,16 @@ open Advent
 open Day_25
 
 let () =
-  let conns = Config.of_lines (input_lines stdin) in
-  Format.(
-    fprintf err_formatter "// %d conns\n" (Hashtbl.length conns);
-    Config.pp err_formatter conns
-  )
+  let conns = of_lines (input_lines stdin) in
+  Graph.pp Format.err_formatter conns;
+  (* Identified from Graphviz visualization. *)
+  let conns =
+    List.fold_left
+      (fun conns (u, v) -> Graph.remove_edge conns u v ())
+      conns
+      [ ("hqq", "xxq"); ("xzz", "kgl"); ("vkd", "qfb") ]
+  in
+  Graph.pp Format.err_formatter conns;
+  Graph.components conns
+  |> List.map (List.length % Graph.vertices)
+  |> List.reduce ( * ) |> string_of_int |> print_endline

a8f094404e925fbb21ea02a2af649e1dede8acfc
Anton Yabchinskiy <arn@bestmx.net>
2024-01-08 17:48:38 +0400

    bin/day_24_2: WIP

---
+++ 2023/bin/day_24_2.ml
@@ -0,0 +1,12 @@
+(* Find position [p] and velocty [v], such that
+   for sample input:
+
+   [19, 13, 30] + t1 [-2, 1, -2] = p + t1 v
+   [18, 19, 22] + t2 [-1, -1, -2] = p + t2 v
+   [20, 25, 34] + t3 [-2, -2, -4] = p + t3 v
+   [12, 31, 28] + t4 [-1, -2, -1] = p + t4 v
+   [20, 19, 15] + t5 [1, -5, -3] = p + t5 v
+ *)
+let () =
+  (* TODO *)
+  ()

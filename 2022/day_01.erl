fb21d589ad0ae84e1280c0258166987444f6c6de
Anton Yabchinskiy <arn@bestmx.net>
2024-09-06 04:08:03 +0300

    day_01: make a day module with main(part()) function

--- 2022/day_01.erl
+++ 2022/day_01.erl
@@ -1,12 +1,24 @@
-#!/usr/bin/env escript
-
--module(day_01_1).
+-module(day_01).
 
 -type food() :: pos_integer().
 -type inventory() :: nonempty_list(food()).
--export_type([food/0, inventory/0]).
 
 -export([main/1]).
+
+-spec main(1..2) -> ok.
+main(Part) ->
+    Inventories = lists:map(fun lists:sum/1, read_inventory_list(standard_io)),
+    case Part of
+        1 ->
+            io:format(<<"~b~n">>, [lists:max(Inventories)]);
+        2 ->
+            {TopThree, _} = lists:split(3, lists:sort(fun erlang:'>='/2, Inventories)),
+            io:format(<<"~b~n">>, [lists:sum(TopThree)])
+    end.
+
+%%--------------------------------------------------------------------
+%% Internal functions.
+%%--------------------------------------------------------------------
 
 -spec parse_food(binary()) -> food().
 parse_food(Number) ->
@@ -25,15 +37,4 @@ read_inventory_list(File) ->
     case read_inventory(File) of
         [] -> [];
         Inventory -> [Inventory | read_inventory_list(File)]
-    end.
-
-main(_) ->
-    io:setopts([binary, {encoding, utf8}]),
-    Inventories = lists:map(fun lists:sum/1, read_inventory_list(standard_io)),
-    case filename:basename(escript:script_name(), ".erl") of
-        "day_01_1" ->
-            io:format(<<"~B~n">>, [lists:max(Inventories)]);
-        "day_01_2" ->
-            {TopThree, _} = lists:split(3, lists:sort(fun erlang:'>='/2, Inventories)),
-            io:format(<<"~p~n">>, [lists:sum(TopThree)])
     end.

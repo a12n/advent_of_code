1050335e223081ba63c6834692041ed0dba7cb60
Anton Yabchinskiy <arn@bestmx.net>
2024-09-06 04:27:01 +0300

    day_02: rearrange functions, add outcome() type

--- 2022/day_02.erl
+++ 2022/day_02.erl
@@ -3,6 +3,7 @@
 -type shape() :: rock | paper | scissors.
 -type round() :: {_Their :: shape(), _Your :: shape()}.
 -type game() :: nonempty_list(round()).
+-type outcome() :: lose | draw | win.
 
 -export([main/1]).
 
@@ -12,7 +13,38 @@ main(1) ->
     io:format(<<"~b~n">>, [game_score(Game)]).
 
 %%--------------------------------------------------------------------
-%% Internal functions.
+%% Scoring functions.
+%%--------------------------------------------------------------------
+
+-spec game_score(game()) -> non_neg_integer().
+game_score(Rounds) ->
+    lists:sum(lists:map(fun round_score/1, Rounds)).
+
+-spec round_score(round()) -> non_neg_integer().
+round_score(Round = {_Their, Your}) ->
+    selected_shape_score(Your) + outcome_score(round_outcome(Round)).
+
+-spec selected_shape_score(shape()) -> non_neg_integer().
+selected_shape_score(rock) -> 1;
+selected_shape_score(paper) -> 2;
+selected_shape_score(scissors) -> 3.
+
+-spec outcome_score(outcome()) -> non_neg_integer().
+outcome_score(lose) -> 0;
+outcome_score(draw) -> 3;
+outcome_score(win) -> 6.
+
+-spec round_outcome(round()) -> outcome().
+round_outcome({rock, scissors}) -> lose;
+round_outcome({paper, rock}) -> lose;
+round_outcome({scissors, paper}) -> lose;
+round_outcome({rock, paper}) -> win;
+round_outcome({paper, scissors}) -> win;
+round_outcome({scissors, rock}) -> win;
+round_outcome({Their, Your}) when Their == Your -> draw.
+
+%%--------------------------------------------------------------------
+%% Part 1 functions.
 %%--------------------------------------------------------------------
 
 -spec read_game(io:device()) -> game().
@@ -21,10 +53,6 @@ read_game(File) ->
         eof -> [];
         <<Line/bytes>> -> [parse_round(string:trim(Line)) | read_game(File)]
     end.
-
--spec game_score(game()) -> non_neg_integer().
-game_score(Rounds) ->
-    lists:sum(lists:map(fun round_score/1, Rounds)).
 
 -spec parse_round(binary()) -> round().
 parse_round(<<Their, $\s, Your>>) ->
@@ -38,23 +66,6 @@ parse_shape(your, $X) -> rock;
 parse_shape(your, $Y) -> paper;
 parse_shape(your, $Z) -> scissors.
 
--spec round_score(round()) -> non_neg_integer().
-round_score(Round = {_Their, Your}) ->
-    selected_shape_score(Your) + round_outcome_score(Round).
-
--spec selected_shape_score(shape()) -> non_neg_integer().
-selected_shape_score(rock) -> 1;
-selected_shape_score(paper) -> 2;
-selected_shape_score(scissors) -> 3.
-
--spec round_outcome_score(round()) -> non_neg_integer().
-%% You lost.
-round_outcome_score({rock, scissors}) -> 0;
-round_outcome_score({paper, rock}) -> 0;
-round_outcome_score({scissors, paper}) -> 0;
-%% You won.
-round_outcome_score({rock, paper}) -> 6;
-round_outcome_score({paper, scissors}) -> 6;
-round_outcome_score({scissors, rock}) -> 6;
-%% Was a draw.
-round_outcome_score({Their, Your}) when Their == Your -> 3.
+%%--------------------------------------------------------------------
+%% Part 2 functions.
+%%--------------------------------------------------------------------

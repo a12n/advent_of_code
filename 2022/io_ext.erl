c3ef9d3204b86ea4f99f65e6f46368a27970b01d
Anton Yabchinskiy <arn@bestmx.net>
2024-09-06 21:18:32 +0300

    io_ext: add read_lines/2

--- 2022/io_ext.erl
+++ 2022/io_ext.erl
@@ -1,10 +1,17 @@
 -module(io_ext).
 
--export([read_lines/1]).
+-export([read_lines/1, read_lines/2]).
 
 -spec read_lines(io:device()) -> [binary()].
-read_lines(File) ->
+read_lines(File) -> read_lines(File, -1).
+
+-spec read_lines(io:device(), integer()) -> [binary()].
+read_lines(_File, 0) ->
+    [];
+read_lines(File, N) ->
     case io:get_line(File, <<>>) of
-        eof -> [];
-        <<Line/bytes>> -> [string:trim(Line) | read_lines(File)]
+        eof ->
+            [];
+        <<Line/bytes>> ->
+            [string:trim(Line) | read_lines(File, N - 1)]
     end.

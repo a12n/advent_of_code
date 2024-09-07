5dea820e9231476ab6290e41d3d06bcf04856572
Anton Yabchinskiy <arn@bestmx.net>
2024-09-08 02:15:48 +0300

    day_06: solution

--- 2022/day_06.erl
+++ 2022/day_06.erl
@@ -1,22 +1,42 @@
 -module(day_06).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -export([main/1]).
 
 -spec main(1..2) -> ok.
-main(1) ->
-    ok = io:setopts(standard_io, [list]),
+main(Part) ->
     [Signal] = io_ext:read_lines(standard_io),
-    io:format(<<"~b~n">>, [find_marker(Signal)]).
+    io:format(<<"~b~n">>, [
+        find_marker(
+            Signal,
+            case Part of
+                1 -> 4;
+                2 -> 14
+            end
+        )
+    ]).
 
--spec find_marker(string()) -> non_neg_integer().
-find_marker(Signal) -> find_marker(Signal, 4).
+-spec find_marker(binary(), pos_integer()) -> pos_integer().
+find_marker(Signal, N) ->
+    find_marker(Signal, N, 0, #{}).
 
--spec find_marker(string(), non_neg_integer()) -> non_neg_integer().
-find_marker([A, B, C, D | _], N) when
-    not (A == B orelse A == C orelse A == D orelse
-        B == C orelse B == D orelse
-        C == D)
-->
-    N;
-find_marker([_ | Signal], N) ->
-    find_marker(Signal, N + 1).
+-spec find_marker(string(), pos_integer(), non_neg_integer(), freq_maps:t(byte())) -> pos_integer().
+find_marker(Signal, N, I, Freqs) ->
+    case maps:size(Freqs) of
+        M when M == N ->
+            %% Characters in the state map are N different characters,
+            %% found the marker.
+            I;
+        _ ->
+            %% Increase the number of next char() in state.
+            Freqs2 = freq_maps:increase(binary:at(Signal, I), Freqs),
+            %% Decrease the number of previous char() in state.
+            Freqs3 =
+                case I >= N of
+                    true -> freq_maps:decrease(binary:at(Signal, I - N), Freqs2);
+                    false -> Freqs2
+                end,
+            %% Look further.
+            find_marker(Signal, N, I + 1, Freqs3)
+    end.

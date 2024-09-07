10e64f17f72b681acb23ddbc6ebed0cff3a8e912
Anton Yabchinskiy <arn@bestmx.net>
2024-09-07 22:14:26 +0300

    io_ext: handle string() lines

--- 2022/io_ext.erl
+++ 2022/io_ext.erl
@@ -14,5 +14,8 @@ read_lines(File, N) ->
             [];
         <<Line/bytes>> ->
             %% Trim the trailing \n.
-            [<<Line:(byte_size(Line) - 1)/bytes>> | read_lines(File, N - 1)]
+            [<<Line:(byte_size(Line) - 1)/bytes>> | read_lines(File, N - 1)];
+        Line when is_list(Line) ->
+            %% Trim the trailing \n in a string.
+            [lists:droplast(Line) | read_lines(File, N - 1)]
     end.

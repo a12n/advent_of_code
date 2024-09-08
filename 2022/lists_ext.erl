6a56f31a40ed03d136432a31d6796c5968884e82
Anton Yabchinskiy <arn@bestmx.net>
2024-09-08 19:53:57 +0300

    lists_ext: rotate/2 function

--- 2022/lists_ext.erl
+++ 2022/lists_ext.erl
@@ -2,10 +2,14 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--export([reduce/2, transpose/1]).
+-export([reduce/2, transpose/1, rotate/2]).
 
 -spec reduce(fun((term(), term()) -> term()), nonempty_list()) -> term().
 reduce(F, [Head | Tail]) -> lists:foldl(F, Head, Tail).
+
+%%--------------------------------------------------------------------
+%% Matrix functions.
+%%--------------------------------------------------------------------
 
 -spec transpose(nonempty_list(nonempty_list(term()))) -> nonempty_list(nonempty_list(term())).
 transpose([[] | _]) ->
@@ -14,6 +18,11 @@ transpose(Lists) ->
     Heads = [H || [H | _] <- Lists],
     Tails = [T || [_ | T] <- Lists],
     [Heads | transpose(Tails)].
+
+-spec rotate(cw | ccw, nonempty_list(nonempty_list(term()))) ->
+    nonempty_list(nonempty_list(term())).
+rotate(cw, Lists) -> transpose(lists:reverse(Lists));
+rotate(ccw, Lists) -> lists:reverse(transpose(Lists)).
 
 -ifdef(TEST).
 
@@ -31,6 +40,45 @@ transpose_test() ->
             [4, 5, 6],
             [7, 8, 9]
         ])
+    ).
+
+rotate_test() ->
+    Lists = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ],
+    CW1 = rotate(cw, Lists),
+    ?assertEqual(
+        [
+            [7, 4, 1],
+            [8, 5, 2],
+            [9, 6, 3]
+        ],
+        CW1
+    ),
+    CW2 = rotate(cw, CW1),
+    ?assertEqual(
+        [
+            [9, 8, 7],
+            [6, 5, 4],
+            [3, 2, 1]
+        ],
+        CW2
+    ),
+    CW3 = rotate(cw, CW2),
+    ?assertEqual(
+        [
+            [3, 6, 9],
+            [2, 5, 8],
+            [1, 4, 7]
+        ],
+        CW3
+    ),
+    CW4 = rotate(cw, CW3),
+    ?assertEqual(
+        Lists,
+        CW4
     ).
 
 -endif.

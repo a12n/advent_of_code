729eba60a23822888f97382a2b26c71ae651b453
Anton Yabchinskiy <arn@bestmx.net>
2024-09-09 11:28:42 +0300

    day_10_1: solution

--- 2022/day_10.erl
+++ 2022/day_10.erl
@@ -1,5 +1,8 @@
 -module(day_10).
 
+-type instruction() :: {addx, integer()} | noop.
+-type trace() :: [{_PC :: non_neg_integer(), _X :: integer()}].
+
 -export([main/1]).
 
 -spec main(1..2) -> ok.
@@ -11,6 +14,30 @@ main(1) ->
         end,
         io_ext:read_lines(standard_io)
     ),
-    io:format("~p~n", [Program]),
-    %% TODO
-    ok.
+    io:format(standard_error, <<"~p~n">>, [Program]),
+    Trace = trace(Program),
+    io:format(standard_error, <<"trace ~p~n">>, [Trace]),
+    SignalStrength = signal_strength(Trace, [20 - 1, 60 - 1, 100 - 1, 140 - 1, 180 - 1, 220 - 1]),
+    io:format(standard_error, <<"signal_strength ~p~n">>, [SignalStrength]),
+    io:format(<<"~b~n">>, [lists:sum(SignalStrength)]).
+
+-spec trace([instruction()]) -> trace().
+trace(Program) -> [{0, 1} | trace(Program, 0, 1)].
+
+-spec trace([instruction()], non_neg_integer(), integer()) -> trace().
+trace([], _, _) ->
+    [];
+trace([noop | Program], PC, X) ->
+    trace(Program, PC + 1, X);
+trace([{addx, Y} | Program], PC, X) ->
+    PC2 = PC + 2,
+    X2 = X + Y,
+    [{PC2, X2} | trace(Program, PC2, X2)].
+
+-spec signal_strength(trace(), [non_neg_integer()]) -> [integer()].
+signal_strength(_, []) ->
+    [];
+signal_strength([{PC0, X0} | Trace = [{PC1, _} | _]], [T | Points]) when T >= PC0, T < PC1 ->
+    [X0 * (T + 1) | signal_strength(Trace, Points)];
+signal_strength([_ | Trace], Points) ->
+    signal_strength(Trace, Points).

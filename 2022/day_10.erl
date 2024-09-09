5de5c3e006cbb4afdf8952affcf551db3b33a0eb
Anton Yabchinskiy <arn@bestmx.net>
2024-09-09 12:47:12 +0300

    day_10: pixel_pos/1 from offset, screen size constants

--- 2022/day_10.erl
+++ 2022/day_10.erl
@@ -3,6 +3,9 @@
 -type instruction() :: {addx, integer()} | noop.
 -type trace() :: [{_PC :: non_neg_integer(), _X :: integer()}].
 
+-define(SCREEN_WIDTH, 40).
+-define(SCREEN_HEIGHT, 6).
+
 -export([main/1]).
 
 -spec main(1..2) -> ok.
@@ -20,8 +23,8 @@ main(Part) ->
             SignalStrength = signal_strength(Trace, [20, 60, 100, 140, 180, 220]),
             io:format(<<"~b~n">>, [lists:sum(SignalStrength)]);
         2 ->
-            LitPixels = maps:from_list([{Pos, $#} || Pos <- lit_pixels(Trace)]),
-            io:format(grids:to_iodata(LitPixels, {6, 40}))
+            LitPixels = maps:from_list([{pixel_pos(Pos), $#} || Pos <- lit_pixels(Trace)]),
+            io:format(grids:to_iodata(LitPixels, {?SCREEN_HEIGHT, ?SCREEN_WIDTH}))
     end.
 
 -spec trace([instruction()]) -> trace().
@@ -45,7 +48,11 @@ signal_strength([{PC0, X0} | Trace = [{PC1, _} | _]], [T | Points]) when T > PC0
 signal_strength([_ | Trace], Points) ->
     signal_strength(Trace, Points).
 
--spec lit_pixels(trace()) -> [grids:pos(integer())].
+-spec lit_pixels(trace()) -> [non_neg_integer()].
 lit_pixels(Trace) ->
     %% TODO
     [].
+
+-spec pixel_pos(non_neg_integer()) -> grids:pos().
+pixel_pos(Pos) ->
+    {(Pos div ?SCREEN_WIDTH) + 1, (Pos rem ?SCREEN_WIDTH) + 1}.

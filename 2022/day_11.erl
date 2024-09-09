a3ae81b7b83f517526cbcce6746cbbfa94f5e907
Anton Yabchinskiy <arn@bestmx.net>
2024-09-09 16:36:07 +0300

    day_11: parse_monkeys/1

--- 2022/day_11.erl
+++ 2022/day_11.erl
@@ -1,7 +1,77 @@
 -module(day_11).
 
+-include_lib("eunit/include/eunit.hrl").
+
+-type monkey() :: term().
+
 -export([main/1]).
 
 -spec main(1..2) -> ok.
 main(1) ->
+    Monkeys = parse_monkeys(
+        lists:filtermap(
+            fun
+                (<<>>) ->
+                    false;
+                (<<Line/bytes>>) ->
+                    [Key, Value] = binary:split(Line, <<":">>),
+                    {true, {string:trim(Key), string:trim(Value)}}
+            end,
+            io_ext:read_lines(standard_io)
+        )
+    ),
+    ?debugFmt("Monkeys ~p", [Monkeys]),
     ok.
+
+-spec parse_monkeys([{binary(), binary()}]) -> [monkey()].
+parse_monkeys([]) ->
+    [];
+parse_monkeys([
+    {<<"Monkey ", IDStr/bytes>>, <<>>},
+    {<<"Starting items">>, ItemsStr},
+    {<<"Operation">>, OperationStr},
+    {<<"Test">>, <<"divisible by ", DivisorStr/bytes>>},
+    {<<"If true">>, <<"throw to monkey ", TrueIDStr/bytes>>},
+    {<<"If false">>, <<"throw to monkey ", FalseIDStr/bytes>>}
+    | Lines
+]) ->
+    ID = binary_to_integer(IDStr),
+    Items = [
+        binary_to_integer(string:trim(ItemStr))
+     || ItemStr <- binary:split(ItemsStr, <<",">>, [global])
+    ],
+    Operation =
+        case binary:split(OperationStr, <<" ">>, [global]) of
+            [<<"new">>, <<"=">>, AStr, OpStr, BStr] ->
+                Op =
+                    case OpStr of
+                        <<"+">> -> fun erlang:'+'/2;
+                        <<"*">> -> fun erlang:'*'/2
+                    end,
+                case {AStr, BStr} of
+                    {<<"old">>, <<"old">>} ->
+                        fun(Old) -> Op(Old, Old) div 3 end;
+                    {<<"old">>, _} ->
+                        B = binary_to_integer(BStr),
+                        fun(Old) -> Op(Old, B) div 3 end;
+                    {_, <<"old">>} ->
+                        A = binary_to_integer(AStr),
+                        fun(Old) -> Op(A, Old) div 3 end
+                end
+        end,
+    Divisor = binary_to_integer(DivisorStr),
+    TrueID = binary_to_integer(TrueIDStr),
+    FalseID = binary_to_integer(FalseIDStr),
+    Next = fun
+        (Item) when Item rem Divisor == 0 -> TrueID;
+        (_) -> FalseID
+    end,
+    [
+        #{
+            id => ID,
+            items => Items,
+            operation => Operation,
+            next => Next
+        }
+        | parse_monkeys(Lines)
+    ].

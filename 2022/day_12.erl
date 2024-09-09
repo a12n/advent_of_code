56564d3f4ee88539955ad1a8fd23266343b5a173
Anton Yabchinskiy <arn@bestmx.net>
2024-09-09 18:23:53 +0300

    day_12: parse input

--- 2022/day_12.erl
+++ 2022/day_12.erl
@@ -1,7 +1,19 @@
 -module(day_12).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -export([main/1]).
 
 -spec main(1..2) -> ok.
 main(1) ->
+    HeightMap = maps:map(
+        fun
+            (_, $S) -> $a - 1;
+            (_, $E) -> $z + 1;
+            (_, C) -> C
+        end,
+        grids:from_lines(io_ext:read_lines(standard_io))
+    ),
+    HeightMapSize = grids:size(HeightMap),
+    io:format(grids:to_iodata(HeightMap, HeightMapSize)),
     ok.

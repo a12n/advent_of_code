0bf2e66cab7512c420bbc0cb469eea6036826342
Anton Yabchinskiy <arn@bestmx.net>
2024-09-10 15:17:20 +0300

    day_14: rename paths_to_rocks/1

--- 2022/day_14.erl
+++ 2022/day_14.erl
@@ -30,7 +30,7 @@ main(1) ->
             io_ext:read_lines(standard_io)
         ),
     Source = {0, 500},
-    Rocks = paths_to_point_set(Paths),
+    Rocks = paths_to_rocks(Paths),
     {{_, MinCol}, MaxPos} = grids:extent(Rocks),
     ?debugFmt("Paths ~p", [Paths]),
     ?debugFmt("Rocks ~p", [Rocks]),
@@ -44,26 +44,26 @@ path_to_lines([]) -> [];
 path_to_lines([P0, P1]) -> [{P0, P1}];
 path_to_lines([P0 | PointsLeft = [P1 | _]]) -> [{P0, P1} | path_to_lines(PointsLeft)].
 
--spec line_to_points(line()) -> [point()].
-line_to_points({{X1, Y}, {X2, Y}}) ->
+-spec line_to_positions(line()) -> [pos()].
+line_to_positions({{X1, Y}, {X2, Y}}) ->
     %% Horizontal line.
     [{X, Y} || X <- lists_ext:seq(X1, X2)];
-line_to_points({{X, Y1}, {X, Y2}}) ->
+line_to_positions({{X, Y1}, {X, Y2}}) ->
     %% Vertical line.
     [{X, Y} || Y <- lists_ext:seq(Y1, Y2)].
 
--spec paths_to_point_set([path()]) -> point_set().
-paths_to_point_set(Paths) ->
+-spec paths_to_rocks([path()]) -> rocks().
+paths_to_rocks(Paths) ->
     lists:foldl(
         fun(Path, Result) ->
             lists:foldl(
                 fun(Line, Result) ->
                     lists:foldl(
-                        fun(Point, Result) ->
-                            Result#{Point => $#}
+                        fun(Pos, Result) ->
+                            Result#{Pos => $#}
                         end,
                         Result,
-                        line_to_points(Line)
+                        line_to_positions(Line)
                     )
                 end,
                 Result,

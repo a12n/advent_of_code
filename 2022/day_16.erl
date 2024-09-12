bee115e755b31ce2c102c84620a04d0bd5a5d739
Anton Yabchinskiy <arn@bestmx.net>
2024-09-12 18:51:31 +0300

    day_16: parse valves

--- 2022/day_16.erl
+++ 2022/day_16.erl
@@ -4,4 +4,26 @@
 
 -spec main(1..2) -> ok.
 main(1) ->
+    Valves = lists:foldl(
+        fun(<<Line/bytes>>, Result) ->
+            [
+                <<"Valve">>,
+                ID,
+                <<"has">>,
+                <<"flow">>,
+                <<"rate">>,
+                FlowStr,
+                <<"tunnel", _/bytes>>,
+                <<"lead", _/bytes>>,
+                <<"to">>,
+                <<"valve", _/bytes>>
+                | LeadTo
+            ] =
+                binary:split(Line, [<<" ">>, <<",">>, <<"=">>, <<";">>], [global, trim, trim_all]),
+            Result#{ID => {binary_to_integer(FlowStr), LeadTo}}
+        end,
+        #{},
+        io_ext:read_lines(standard_io)
+    ),
+    io:format(standard_error, <<"~p valves ~p~n">>, [maps:size(Valves), Valves]),
     ok.

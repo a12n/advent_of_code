febf895b5d909e1c3430ea061bce6f7925c9fac7
Anton Yabchinskiy <arn@bestmx.net>
2024-09-12 15:15:16 +0300

    io_ext: work in progress on fread_lines/{2,3}

--- 2022/io_ext.erl
+++ 2022/io_ext.erl
@@ -1,6 +1,6 @@
 -module(io_ext).
 
--export([read_lines/1, read_lines/2]).
+-export([fread_lines/2, fread_lines/3, read_lines/1, read_lines/2]).
 
 -spec read_lines(io:device()) -> [binary()].
 read_lines(File) -> read_lines(File, -1).
@@ -18,4 +18,18 @@ read_lines(File, N) ->
         Line when is_list(Line) ->
             %% Trim the trailing \n in a string.
             [lists:droplast(Line) | read_lines(File, N - 1)]
+    end.
+
+-spec fread_lines(io:device(), string() | binary()) -> [[term()]].
+fread_lines(File, Format) ->
+    fread_lines(File, Format, -1).
+
+%% XXX
+-spec fread_lines(io:device(), string() | binary(), integer()) -> [[term()]].
+fread_lines(_, _, 0) ->
+    [];
+fread_lines(File, Format, N) ->
+    case io:fread(File, <<>>, Format) of
+        eof -> [];
+        {ok, Terms} -> [Terms | fread_lines(File, Format, N - 1)]
     end.

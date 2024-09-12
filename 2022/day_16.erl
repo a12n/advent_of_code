ec4b9d5f0f559491232052892018b29f2d151be0
Anton Yabchinskiy <arn@bestmx.net>
2024-09-12 18:54:20 +0300

    day_16: separate parse_valve/1

--- 2022/day_16.erl
+++ 2022/day_16.erl
@@ -4,26 +4,24 @@
 
 -spec main(1..2) -> ok.
 main(1) ->
-    Valves = lists:foldl(
-        fun(<<Line/bytes>>, Result) ->
-            [
-                <<"Valve">>,
-                ID,
-                <<"has">>,
-                <<"flow">>,
-                <<"rate">>,
-                FlowStr,
-                <<"tunnel", _/bytes>>,
-                <<"lead", _/bytes>>,
-                <<"to">>,
-                <<"valve", _/bytes>>
-                | LeadTo
-            ] =
-                binary:split(Line, [<<" ">>, <<",">>, <<"=">>, <<";">>], [global, trim, trim_all]),
-            Result#{ID => {binary_to_integer(FlowStr), LeadTo}}
-        end,
-        #{},
-        io_ext:read_lines(standard_io)
-    ),
+    Valves = maps:from_list(lists:map(fun parse_valve/1, io_ext:read_lines(standard_io))),
     io:format(standard_error, <<"~p valves ~p~n">>, [maps:size(Valves), Valves]),
     ok.
+
+-spec parse_valve(binary()) -> {binary(), {non_neg_integer(), [binary()]}}.
+parse_valve(Line) ->
+    [
+        <<"Valve">>,
+        ID,
+        <<"has">>,
+        <<"flow">>,
+        <<"rate">>,
+        FlowStr,
+        <<"tunnel", _/bytes>>,
+        <<"lead", _/bytes>>,
+        <<"to">>,
+        <<"valve", _/bytes>>
+        | LeadsTo
+    ] =
+        binary:split(Line, [<<" ">>, <<",">>, <<"=">>, <<";">>], [global, trim, trim_all]),
+    {ID, {binary_to_integer(FlowStr), LeadsTo}}.

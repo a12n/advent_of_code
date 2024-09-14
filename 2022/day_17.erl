20485233f9fd5959fa1577795bdd8862b09aec0b
Anton Yabchinskiy <arn@bestmx.net>
2024-09-15 01:27:32 +0300

    day_17: shape definitions, rewrite move_shape/2

--- 2022/day_17.erl
+++ 2022/day_17.erl
@@ -3,32 +3,54 @@
 %% -export([main/1]).
 -compile(export_all).
 
--spec shape_1() -> map().
-shape_1() -> #{{1, 1} => $#, {1, 2} => $#, {1, 3} => $#, {1, 4} => $#}.
+-type shape() :: #{grids:pos(integer()) => char()}.
 
--spec shape_2() -> map().
-shape_2() -> #{{1, 2} => $#, {2, 1} => $#, {2, 2} => $#, {2, 3} => $#, {3, 2} => $#}.
+-spec shape([string()]) -> shape().
+shape(Definition) ->
+    maps:from_list(
+        [
+            %% Shape's zero coordinate is at the bottom left
+            %% corner. Reverse lines and negate row numbers.
+            {{-Row, Col}, Char}
+         || {Row, Line} <- lists:enumerate(0, lists:reverse(Definition)),
+            {Col, Char} <- lists:enumerate(0, Line),
+            Char =/= $\s,
+            Char =/= $.
+        ]
+    ).
 
--spec shape_3() -> map().
-shape_3() -> #{{1, 3} => $#, {2, 3} => $#, {3, 1} => $#, {3, 2} => $#, {3, 3} => $#}.
-
--spec shape_4() -> map().
-shape_4() -> #{{1, 1} => $#, {2, 1} => $#, {3, 1} => $#, {4, 1} => $#}.
-
--spec shape_5() -> map().
-shape_5() -> #{{1, 1} => $#, {1, 2} => $#, {2, 1} => $#, {2, 2} => $#}.
-
--spec shapes() -> [map()].
-shapes() -> [shape_1(), shape_2(), shape_3(), shape_4(), shape_5()].
+-spec shapes() -> [shape()].
+shapes() ->
+    [
+        shape([
+            "####"
+        ]),
+        shape([
+            " # ",
+            "###",
+            " # "
+        ]),
+        shape([
+            "  #",
+            "  #",
+            "###"
+        ]),
+        shape([
+            "#",
+            "#",
+            "#",
+            "#"
+        ]),
+        shape([
+            "##",
+            "##"
+        ])
+    ].
 
 -spec move_shape(grids:dir() | grids:pos(integer()), map()) -> map().
-move_shape(Offset = {_, _}, Shape) ->
-    maps:fold(
-        fun(Pos, Char, Result) ->
-            Result#{grids:add_pos(Pos, Offset) => Char}
-        end,
-        #{},
-        Shape
+move_shape(Move = {_, _}, Shape) ->
+    maps:from_list(
+        [{grids:add_pos(Pos, Move), Char} || {Pos, Char} <- maps:to_list(Shape)]
     );
 move_shape(Dir, Shape) ->
     move_shape(grids:dir_to_pos(Dir), Shape).

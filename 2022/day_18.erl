3e9ab4a8f22f1fdd7fa8811c6284e7a8d931df4a
Anton Yabchinskiy <arn@bestmx.net>
2024-09-16 13:32:08 +0300

    day_18: define surface_area/1

--- 2022/day_18.erl
+++ 2022/day_18.erl
@@ -2,39 +2,39 @@
 
 -export([main/1]).
 
+-define(AIR, $.).
+-define(LAVA, $#).
+
 -spec main(1..2) -> ok.
 main(1) ->
-    CubeMap =
+    Cubes =
         maps:from_list(
-            lists:map(
-                fun(Pos) ->
-                    {Pos, undefined}
-                end,
-                lists:map(
-                    fun(<<Line/bytes>>) ->
-                        [X, Y, Z] = binary:split(Line, <<",">>, [global, trim]),
-                        {binary_to_integer(X), binary_to_integer(Y), binary_to_integer(Z)}
-                    end,
-                    io_ext:read_lines(standard_io)
-                )
-            )
+            [
+                {Pos, ?LAVA}
+             || Pos <-
+                    lists:map(
+                        fun(<<Line/bytes>>) ->
+                            [X, Y, Z] = binary:split(Line, <<",">>, [global, trim]),
+                            {binary_to_integer(X), binary_to_integer(Y), binary_to_integer(Z)}
+                        end,
+                        io_ext:read_lines(standard_io)
+                    )
+            ]
         ),
-    NumExposedMap = maps:map(
-        fun({X, Y, Z}, _) ->
-            length([
-                AdjPos
-             || AdjPos <- [
-                    {X - 1, Y, Z},
-                    {X + 1, Y, Z},
-                    {X, Y - 1, Z},
-                    {X, Y + 1, Z},
-                    {X, Y, Z - 1},
-                    {X, Y, Z + 1}
-                ],
-                not maps:is_key(AdjPos, CubeMap)
-            ])
-        end,
-        CubeMap
-    ),
-    NumExposed = lists:sum(maps:values(NumExposedMap)),
-    io:format(<<"~b~n">>, [NumExposed]).
+    io:format(<<"~b~n">>, [surface_area(Cubes)]).
+
+-spec surface_area(map()) -> non_neg_integer().
+surface_area(Cubes) ->
+    length([
+        AdjPos
+     || {{X, Y, Z}, ?LAVA} <- maps:to_list(Cubes),
+        AdjPos <- [
+            {X - 1, Y, Z},
+            {X + 1, Y, Z},
+            {X, Y - 1, Z},
+            {X, Y + 1, Z},
+            {X, Y, Z - 1},
+            {X, Y, Z + 1}
+        ],
+        maps:get(AdjPos, Cubes, ?AIR) /= ?LAVA
+    ]).

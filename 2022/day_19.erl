eab9c348f45c40ec841eb854c97fd04034cbc4cf
Anton Yabchinskiy <arn@bestmx.net>
2024-09-17 00:36:33 +0300

    day_19: parse input

--- 2022/day_19.erl
+++ 2022/day_19.erl
@@ -1,7 +1,71 @@
 -module(day_19).
 
+-include_lib("eunit/include/eunit.hrl").
+
 -export([main/1]).
+
+-type ore_robot() :: #{ore := pos_integer()}.
+-type clay_robot() :: #{ore := pos_integer()}.
+-type obsidian_robot() :: #{ore := pos_integer(), clay := pos_integer()}.
+-type geode_robot() :: #{ore := pos_integer(), obsidian := pos_integer()}.
+-type blueprint() :: #{
+    id := integer(),
+    ore := ore_robot(),
+    clay := clay_robot(),
+    obsidian := obsidian_robot(),
+    geode := geode_robot()
+}.
 
 -spec main(1..2) -> ok.
 main(1) ->
+    Blueprints = lists:map(fun parse_blueprint/1, io_ext:read_lines(standard_io)),
+    ?debugFmt("Blueprints ~p", [Blueprints]),
     ok.
+
+-spec parse_blueprint(binary()) -> blueprint().
+parse_blueprint(Line) ->
+    [
+        <<"Blueprint">>,
+        ID,
+        <<"ore">>,
+        OreCostOre,
+        <<"ore">>,
+        <<"clay">>,
+        ClayCostOre,
+        <<"ore">>,
+        <<"obsidian">>,
+        ObsidianCostOre,
+        <<"ore">>,
+        ObsidianCostClay,
+        <<"clay">>,
+        <<"geode">>,
+        GeodeCostOre,
+        <<"ore">>,
+        GeodeCostObsidian,
+        <<"obsidian">>
+    ] = binary:split(
+        Line,
+        [
+            <<" ">>,
+            <<".">>,
+            <<":">>,
+            <<"Each">>,
+            <<"and">>,
+            <<"costs">>,
+            <<"robot">>
+        ],
+        [global, trim_all]
+    ),
+    #{
+        id => binary_to_integer(ID),
+        ore => #{ore => binary_to_integer(OreCostOre)},
+        clay => #{ore => binary_to_integer(ClayCostOre)},
+        obsidian => #{
+            ore => binary_to_integer(ObsidianCostOre),
+            clay => binary_to_integer(ObsidianCostClay)
+        },
+        geode => #{
+            ore => binary_to_integer(GeodeCostOre),
+            obsidian => binary_to_integer(GeodeCostObsidian)
+        }
+    }.

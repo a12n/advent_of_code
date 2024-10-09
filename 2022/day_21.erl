78cf773c2b323edec6bf0998cc7b0c362cdb7829
Anton Yabchinskiy <arn@bestmx.net>
2024-10-10 01:01:14 +0300

    day_21: separate parse_monkey/1

--- 2022/day_21.erl
+++ 2022/day_21.erl
@@ -2,28 +2,22 @@
 
 -export([main/1]).
 
+-type monkey_id() :: binary().
+-type monkey_descr() ::
+    {
+        monkey_id(),
+        integer()
+        | {
+            fun((integer(), integer()) -> integer()),
+            monkey_id() | integer(),
+            monkey_id() | integer()
+        }
+    }.
+
 -spec main(1..2) -> ok.
 main(1) ->
     %% Parse monkey definitions.
-    MonkeyDescrs =
-        lists:map(
-            fun(Line) ->
-                [ID | Descr] = binary:split(Line, [<<" ">>, <<":">>], [global, trim_all]),
-                case Descr of
-                    [Number] ->
-                        {ID, binary_to_integer(Number)};
-                    [LeftID, <<Op>>, RightID] ->
-                        {ID,
-                            case Op of
-                                $+ -> fun erlang:'+'/2;
-                                $- -> fun erlang:'-'/2;
-                                $* -> fun erlang:'*'/2;
-                                $/ -> fun erlang:'div'/2
-                            end, LeftID, RightID}
-                end
-            end,
-            io_ext:read_lines(standard_io)
-        ),
+    MonkeyDescrs = lists:map(fun parse_monkey/1, io_ext:read_lines(standard_io)),
     io:format(standard_error, "MonkeyDescrs ~p~n", [MonkeyDescrs]),
     %% Spawn monkey processes.
     {ok, _} = pg:start_link(),
@@ -53,6 +47,25 @@ main(1) ->
             end
         end)()
     ]).
+
+-spec parse_monkey(binary()) -> monkey_descr().
+parse_monkey(Line) ->
+    [ID | Descr] = binary:split(Line, [<<" ">>, <<":">>], [global, trim_all]),
+    case Descr of
+        [Number] ->
+            {ID, binary_to_integer(Number)};
+        [LeftID, <<Op>>, RightID] ->
+            {ID, {
+                case Op of
+                    $+ -> fun erlang:'+'/2;
+                    $- -> fun erlang:'-'/2;
+                    $* -> fun erlang:'*'/2;
+                    $/ -> fun erlang:'div'/2
+                end,
+                LeftID,
+                RightID
+            }}
+    end.
 
 %%--------------------------------------------------------------------
 %% Monkey processes

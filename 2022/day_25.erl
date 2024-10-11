8d2b34d6cae7277886f1f7be2930f6301dd7a93a
Anton Yabchinskiy <arn@bestmx.net>
2024-10-11 20:08:09 +0300

    day_25: add snafu_to_integer/1

--- 2022/day_25.erl
+++ 2022/day_25.erl
@@ -6,3 +6,25 @@
 -spec main(1..2) -> ok.
 main(1) ->
     ok.
+
+-spec snafu_to_integer(binary() | string()) -> integer().
+snafu_to_integer(SNAFU) when is_binary(SNAFU) -> snafu_to_integer(binary_to_list(SNAFU));
+snafu_to_integer(SNAFU) when is_list(SNAFU) ->
+    {Digits, _} = lists:mapfoldl(
+        fun(Digit5, Pow5) ->
+            {
+                Pow5 *
+                    case Digit5 of
+                        $2 -> 2;
+                        $1 -> 1;
+                        $0 -> 0;
+                        $- -> -1;
+                        $= -> -2
+                    end,
+                Pow5 * 5
+            }
+        end,
+        1,
+        lists:reverse(SNAFU)
+    ),
+    lists:sum(Digits).

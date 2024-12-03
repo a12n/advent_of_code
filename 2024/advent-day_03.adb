aefa8a88f837a68eee1b3d8ba48a1962d573c4b5
Anton Yabchinskiy <arn@bestmx.net>
2024-12-03 13:52:45 +0100

    day_03: handle Enabled flag

--- 2024/advent-day_03.adb
+++ 2024/advent-day_03.adb
@@ -1,5 +1,7 @@
 package body Advent.Day_03 is
-   procedure Get_Input_Entry (File : in File_Type; N, M : out Natural) is
+   procedure Get_Input_Entry (File    : in File_Type;
+                              N, M    : out Natural;
+                              Enabled : in out Boolean) is
       Next  : Character;
       State : Natural := 0;
    begin
@@ -28,6 +30,8 @@ package body Advent.Day_03 is
                   when 3 =>
                      N := 0;
                      State := 4;
+                  when 11 => State := 12;
+                  when 22 => State := 23;
                   when others => State := 0;
                end case;
             when '0' .. '9' =>
@@ -46,6 +50,37 @@ package body Advent.Day_03 is
             when ')' =>
                case State is
                   when 5 => return;
+                  when 12 =>
+                     Enabled := True;
+                     State := 0;
+                  when 23 =>
+                     Enabled := False;
+                     State := 0;
+                  when others => State := 0;
+               end case;
+            when 'd' =>
+               case State is
+                  when 0 => State := 10;
+                  when others => State := 0;
+               end case;
+            when 'o' =>
+               case State is
+                  when 10 => State := 11;
+                  when others => State := 0;
+               end case;
+            when 'n' =>
+               case State is
+                  when 11 => State := 20;
+                  when others => State := 0;
+               end case;
+            when ''' =>
+               case State is
+                  when 20 => State := 21;
+                  when others => State := 0;
+               end case;
+            when 't' =>
+               case State is
+                  when 21 => State := 22;
                   when others => State := 0;
                end case;
             when others =>
--- 2024/advent-day_03.ads
+++ 2024/advent-day_03.ads
@@ -1,5 +1,7 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package Advent.Day_03 is
-   procedure Get_Input_Entry (File : in File_Type; N, M : out Natural);
+   procedure Get_Input_Entry (File    : in File_Type;
+                              N, M    : out Natural;
+                              Enabled : in out Boolean);
 end Advent.Day_03;

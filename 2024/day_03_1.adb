2d8f2f81ac9f5388cc7f0af47421d7738876b0e5
Anton Yabchinskiy <arn@bestmx.net>
2024-12-03 13:53:01 +0100

    day_03_1: fixed call to Get_Input_Entry()

--- 2024/day_03_1.adb
+++ 2024/day_03_1.adb
@@ -4,13 +4,14 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Advent.Day_03; use Advent.Day_03;
 
 procedure Day_03_1 is
-   Sum : Natural := 0;
+   Sum     : Natural := 0;
+   Enabled : Boolean := True;
 begin
    loop
       declare
          N, M : Natural;
       begin
-         Get_Input_Entry (Standard_Input, N, M);
+         Get_Input_Entry (Standard_Input, N, M, Enabled);
          Sum := Sum + N * M;
       end;
    end loop;

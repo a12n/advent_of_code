55f02b5a7b179cb190e384761ec77fd1cc92cfaf
Anton Yabchinskiy <arn@bestmx.net>
2024-12-04 10:11:54 +0100

    day_04: read square grid in Advent.Day_04.Input

--- 2024/advent-day_04.adb
+++ 2024/advent-day_04.adb
@@ -1,10 +1,16 @@
 package body Advent.Day_04 is
    function Input (File : File_Type) return Word_Search is
-      --  Letters1 : Word_Search (1 .. 4, 1 .. 4) := [others => [others => 'A']];
-      --  Letters2 : Word_Search (1 .. 4, 1..4) := [others => [others => 'B']];
-      --  Line : constant String := Get_Line (File);
-      Line : constant String := "ABC";
+      Line    : constant String := Get_Line (File);
+      Letters : Word_Search (1 .. Line'Length, 1 .. Line'Length);
    begin
-      return Word_Search'(1 => Line);
+      for I in Line'Range loop
+         Letters (1, I) := Line (I);
+      end loop;
+      for I in 2 .. Letters'Last (1) loop
+         for J in Letters'Range (2) loop
+            Get (File, Letters (I, J));
+         end loop;
+      end loop;
+      return Letters;
    end Input;
 end Advent.Day_04;

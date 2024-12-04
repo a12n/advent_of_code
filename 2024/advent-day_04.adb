ce4e8cb7486dee27ee5aa527ceaf76095f65125d
Anton Yabchinskiy <arn@bestmx.net>
2024-12-04 19:32:01 +0100

    day_04: gnatpp

--- 2024/advent-day_04.adb
+++ 2024/advent-day_04.adb
@@ -14,10 +14,10 @@ package body Advent.Day_04 is
       return Letters;
    end Input;
 
-   function Has_Word (Letters : Word_Search;
-                      Word : String;
-                      Row, Col : Positive;
-                      Direction : Direction_Type) return Boolean is
+   function Has_Word
+     (Letters   : Word_Search; Word : String; Row, Col : Positive;
+      Direction : Direction_Type) return Boolean
+   is
    begin
       if Word'Length = 0 then
          return True;
@@ -26,30 +26,35 @@ package body Advent.Day_04 is
          return False;
       end if;
       if Row + Direction (1) not in Letters'Range (1) or
-        Col + Direction (2) not in Letters'Range (2) then
+        Col + Direction (2) not in Letters'Range (2)
+      then
          return False;
       end if;
-      return Has_Word (Letters, Word (Word'First + 1 .. Word'Last),
-        Row + Direction (1), Col + Direction (2), Direction);
+      return
+        Has_Word
+          (Letters, Word (Word'First + 1 .. Word'Last), Row + Direction (1),
+           Col + Direction (2), Direction);
    end Has_Word;
 
-   function Num_Words (Letters : Word_Search;
-                       Word : String;
-                       Row, Col : Positive) return Natural is
+   function Num_Words
+     (Letters : Word_Search; Word : String; Row, Col : Positive) return Natural
+   is
    begin
-      if Word'Length = 0 or Word (Word'First) /= Letters (Row,Col) then
+      if Word'Length = 0 or Word (Word'First) /= Letters (Row, Col) then
          return 0;
       end if;
-      return N:Natural :=0 do
-      for Vert in -1 .. 1 loop
-         for Horiz in -1 .. 1 loop
-            if Vert/=0 or Horiz /= 0 then
-               if Has_Word (Letters, Word, Row, Col, Direction_Type'(Vert, Horiz)) then
-                  N := N + 1;
+      return N : Natural := 0 do
+         for Vert in -1 .. 1 loop
+            for Horiz in -1 .. 1 loop
+               if Vert /= 0 or Horiz /= 0 then
+                  if Has_Word
+                      (Letters, Word, Row, Col, Direction_Type'(Vert, Horiz))
+                  then
+                     N := N + 1;
+                  end if;
                end if;
-            end if;
+            end loop;
          end loop;
-      end loop;
       end return;
    end Num_Words;
 end Advent.Day_04;
--- 2024/day_04_1.adb
+++ 2024/day_04_1.adb
@@ -1,10 +1,10 @@
-with Ada.Text_IO;   use Ada.Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Advent.Day_04; use Advent.Day_04;
+with Advent.Day_04;       use Advent.Day_04;
 
 procedure Day_04_1 is
    Letters : constant Word_Search := Input (Standard_Input);
-   Total   : Natural := 0;
+   Total   : Natural              := 0;
 begin
    for Row in Letters'Range (1) loop
       for Col in Letters'Range (2) loop

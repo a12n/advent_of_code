cc08c00abbf6bea699159100b9a0c0622d5324cd
Anton Yabchinskiy <arn@bestmx.net>
2024-12-07 01:02:09 +0100

    day_06: implement Walk

--- 2024/advent-day_06.adb
+++ 2024/advent-day_06.adb
@@ -42,8 +42,56 @@ package body Advent.Day_06 is
      (Obstructions : Obstruction_Map; Guard : Position; Guard_Dir : Direction)
       return Visited_Map
    is
-      Visited : Visited_Map (Obstructions'Range (1), Obstructions'Range (2));
+      function Advance (Pos : Position; Dir : Direction) return Position is
+      begin
+         case Dir is
+            when Down =>
+               return [Pos (1) + 1, Pos (2)];
+            when Left =>
+               return [Pos (1), Pos (2) - 1];
+            when Right =>
+               return [Pos (1), Pos (2) + 1];
+            when Up =>
+               return [Pos (1) - 1, Pos (2)];
+         end case;
+      end Advance;
+
+      function Rotate (Dir : Direction) return Direction is
+      begin
+         case Dir is
+            when Down =>
+               return Left;
+            when Left =>
+               return Up;
+            when Right =>
+               return Down;
+            when Up =>
+               return Right;
+         end case;
+      end Rotate;
+
+      Visited : Visited_Map (Obstructions'Range (1), Obstructions'Range (2)) :=
+        [others => [others => False]];
+      Pos     : Position := Guard;
+      Dir     : Direction := Guard_Dir;
    begin
-      return Visited;
+      Put_Line (Standard_Error, "Obstructions" & Obstructions'Image);
+      loop
+         declare
+            Next : constant Position := Advance (Pos, Dir);
+         begin
+            Put_Line
+              (Standard_Error, "Pos " & Pos'Image & ", Dir " & Dir'Image);
+            Visited (Pos (1), Pos (2)) := True;
+            if Obstructions (Next (1), Next (2)) then
+               Dir := Rotate (Dir);
+            else
+               Pos := Next;
+            end if;
+         exception
+            when Constraint_Error =>
+               return Visited;
+         end;
+      end loop;
    end Walk;
 end Advent.Day_06;

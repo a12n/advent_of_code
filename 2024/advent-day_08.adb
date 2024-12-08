08ab9bcb512131ec55e2fa67992c844ed76f3025
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 19:41:24 +0100

    Advent.Day_08: reorder functions

--- 2024/advent-day_08.adb
+++ 2024/advent-day_08.adb
@@ -1,25 +1,6 @@
 with Advent.Grids; use Advent.Grids;
 
 package body Advent.Day_08 is
-   function Input (File : File_Type) return Antenna_Map is
-      Line : constant String := Get_Line (File);
-
-      Antennas : Antenna_Map (Line'Range, Line'Range) :=
-        [others => [others => '.']];
-   begin
-      for Col in Line'Range loop
-         --  FIXME: Antenna range check.
-         Antennas (1, Col) := Line (Col);
-      end loop;
-      for Row in 2 .. Antennas'Last (1) loop
-         for Col in Antennas'Range (2) loop
-            --  FIXME: Antenna range check.
-            Get (File, Antennas (Row, Col));
-         end loop;
-      end loop;
-      return Antennas;
-   end Input;
-
    function Antenna_Positions
      (Antennas : Antenna_Map; Frequency : Antenna) return Position_Array
    is
@@ -38,6 +19,25 @@ package body Advent.Day_08 is
    begin
       return Iterate ([Antennas'First (1), Antennas'First (2)]);
    end Antenna_Positions;
+
+   function Input (File : File_Type) return Antenna_Map is
+      Line : constant String := Get_Line (File);
+
+      Antennas : Antenna_Map (Line'Range, Line'Range) :=
+        [others => [others => '.']];
+   begin
+      for Col in Line'Range loop
+         --  FIXME: Antenna range check.
+         Antennas (1, Col) := Line (Col);
+      end loop;
+      for Row in 2 .. Antennas'Last (1) loop
+         for Col in Antennas'Range (2) loop
+            --  FIXME: Antenna range check.
+            Get (File, Antennas (Row, Col));
+         end loop;
+      end loop;
+      return Antennas;
+   end Input;
 
    procedure Mark_Antinodes
      (Antinodes : in out Antinode_Map; Antennas : in Antenna_Map;

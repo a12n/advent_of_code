c8f5ad652141ee2dcb63f1984db35bccf88d0adc
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 17:18:57 +0100

    Advent.Day_07: implement Input_Entry

--- 2024/advent-day_07.adb
+++ 2024/advent-day_07.adb
@@ -1,21 +1,27 @@
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
+with Ada.Text_IO;         use Ada.Text_IO;
 
 package body Advent.Day_07 is
    function Input_Entry
      (File : in File_Type; Test : out Number) return Number_Array
    is
-      Line : constant String := Get_Line (File);
-      Last : Positive;
+      Line  : constant String  := Get_Line (File);
+      Colon : constant Natural := Index (Line, ":", 1);
+      Last  : Positive;
 
       function Input_Numbers (Line : String) return Number_Array is
+         N : Number;
       begin
-         return Number_Array'[];
+         if Line'Length = 0 then
+            return Number_Array'[];
+         end if;
+         Get (Line, N, Last);
+         return
+           Number_Array'[N] & Input_Numbers (Line (Last + 1 .. Line'Last));
       end Input_Numbers;
    begin
-      Get (Line, Test, Last);
-      if Line (Last + 1) /= ':' then
-         raise Constraint_Error;
-      end if;
-      return Input_Numbers (Line (Last + 2 .. Line'Last));
+      Get (Line (Line'First .. Colon - 1), Test, Last);
+      return Input_Numbers (Line (Colon + 1 .. Line'Last));
    end Input_Entry;
 end Advent.Day_07;

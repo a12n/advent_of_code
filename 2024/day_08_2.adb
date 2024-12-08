7f6acca2891f05f166bea6af58446e2229bdb488
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 15:23:46 +0100

    day_08_2: call Mark_Antinodes with Resonant

--- 2024/day_08_2.adb
+++ 2024/day_08_2.adb
@@ -1,4 +1,13 @@
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Advent.Day_08;       use Advent.Day_08;
+
 procedure Day_08_2 is
+   Antennas  : constant Antenna_Map := Input (Standard_Input);
+   Antinodes : Antinode_Map (Antennas'Range (1), Antennas'Range (2)) :=
+     [others => [others => False]];
 begin
-   null;
+   Mark_Antinodes (Antinodes, Antennas, True);
+   Put (Number_Antinodes (Antinodes), 0);
+   New_Line;
 end Day_08_2;

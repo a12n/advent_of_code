1832b808febe929451ad658b879494e2f152740a
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 12:08:27 +0100

    Advent.Grids: add Is_Horizontal_Line and Is_Vertical_Line

--- 2024/advent-grids.ads
+++ 2024/advent-grids.ads
@@ -11,6 +11,12 @@ package Advent.Grids is
    To_Offset : constant array (Direction) of Offset :=
      [Down => [1, 0], Left => [0, -1], Right => [0, 1], Up => [-1, 0]];
 
+   function Is_Horizontal_Line (P, Q : Position) return Boolean is
+     (P (1) = Q (1) and P (2) /= Q (2));
+
+   function Is_Vertical_Line (P, Q : Position) return Boolean is
+     (P (2) = Q (2) and P (1) /= Q (1));
+
    function "+" (P : Position; V : Offset) return Position is
      [P (1) + V (1), P (2) + V (2)];
 end Advent.Grids;

74b7445e6137210da88653809c546313f1bafb59
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 01:41:00 +0100

    Advent.Day_06: adjust Post conditions for Input and Walk

--- 2024/advent-day_06.ads
+++ 2024/advent-day_06.ads
@@ -11,12 +11,14 @@ package Advent.Day_06 is
       return Obstruction_Map with
      Post =>
       Guard (1) in Input'Result'Range (1) and
-      Guard (2) in Input'Result'Range (2);
+      Guard (2) in Input'Result'Range (2) and
+      not Input'Result (Guard (1), Guard (2));
 
    function Walk
      (Obstructions : Obstruction_Map; Guard : Position; Guard_Dir : Direction)
       return Visited_Map with
      Pre =>
       Guard (1) in Obstructions'Range (1) and
-      Guard (2) in Obstructions'Range (2);
+      Guard (2) in Obstructions'Range (2) and
+      not Obstructions (Guard (1), Guard (2));
 end Advent.Day_06;

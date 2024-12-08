b05da93741145a588aa79d948ce3afffd678ac36
Anton Yabchinskiy <arn@bestmx.net>
2024-12-08 14:26:52 +0100

    Advent.Day_08: internal Antenna_Positions function

--- 2024/advent-day_08.adb
+++ 2024/advent-day_08.adb
@@ -1,3 +1,5 @@
+with Advent.Grids; use Advent.Grids;
+
 package body Advent.Day_08 is
    function Input (File : File_Type) return Antenna_Map is
       Line : constant String := Get_Line (File);
@@ -17,6 +19,25 @@ package body Advent.Day_08 is
       end loop;
       return Antennas;
    end Input;
+
+   function Antenna_Positions
+     (Antennas : Antenna_Map; Frequency : Antenna) return Position_Array
+   is
+      function Iterate (Pos : Position) return Position_Array is
+      begin
+         if Pos (1) > Antennas'Last (1) then
+            return [];
+         elsif Pos (2) > Antennas'Last (2) then
+            return Iterate ([Pos (1) + 1, Antennas'First (2)]);
+         elsif Antennas (Pos (1), Pos (2)) = Frequency then
+            return Position_Array'[Pos] & Iterate ([Pos (1), Pos (2) + 1]);
+         else
+            return Iterate ([Pos (1), Pos (2) + 1]);
+         end if;
+      end Iterate;
+   begin
+      return Iterate ([Antennas'First (1), Antennas'First (2)]);
+   end Antenna_Positions;
 
    function Number_Antinodes
      (Antennas : Antenna_Map; Frequency : Antenna) return Natural

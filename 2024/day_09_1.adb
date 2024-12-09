e83caefb74cde9c2e673cca2e88999da53699e3d
Anton Yabchinskiy <arn@bestmx.net>
2024-12-10 00:34:07 +0100

    day_09_1: implement with Rearrange and Checksum

--- 2024/day_09_1.adb
+++ 2024/day_09_1.adb
@@ -1,10 +1,11 @@
-with Ada.Text_IO;   use Ada.Text_IO;
-with Advent.Day_09; use Advent.Day_09;
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Advent.Day_09;       use Advent.Day_09;
 
 procedure Day_09_1 is
    Blocks : Block_Array := Input (Standard_Input);
 begin
-   --  Put_Line (Standard_Error, Blocks'Image);
-   --  TODO
-   null;
+   Rearrange (Blocks);
+   Put (Checksum (Blocks), 0);
+   New_Line;
 end Day_09_1;

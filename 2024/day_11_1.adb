5ace0bb18d131528c7399c901a6590bad9a1a3f0
Anton Yabchinskiy <arn@bestmx.net>
2024-12-13 00:24:12 +0100

    day_11: remove debug output

--- 2024/day_11_1.adb
+++ 2024/day_11_1.adb
@@ -9,10 +9,7 @@ procedure Day_11_1 is
 begin
    loop
       Get (Standard_Input, Stone);
-      Put_Line
-        (Standard_Error, "Stone " & Stone'Image & ", Count " & Count'Image);
       Count := Count + 1 + Splitter.Number_Stones (Stone, 25);
-      Put_Line (Standard_Error, "Count " & Count'Image);
    end loop;
 exception
    when End_Error =>
--- 2024/day_11_2.adb
+++ 2024/day_11_2.adb
@@ -9,10 +9,7 @@ procedure Day_11_2 is
 begin
    loop
       Get (Standard_Input, Stone);
-      Put_Line
-        (Standard_Error, "Stone " & Stone'Image & ", Count " & Count'Image);
       Count := Count + 1 + Splitter.Number_Stones (Stone, 75);
-      Put_Line (Standard_Error, "Count " & Count'Image);
    end loop;
 exception
    when End_Error =>

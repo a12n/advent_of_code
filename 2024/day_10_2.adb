7c7eabb10f1a28e932c6506cef533570f185326e
Anton Yabchinskiy <arn@bestmx.net>
2024-12-12 01:08:53 +0100

    day_10_2: rename Heights, remove debug output

--- 2024/day_10_2.adb
+++ 2024/day_10_2.adb
@@ -3,16 +3,13 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Advent.Day_10;       use Advent.Day_10;
 
 procedure Day_10_2 is
-   Map : constant Height_Map := Input (Standard_Input);
-   Sum : Natural             := 0;
+   Heights : constant Height_Map := Input (Standard_Input);
+   Sum     : Natural             := 0;
 begin
-   Put_Line (Standard_Error, Map'Image);
-   for I in Map'Range (1) loop
-      for J in Map'Range (2) loop
-         if Map (I, J) = 0 then
-            Put_Line (Standard_Error, I'Image & J'Image & Sum'Image);
-            Sum := Sum + Number_Trails (Map, [I, J]);
-            Put_Line (Standard_Error, Sum'Image);
+   for I in Heights'Range (1) loop
+      for J in Heights'Range (2) loop
+         if Heights (I, J) = 0 then
+            Sum := Sum + Number_Trails (Heights, [I, J]);
          end if;
       end loop;
    end loop;

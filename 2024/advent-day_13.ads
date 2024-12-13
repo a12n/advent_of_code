3e380fc2cce6df4221e05f8910aa4bc5e1aaa0ca
Anton Yabchinskiy <arn@bestmx.net>
2024-12-13 14:09:49 +0100

    day_13: import notes

---
+++ 2024/advent-day_13.ads
@@ -0,0 +1,17 @@
+package Advent.Day_13 is
+   --  Button A: X+94, Y+34
+   --  Button B: X+22, Y+67
+   --  Prize: X=8400, Y=5400
+   --
+   --  A = (94, 34)
+   --  B = (22, 67)
+   --  Prize = (8400, 5400)
+   --  Prize = n * A + m * B
+   --  (8400, 5400) = n (94, 34) + m (22, 67)
+   --  Cost = 3 n + 1 m
+   --
+   --  n ∈ [0, 100], m ∈ [0, 100]
+   --  min Cost
+   --
+   --  Maybe infeasible.
+end Advent.Day_13;

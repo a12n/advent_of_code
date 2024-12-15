ba5515760b94c03fe753a030425d3100d58a6621
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 13:34:27 +0100

    Advent.Day_14: X_Position and Y_Position types for actual input

--- 2024/advent-day_14.ads
+++ 2024/advent-day_14.ads
@@ -1,13 +1,10 @@
 with Ada.Text_IO; use Ada.Text_IO;
 
 package Advent.Day_14 is
-   --  For sample input.
-   type X_Position is mod 11;
-   type Y_Position is mod 7;
-
-   --  --  For actual input.
-   --  type X_Position is mod 101;
-   --  type Y_Position is mod 103;
+   --  type X_Position is mod 11;
+   --  type Y_Position is mod 7;
+   type X_Position is mod 101;
+   type Y_Position is mod 103;
 
    type Position is record
       X : X_Position;

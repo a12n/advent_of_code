2dca83186ec8230935e97c7b5b75a642cb54c1f3
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 22:16:17 +0100

    day_12_2: WIP

--- 2024/day_12_2.adb
+++ 2024/day_12_2.adb
@@ -1,4 +1,29 @@
+with Ada.Text_IO;   use Ada.Text_IO;
+with Advent.Day_12; use Advent.Day_12;
+
 procedure Day_12_2 is
+   Plants  : constant Garden := Input (Standard_Input);
+   Visited : Visited_Map (Plants'Range (1), Plants'Range (2)) :=
+     [others => [others => False]];
 begin
-   null;
+   for Row in Plants'Range (1) loop
+      for Col in Plants'Range (2) loop
+         if not Visited (Row, Col) then
+            declare
+               Area    : Natural;
+               Polygon : constant Visited_Map :=
+                 Flood_Fill (Plants, Row, Col, Area);
+            begin
+               Put_Line
+                 (Standard_Error,
+                  "Polygon" & Polygon'Image & ", Area " & Area'Image);
+               for I in Polygon'Range (1) loop
+                  for J in Polygon'Range (2) loop
+                     Visited (I, J) := Visited (I, J) or Polygon (I, J);
+                  end loop;
+               end loop;
+            end;
+         end if;
+      end loop;
+   end loop;
 end Day_12_2;

600f01248ccdaeb30da46c3f2065a7dbd694951f
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 13:57:43 +0100

    Day_14_2: Input and Print

--- 2024/day_14_2.adb
+++ 2024/day_14_2.adb
@@ -1,4 +1,8 @@
+with Ada.Text_IO;   use Ada.Text_IO;
+with Advent.Day_14; use Advent.Day_14;
+
 procedure Day_14_2 is
+   Robots : Robot_Array := Input (Standard_Input);
 begin
-   null;
+   Print (Standard_Error, Robots);
 end Day_14_2;

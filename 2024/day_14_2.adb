84fbee8d6d8f7c91679cea9e687c2c31db3161da
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 14:20:30 +0100

    Day_14_2: solution

--- 2024/day_14_2.adb
+++ 2024/day_14_2.adb
@@ -2,7 +2,22 @@ with Ada.Text_IO;   use Ada.Text_IO;
 with Advent.Day_14; use Advent.Day_14;
 
 procedure Day_14_2 is
-   Robots : Robot_Array := Input (Standard_Input);
+   Robots   : Robot_Array := Input (Standard_Input);
+   Min_Dist : Natural     := Natural'Last;
 begin
-   Print (Standard_Error, Robots);
+   for Time in Natural'Range loop
+      declare
+         Dist : constant Natural := Distance (Robots);
+      begin
+         if Dist < Min_Dist then
+            Min_Dist := Dist;
+            Put_Line
+              (Standard_Error,
+               ASCII.ESC & "[2JAfter " & Time'Image & ", Distance " &
+               Dist'Image);
+            Print (Standard_Error, Robots);
+         end if;
+      end;
+      Simulate (Robots, 1);
+   end loop;
 end Day_14_2;

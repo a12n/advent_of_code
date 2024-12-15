51255464c4280149e76489a26ec3dc09ab08f2b5
Anton Yabchinskiy <arn@bestmx.net>
2024-12-16 00:19:27 +0100

    Day_15_2: read warehouse, widen

--- 2024/day_15_2.adb
+++ 2024/day_15_2.adb
@@ -1,4 +1,13 @@
+with Ada.Text_IO;   use Ada.Text_IO;
+with Advent.Day_15; use Advent.Day_15;
+with Advent.Grids;  use Advent.Grids;
+
 procedure Day_15_2 is
+   Robot_Pos : Position;
+   Warehouse : Wide_Warehouse_Map :=
+     Widen (Get_Warehouse (Standard_Input, Robot_Pos), Robot_Pos);
 begin
-   null;
+   Put_Line (Standard_Error, "Robot_Pos" & Robot_Pos'Image);
+   Put_Line (Standard_Error, "Warehouse" & Warehouse'Image);
+   --  TODO
 end Day_15_2;

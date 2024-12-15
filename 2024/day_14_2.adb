226288eac25e91cd0cb963e7499d007e641f9d1a
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 14:27:04 +0100

    Day_14_2: print time of minimal distance

--- 2024/day_14_2.adb
+++ 2024/day_14_2.adb
@@ -1,16 +1,19 @@
-with Ada.Text_IO;   use Ada.Text_IO;
-with Advent.Day_14; use Advent.Day_14;
+with Ada.Text_IO;         use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Advent.Day_14;       use Advent.Day_14;
 
 procedure Day_14_2 is
    Robots   : Robot_Array := Input (Standard_Input);
    Min_Dist : Natural     := Natural'Last;
+   Min_Time : Natural;
 begin
-   for Time in Natural'Range loop
+   for Time in 0 .. 10_000 loop
       declare
          Dist : constant Natural := Distance (Robots);
       begin
          if Dist < Min_Dist then
             Min_Dist := Dist;
+            Min_Time := Time;
             Put_Line
               (Standard_Error,
                ASCII.ESC & "[2JAfter " & Time'Image & ", Distance " &
@@ -20,4 +23,6 @@ begin
       end;
       Simulate (Robots, 1);
    end loop;
+   Put (Min_Time, 0);
+   New_Line;
 end Day_14_2;

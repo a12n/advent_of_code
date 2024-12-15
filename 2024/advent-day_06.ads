d12b52e7299e8fbb484eee6d13a7e6cb643bdbe4
Anton Yabchinskiy <arn@bestmx.net>
2024-12-15 12:23:33 +0100

    Advent.Day_06: remove old functions

--- 2024/advent-day_06.ads
+++ 2024/advent-day_06.ads
@@ -20,35 +20,4 @@ package Advent.Day_06 is
    procedure Walk
      (Blocked :    Blocked_Map; Guard_Pos : Position; Guard_Dir : Direction;
       Visited : in out Visited_Map; Number_Loops : out Natural);
-
-   type Boolean_Grid is
-     array (Positive range <>, Positive range <>) of Boolean;
-   type Polygonal_Chain is array (Positive range <>) of Position;
-
-   function Input
-     (File : in File_Type; Guard : out Position; Guard_Dir : out Direction)
-      return Boolean_Grid with
-     Post =>
-      Guard (1) in Input'Result'Range (1) and
-      Guard (2) in Input'Result'Range (2) and
-      not Input'Result (Guard (1), Guard (2));
-
-   function Loop_Obstructions (Path : Polygonal_Chain) return Position_Array;
-
-   function Number_Loops
-     (Blocked : Boolean_Grid; Path : Polygonal_Chain) return Natural;
-
-   function Walk
-     (Blocked : Boolean_Grid; Guard : Position; Guard_Dir : Direction)
-      return Boolean_Grid with
-     Pre =>
-      Guard (1) in Blocked'Range (1) and Guard (2) in Blocked'Range (2) and
-      not Blocked (Guard (1), Guard (2));
-
-   function Walk
-     (Blocked : Boolean_Grid; Guard : Position; Guard_Dir : Direction)
-      return Polygonal_Chain with
-     Pre =>
-      Guard (1) in Blocked'Range (1) and Guard (2) in Blocked'Range (2) and
-      not Blocked (Guard (1), Guard (2));
 end Advent.Day_06;

c4baeb1470a8ff87e8b6a8690deed05cfe252a19
Anton Yabchinskiy <arn@bestmx.net>
2024-12-16 17:40:44 +0100

    Day_15_2: compute coordinates, print sum

--- 2024/day_15_2.adb
+++ 2024/day_15_2.adb
@@ -1,8 +1,9 @@
-with Ada.Text_IO;   use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with Ada.Text_IO;         use Ada.Text_IO;
 with Advent.ANSI;
-with Advent.Day_15; use Advent.Day_15;
-with Advent.Grids;  use Advent.Grids;
-with Advent;        use Advent;
+with Advent.Day_15;       use Advent.Day_15;
+with Advent.Grids;        use Advent.Grids;
+with Advent;              use Advent;
 
 procedure Day_15_2 is
    Robot_Pos, Unused : Position;
@@ -20,7 +21,7 @@ begin
    Print (Standard_Error, Warehouse);   --  50x100
    begin
       for I in Positive'Range loop
-         delay 0.33;
+         delay 0.0125;
 
          Next_Warehouse := Warehouse;
          Next_Robot_Pos :=
@@ -39,5 +40,17 @@ begin
    end;
    Put (Standard_Error, ANSI.Cursor.Show);
 
-   --  TODO
+   declare
+      Sum : Natural := 0;
+   begin
+      for Row in Warehouse'Range (1) loop
+         for Col in Warehouse'Range (2) loop
+            if Warehouse (Row, Col) = Box then
+               Sum := Sum + GPS_Coordinate ([Row, Col]);
+            end if;
+         end loop;
+      end loop;
+      Put (Sum, 0);
+      New_Line;
+   end;
 end Day_15_2;

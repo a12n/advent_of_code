acaf7abdf61ccc9d9eb77495d7ab82a5539fc64e
Anton Yabchinskiy <arn@bestmx.net>
2024-12-16 13:44:40 +0100

    Advent.Day_16: implement Best_Path

--- 2024/advent-day_16.adb
+++ 2024/advent-day_16.adb
@@ -57,23 +57,47 @@ package body Advent.Day_16 is
       function Get_Priority (S : State) return Natural is (S.Cost);
       function Before (N, M : Natural) return Boolean renames "<";
 
-      package State_Interface is new Ada.Containers
+      package State_Queue_Interface is new Ada.Containers
         .Synchronized_Queue_Interfaces
         (State);
-      package Priority_Queues is new Ada.Containers.Bounded_Priority_Queues
-        (Queue_Interfaces => State_Interface, Queue_Priority => Natural,
+      package State_Queues is new Ada.Containers.Bounded_Priority_Queues
+        (Queue_Interfaces => State_Queue_Interface, Queue_Priority => Natural,
          Default_Capacity => 16_384);
 
-      use Priority_Queues;
+      use State_Queues;
 
       Q : Queue;
       S : State;
+
+      Visited : array (Maze'Range (1), Maze'Range (2), Direction) of Boolean :=
+        [others => [others => [others => False]]];
    begin
       Q.Enqueue ((Start_Pos, Start_Dir, 0));
       while Q.Current_Use > 0 loop
          Q.Dequeue (S);
+         Put_Line (Standard_Error, Q.Current_Use'Image & S'Image);
+
          if S.Pos = Finish_Pos then
             return S.Cost;
+         end if;
+
+         declare
+            Next_Pos : constant Position := S.Pos + To_Offset (S.Dir);
+         begin
+            if not Visited (Next_Pos (1), Next_Pos (2), S.Dir) and
+              Maze (Next_Pos (1), Next_Pos (2)) = Empty
+            then
+               Q.Enqueue ((Next_Pos, S.Dir, S.Cost + 1));
+               Visited (Next_Pos (1), Next_Pos (2), S.Dir) := True;
+            end if;
+         end;
+
+         if not Visited (S.Pos (1), S.Pos (2), Rotate (CW, S.Dir)) then
+            Q.Enqueue ((S.Pos, Rotate (CW, S.Dir), S.Cost + 1_000));
+         end if;
+
+         if not Visited (S.Pos (1), S.Pos (2), Rotate (CCW, S.Dir)) then
+            Q.Enqueue ((S.Pos, Rotate (CCW, S.Dir), S.Cost + 1_000));
          end if;
       end loop;
       raise Constraint_Error;

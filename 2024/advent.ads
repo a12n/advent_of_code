6107a5f57c0ad74d7b3198c367c17d12b753f23f
Anton Yabchinskiy <arn@bestmx.net>
2024-12-18 01:58:14 +0100

    Advent: add Debug_Level constant

--- 2024/advent.ads
+++ 2024/advent.ads
@@ -1,5 +1,7 @@
 with Ada.Environment_Variables;
 
 package Advent is
-   Debug : constant Boolean := Ada.Environment_Variables.Exists ("DEBUG");
+   Debug_Level : constant Natural :=
+     Natural'Value (Ada.Environment_Variables.Value ("DEBUG", "0"));
+   Debug       : constant Boolean := Debug_Level > 0;
 end Advent;

9feadd2a51a5024df597a268b31c9969ecba2b17
Anton Yabchinskiy <arn@bestmx.net>
2024-12-18 16:28:47 +0100

    Day_18_1: debug output

--- 2024/day_18_1.adb
+++ 2024/day_18_1.adb
@@ -2,6 +2,7 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO;         use Ada.Text_IO;
 with Advent.Day_18;       use Advent.Day_18;
 with Advent.Grids;        use Advent.Grids;
+with Advent;              use Advent;
 
 procedure Day_18_1 is
    Size    : constant Natural  := 6;
@@ -20,9 +21,17 @@ begin
       declare
          Pos : constant Position := Get_Byte_Position (Standard_Input);
       begin
+         if Debug then
+            Put_Line (Standard_Error, I'Image & ":" & Pos'Image);
+         end if;
          Corrupted (Pos (1), Pos (2)) := True;
       end;
    end loop;
+
+   if Debug then
+      Print (Standard_Error, Corrupted);
+   end if;
+
    Put (Shortest_Path (Corrupted, Start_Pos, Finish_Pos), 0);
    New_Line;
 end Day_18_1;

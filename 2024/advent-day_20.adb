312d7d5289b751bd9abc60da5ea0eb23cb23a3bc
Anton Yabchinskiy <arn@bestmx.net>
2024-12-22 15:53:26 +0100

    Advent.Day_20: split Shortest_Path into multiple functions/procedures, export Find_Shortest_Path with access to Distance and Previous matrices

--- 2024/advent-day_20.adb
+++ 2024/advent-day_20.adb
@@ -3,21 +3,65 @@ with Ada.Containers.Unbounded_Priority_Queues;
 with Ada.Containers; use Ada.Containers;
 
 package body Advent.Day_20 is
+   procedure Find_Shortest_Path
+     (Track    :     Racetrack_Type; Start_Pos, Finish_Pos : Position;
+      Previous : out Previous_Map; Distance : out Distance_Map)
+   is
+      function Get_Priority (Pos : Position) return Natural is
+        (Distance (Pos (1), Pos (2)));
+
+      package Position_Queue_Interface is new Ada.Containers
+        .Synchronized_Queue_Interfaces
+        (Position);
+
+      package Position_Queues is new Ada.Containers.Unbounded_Priority_Queues
+        (Queue_Interfaces => Position_Queue_Interface,
+         Queue_Priority   => Natural, Before => "<");
+
+      use Position_Queues;
+
+      Q             : Queue;
+      New_Dist      : Natural;
+      Pos, Next_Pos : Position;
+   begin
+      Distance := [others => [others => Natural'Last]];
+      Previous := [others => [others => None]];
+
+      Distance (Start_Pos (1), Start_Pos (2)) := 0;
+      Q.Enqueue (Start_Pos);
+
+      while Q.Current_Use > 0 loop
+         Q.Dequeue (Pos);
+
+         exit when Pos = Finish_Pos;
+
+         for Dir in Direction'Range loop
+            Next_Pos := Pos + To_Offset (Dir);
+            New_Dist := Distance (Pos (1), Pos (2)) + 1;
+
+            if Track (Next_Pos (1), Next_Pos (2)) = Empty and
+              New_Dist < Distance (Next_Pos (1), Next_Pos (2))
+            then
+               Distance (Next_Pos (1), Next_Pos (2)) := New_Dist;
+               Previous (Next_Pos (1), Next_Pos (2)) := Opposite (Dir);
+               Q.Enqueue (Next_Pos);
+            end if;
+         end loop;
+      end loop;
+
+      if Distance (Finish_Pos (1), Finish_Pos (2)) = Natural'Last then
+         --  No path.
+         raise Constraint_Error;
+      end if;
+   end Find_Shortest_Path;
+
    function Shortest_Path
-     (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position)
+     (Previous : Previous_Map; Start_Pos, Finish_Pos : Position)
       return Position_Array
    is
-      Distances : array (Track'Range (1), Track'Range (2)) of Natural :=
-        [others => [others => Natural'Last]];
-
-      Previous : array (Track'Range (1), Track'Range (2)) of Maybe_Direction :=
-        [others => [others => None]];
-
       --  Buffer to store the shortest path. A slice of this array is
       --  returned by the function as a result.
-      Path :
-        Position_Array
-          (1 .. ((Track'Length (1) - 2) * (Track'Length (2) - 2)));
+      Path : Position_Array (1 .. (Previous'Length (1) * Previous'Length (2)));
 
       function Backtrack
         (Pos : Position; Path_Index : Positive) return Positive
@@ -38,51 +82,28 @@ package body Advent.Day_20 is
                  Path_Index - 1);
          end if;
       end Backtrack;
-
-      function Get_Priority (Pos : Position) return Natural is
-        (Distances (Pos (1), Pos (2)));
-
-      package Position_Queue_Interface is new Ada.Containers
-        .Synchronized_Queue_Interfaces
-        (Position);
-
-      package Position_Queues is new Ada.Containers.Unbounded_Priority_Queues
-        (Queue_Interfaces => Position_Queue_Interface,
-         Queue_Priority   => Natural, Before => "<");
-
-      use Position_Queues;
-
-      Q             : Queue;
-      New_Dist      : Natural;
-      Pos, Next_Pos : Position;
    begin
-      Distances (Start_Pos (1), Start_Pos (2)) := 0;
-      Q.Enqueue (Start_Pos);
-
-      while Q.Current_Use > 0 loop
-         Q.Dequeue (Pos);
-
-         exit when Pos = Finish_Pos;
-
-         for Dir in Direction'Range loop
-            Next_Pos := Pos + To_Offset (Dir);
-            New_Dist := Distances (Pos (1), Pos (2)) + 1;
-
-            if Track (Next_Pos (1), Next_Pos (2)) = Empty and
-              New_Dist < Distances (Next_Pos (1), Next_Pos (2))
-            then
-               Distances (Next_Pos (1), Next_Pos (2)) := New_Dist;
-               Previous (Next_Pos (1), Next_Pos (2))  := Opposite (Dir);
-               Q.Enqueue (Next_Pos);
-            end if;
-         end loop;
-      end loop;
-
-      if Distances (Finish_Pos (1), Finish_Pos (2)) = Natural'Last then
-         --  No path.
-         raise Constraint_Error;
-      end if;
-
       return Path (Backtrack (Finish_Pos, Path'Last) .. Path'Last);
    end Shortest_Path;
+
+   function Shortest_Path
+     (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position)
+      return Position_Array
+   is
+      Distance : Distance_Map (Track'Range (1), Track'Range (2));
+      Previous : Previous_Map (Track'Range (1), Track'Range (2));
+   begin
+      Find_Shortest_Path (Track, Start_Pos, Finish_Pos, Previous, Distance);
+      return Shortest_Path (Previous, Start_Pos, Finish_Pos);
+   end Shortest_Path;
+
+   function Shortest_Path_Length
+     (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position) return Natural
+   is
+      Distance : Distance_Map (Track'Range (1), Track'Range (2));
+      Previous : Previous_Map (Track'Range (1), Track'Range (2));
+   begin
+      Find_Shortest_Path (Track, Start_Pos, Finish_Pos, Previous, Distance);
+      return Distance (Finish_Pos (1), Finish_Pos (2));
+   end Shortest_Path_Length;
 end Advent.Day_20;
--- 2024/advent-day_20.ads
+++ 2024/advent-day_20.ads
@@ -6,21 +6,52 @@ with Advent.Grids; use Advent.Grids;
 package Advent.Day_20 is
    subtype Racetrack_Type is Day_16.Maze_Type;
 
+   --  Distance from the origin position to a position on the track.
+   type Distance_Map is
+     array (Positive range <>, Positive range <>) of Natural;
+
+   --  Direction to the previous position in a shortest path.
+   type Previous_Map is
+     array (Positive range <>, Positive range <>) of Maybe_Direction;
+
+   procedure Find_Shortest_Path
+     (Track    :     Racetrack_Type; Start_Pos, Finish_Pos : Position;
+      Previous : out Previous_Map; Distance : out Distance_Map) with
+     Pre =>
+      Start_Pos (1) in Track'Range (1) and Start_Pos (2) in Track'Range (2) and
+      Finish_Pos (1) in Track'Range (1) and
+      Finish_Pos (2) in Track'Range (2) and
+      Previous'Length (1) = Track'Length (1) and
+      Previous'Length (2) = Track'Length (2) and
+      Distance'Length (1) = Track'Length (1) and
+      Distance'Length (2) = Track'Length (2);
+
    function Get_Racetrack
      (File : File_Type; Start_Pos, Finish_Pos : out Position)
       return Racetrack_Type renames
      Day_16.Get_Maze;
 
+   --  Backtrack shortest path positions from the previous map of a
+   --  found shortest path.
+   function Shortest_Path
+     (Previous : Previous_Map; Start_Pos, Finish_Pos : Position)
+      return Position_Array with
+     Pre =>
+      Start_Pos (1) in Previous'Range (1) and
+      Start_Pos (2) in Previous'Range (2) and
+      Finish_Pos (1) in Previous'Range (1) and
+      Finish_Pos (2) in Previous'Range (2) and
+      Previous (Start_Pos (1), Start_Pos (2)) = None and
+      Previous (Finish_Pos (1), Finish_Pos (2)) /= None;
+
+   --  Find shortest path and then backtrack path positions.
    function Shortest_Path
      (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position)
       return Position_Array;
 
+   --  Find shortest path and get distance from start to finish.
    function Shortest_Path_Length
-     (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position)
-      return Natural is
-     (Shortest_Path (Track, Start_Pos, Finish_Pos)'Length -
-      1 --  Path from Start_Pos to Start_Pos has length 0
-   );
+     (Track : Racetrack_Type; Start_Pos, Finish_Pos : Position) return Natural;
 
    procedure Print
      (File       : File_Type; Track, Paths : Racetrack_Type;

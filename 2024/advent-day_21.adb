21e9d2c5c0d381ff558249cdf7ddabf64b8a6e9a
Anton Yabchinskiy <arn@bestmx.net>
2024-12-22 18:52:14 +0100

    Advent.Day_21: convert from Character to Numeric_Key

--- 2024/advent-day_21.adb
+++ 2024/advent-day_21.adb
@@ -1,11 +1,20 @@
 package body Advent.Day_21 is
    function Get_Code (File : File_Type) return Numeric_Presses is
-      Line : constant String := Get_Line (File);
+      Line    : constant String := Get_Line (File);
+      Presses : Numeric_Presses (Line'Range);
    begin
-      return
-        Numeric_Presses'
-          [Numeric_Key'Value (Line (1 .. 1)),
-          Numeric_Key'Value (Line (2 .. 2)), Numeric_Key'Value (Line (3 .. 3)),
-          Numeric_Key'Value (Line (4 .. 4))];
+      for I in Line'Range loop
+         case Line (I) is
+            when '0' .. '9' =>
+               Presses (I) :=
+                 Numeric_Key'Val
+                   (Character'Pos (Line (I)) - Character'Pos ('0'));
+            when 'A' =>
+               Presses (I) := 'A';
+            when others =>
+               raise Constraint_Error;
+         end case;
+      end loop;
+      return Presses;
    end Get_Code;
 end Advent.Day_21;

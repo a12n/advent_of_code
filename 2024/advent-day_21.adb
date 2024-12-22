a5946b2b179540db7d4e6eb9deb7dae672b387f7
Anton Yabchinskiy <arn@bestmx.net>
2024-12-22 20:30:12 +0100

    Advent.Day_21: Translate from Numeric_Key to Numeric_Key

--- 2024/advent-day_21.adb
+++ 2024/advent-day_21.adb
@@ -26,9 +26,150 @@ package body Advent.Day_21 is
    end To_Number;
 
    function Translate (From, To : Numeric_Key) return Directional_Presses is
+      function Revert
+        (Presses : Directional_Presses) return Directional_Presses
+      is
+         Revert_Key : constant array (Directional_Key) of Directional_Key :=
+           ['v' => '^', '<' => '>', '>' => '<', '^' => 'v', 'A' => 'A'];
+         Result     : Directional_Presses (Presses'Range);
+      begin
+         for I in Presses'Range loop
+            Result (Result'Last - I - 1) := Revert_Key (Presses (I));
+         end loop;
+         return Result;
+      end Revert;
    begin
-      --  TODO
-      return Directional_Presses'[];
+      if From = To then
+         return "";
+      elsif From > To then
+         return Revert (Translate (To, From));
+      end if;
+
+      case Numeric_Presses'[From, To] is
+         when "01" =>
+            return "^<";
+         when "02" =>
+            return "^";
+         when "03" =>
+            return "^>";
+         when "04" =>
+            return "^^<";
+         when "05" =>
+            return "^^";
+         when "06" =>
+            return "^^>";
+         when "07" =>
+            return "^^^<";
+         when "08" =>
+            return "^^^";
+         when "09" =>
+            return "^^^>";
+         when "0A" =>
+            return ">";
+
+         when "12" =>
+            return ">";
+         when "13" =>
+            return ">>";
+         when "14" =>
+            return "^";
+         when "15" =>
+            return "^>";
+         when "16" =>
+            return "^>>";
+         when "17" =>
+            return "^^";
+         when "18" =>
+            return "^^>";
+         when "19" =>
+            return "^^>>";
+         when "1A" =>
+            return ">>v";
+
+         when "23" =>
+            return ">";
+         when "24" =>
+            return "^<";
+         when "25" =>
+            return "^";
+         when "26" =>
+            return "^>";
+         when "27" =>
+            return "^^<";
+         when "28" =>
+            return "^^";
+         when "29" =>
+            return "^^>";
+         when "2A" =>
+            return ">v";
+
+         when "34" =>
+            return "^<<";
+         when "35" =>
+            return "^<";
+         when "36" =>
+            return "^";
+         when "37" =>
+            return "^^<<";
+         when "38" =>
+            return "^^<";
+         when "39" =>
+            return "^^";
+         when "3A" =>
+            return "v";
+
+         when "45" =>
+            return ">";
+         when "46" =>
+            return ">>";
+         when "47" =>
+            return "^";
+         when "48" =>
+            return "^>";
+         when "49" =>
+            return "^>>";
+         when "4A" =>
+            return ">>vv";
+
+         when "56" =>
+            return ">";
+         when "57" =>
+            return "^<";
+         when "58" =>
+            return "^";
+         when "59" =>
+            return "^>";
+         when "5A" =>
+            return ">vv";
+
+         when "67" =>
+            return "^<<";
+         when "68" =>
+            return "^<";
+         when "69" =>
+            return "^";
+         when "6A" =>
+            return "vv";
+
+         when "78" =>
+            return ">";
+         when "79" =>
+            return ">>";
+         when "7A" =>
+            return ">>vvv";
+
+         when "89" =>
+            return ">";
+         when "8A" =>
+            return ">vvv";
+
+         when "9A" =>
+            return "vvv";
+
+         when others =>
+            --  Already covered at the top.
+            raise Program_Error;
+      end case;
    end Translate;
 
    function Translate (Presses : Numeric_Presses) return Directional_Presses is

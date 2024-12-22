4538c2629fee2d09bba58c20e70d98003d9121c4
Anton Yabchinskiy <arn@bestmx.net>
2024-12-22 17:21:20 +0100

    day_20_1: alternative solution, try to cheat only between shortest path positions

--- 2024/day_20_1.adb
+++ 2024/day_20_1.adb
@@ -3,8 +3,7 @@ with Ada.Integer_Text_IO;       use Ada.Integer_Text_IO;
 with Ada.Text_IO;               use Ada.Text_IO;
 with Advent.Day_20;             use Advent.Day_20;
 with Advent.Grids;              use Advent.Grids;
-with Advent;
-use Advent;
+with Advent;                    use Advent;
 
 --  FIXME: Re-export from Day_20?
 with Advent.Day_16;
@@ -13,18 +12,21 @@ use all type Advent.Day_16.Tile_Type;
 procedure Day_20_1 is
    Threshold : constant Natural := Natural'Value (Value ("THRESHOLD", "100"));
 
+   --  Read racetrack and start and finish positions.
    Start, Finish : Position;
-   Track : constant Racetrack_Type :=
+   Track         : constant Racetrack_Type :=
      Get_Racetrack (Standard_Input, Start, Finish);
 
-   Distance : Distance_Map(Track'Range(1),Track'Range(2));
-   Previous : Previous_Map(Track'Range(1),Track'Range(2));
-   Exists   : constant Boolean := Shortest_Path (Track, Start, Finish, Previous, Distance);
+   --  Find a shortest path, backtrack path positions.
+   Distance : Distance_Map (Track'Range (1), Track'Range (2));
+   Previous : Previous_Map (Track'Range (1), Track'Range (2));
+   Unused   : constant Boolean        :=
+     Shortest_Path (Track, Start, Finish, Previous, Distance);
+   Path     : constant Position_Array :=
+     Shortest_Path_Positions (Previous, Start, Finish);
 
    N_Cheats : Natural := 0;
 begin
-   Find_Shortest_Path (Track, Start, Finish, Previous, Distance);
-
    if Debug then
       Print (Standard_Error, Track, Track, Path_Char => ' ');
       Put_Line (Standard_Error, Start'Image);
@@ -36,51 +38,38 @@ begin
    for I in Path'First .. Path'Last - 1 loop
       for J in I + 1 .. Path'Last loop
          declare
-            P : constant Position := [Integer'Min (Path (I) (1), Path (J) (1)), Integer'Min (Path (I) (2), Path (J) (2))];
-            Q : constant Position := [Integer'Max (Path (I) (1), Path (J) (1)), Integer'Max (Path (I) (2), Path (J) (2))];
-            N : constant Natural  := Taxicab_Distance (P, Q);
+            P : Position renames Path (I);
+            Q : Position renames Path (J);
+
+            Old_Dist : constant Natural :=
+              Distance (Q (1), Q (2)) - Distance (P (1), P (2));
+            New_Dist : constant Natural := Taxicab_Distance (P, Q);
          begin
-            if P (1) = Q (1) and N = 2 then    --  Horizontal line.
-               null;
-            elsif P (2) = Q (2) and N = 2 then --  Vertical line.
-               null;
+            --  Taxicab distance between positions is 2, and…
+            --  …a wall between positions in horizontal line,
+            --  …or a wall between positions in vertical line.
+            if New_Dist = 2 and
+              ((P (1) = Q (1) and Track (P (1), (P (2) + Q (2)) / 2) = Wall) or
+               (P (2) = Q (2) and Track ((P (1) + Q (1)) / 2, P (2)) = Wall))
+            then
+               if Debug then
+                  Put_Line
+                    (Standard_Error,
+                     "Cheat between path positions [" & P (1)'Image & "," &
+                     P (2)'Image & "] and [" & Q (1)'Image & "," &
+                     Q (2)'Image & "], old" & Old_Dist'Image & " - new" &
+                     New_Dist'Image & " =" &
+                     Natural'(Old_Dist - New_Dist)'Image);
+               end if;
+
+               if (Old_Dist - New_Dist) >= Threshold then
+                  N_Cheats := @ + 1;
+               end if;
             end if;
          end;
       end loop;
    end loop;
 
-   --  Try removing each possible wall, compute Shortest_Path_Length
-   --  again. Straightforward, but slow.
-   for Row in Track'First(1) + 1 .. Track'Last(1) - 1 loop
-      for Col in Track'First(2) + 1 .. Track'Last(2) - 1 loop
-         if Track (Row, Col) = Wall
-           and ((Track (Row - 1, Col) = Empty and Track (Row + 1, Col) = Empty)
-                or (Track (Row, Col - 1) = Empty
-                    and Track (Row, Col + 1) = Empty))
-         then
-            Track_Copy (Row, Col) := Empty;
-            Cheat_Length := Shortest_Path_Length (Track_Copy, Start, Finish);
-            Track_Copy (Row, Col) := Wall;
-
-            if Debug then
-               Put_Line
-                 (Standard_Error,
-                  "Cheat at ["
-                  & Row'Image
-                  & ","
-                  & Col'Image
-                  & "] saved "
-                  & Integer'(Length - Cheat_Length)'Image);
-            end if;
-
-            --  Cheat saved at least THRESHOLD picoseconds.
-            if (Length - Cheat_Length) >= Threshold then
-               N := N + 1;
-            end if;
-         end if;
-      end loop;
-   end loop;
-
-   Put (N, 0);
+   Put (N_Cheats, 0);
    New_Line;
 end Day_20_1;

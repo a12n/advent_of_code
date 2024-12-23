8011c4b29bc92ccdc687a144d393295e5f7f9454
Anton Yabchinskiy <arn@bestmx.net>
2024-12-23 17:20:59 +0100

    day_22_2: Count_Array variable

--- 2024/day_22_2.adb
+++ 2024/day_22_2.adb
@@ -6,6 +6,12 @@ procedure Day_22_2 is
    type Change_Type is range -9 .. 9;
    type Count_Array is
      array (Change_Type, Change_Type, Change_Type, Change_Type) of Natural;
+   pragma Pack (Count_Array);
+
+   --  Number of bananas for each possible sequence of four
+   --  consecutive changes.
+   Bananas : Count_Array := [others => [others => [others => [others => 0]]]];
 begin
+   --  TODO
    null;
 end Day_22_2;

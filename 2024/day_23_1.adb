dddd274943866a084df229a92528c0ebf10e7fe0
Anton Yabchinskiy <arn@bestmx.net>
2024-12-23 13:25:27 +0100

    day_23_1: analyze connected components

--- 2024/day_23_1.adb
+++ 2024/day_23_1.adb
@@ -3,7 +3,12 @@ with Advent.Day_23; use Advent.Day_23;
 
 procedure Day_23_1 is
    Connections : constant Connection_Map := Get_Connections (Standard_Input);
+   Components      : Component_Map;
+   Component_Sizes : constant Component_Size_Array :=
+     Connected_Components (Connections, Components);
 begin
-   Put_Line (Standard_Error, Connections'Image);
+   Put_Line (Standard_Error, "Connections" & Connections'Image);
+   Put_Line (Standard_Error, "Components" & Components'Image);
+   Put_Line (Standard_Error, "Component_Sizes" & Component_Sizes'Image);
    --  TODO
 end Day_23_1;

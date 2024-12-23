3d72574b9f492dcab21c3eac093e590b058ca9fb
Anton Yabchinskiy <arn@bestmx.net>
2024-12-23 17:17:38 +0100

    day_22_2: Count_Array type

--- 2024/day_22_2.adb
+++ 2024/day_22_2.adb
@@ -2,6 +2,10 @@ with Ada.Text_IO;   use Ada.Text_IO;
 with Advent.Day_22; use Advent.Day_22;
 
 procedure Day_22_2 is
+   type Digit_Type is range 0 .. 9;
+   type Change_Type is range -9 .. 9;
+   type Count_Array is
+     array (Change_Type, Change_Type, Change_Type, Change_Type) of Natural;
 begin
    null;
 end Day_22_2;

dc4883a8fc9dfe0367b0574f53273e268bb9b492
Anton Yabchinskiy <arn@bestmx.net>
2024-12-25 00:19:06 +0100

    day_24_1: read input

--- 2024/day_24_1.adb
+++ 2024/day_24_1.adb
@@ -1,4 +1,9 @@
+with Ada.Text_IO;   use Ada.Text_IO;
+with Advent.Day_24; use Advent.Day_24;
+
 procedure Day_24_1 is
+   Wires : Wire_Map := Get_Wires (Standard_Input);
 begin
-   null;
+   Put_Line (Standard_Error, Wires'Image);
+   --  TODO
 end Day_24_1;

1d53b06d957da2dfc4625503ad4c6bfb43f0a6f4
Anton Yabchinskiy <arn@bestmx.net>
2024-12-25 00:39:26 +0100

    day_24_1: debug output only if Debug flag set

--- 2024/day_24_1.adb
+++ 2024/day_24_1.adb
@@ -1,9 +1,12 @@
 with Ada.Text_IO;   use Ada.Text_IO;
 with Advent.Day_24; use Advent.Day_24;
+with Advent;        use Advent;
 
 procedure Day_24_1 is
    Wires : constant Wire_Map := Get_Wires (Standard_Input);
 begin
-   Put_Line (Standard_Error, Wires'Image);
+   if Debug then
+      Put_Line (Standard_Error, Wires'Image);
+   end if;
    --  TODO
 end Day_24_1;

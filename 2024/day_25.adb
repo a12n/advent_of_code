ef2c8bd2bb0f136dcd166375a250451d911791ee
Anton Yabchinskiy <arn@bestmx.net>
2024-12-25 11:28:53 +0100

    day_25: remove bit schematics parts

--- 2024/day_25.adb
+++ 2024/day_25.adb
@@ -2,24 +2,6 @@ with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Text_IO;         use Ada.Text_IO;
 
 procedure Day_25 is
-   --  Grid of 7×5 bits may be represented as 35-bit number.
-   --  type Schematic_Type is mod 2**35;
-   --  type Schematic_Array is array (Positive range <>) of Schematic_Type;
-   --
-   --  package Schematic_Text_IO is new Ada.Text_IO.Modular_IO (Schematic_Type);
-   --
-   --  subtype Key_Type is Schematic_Type with
-   --      Dynamic_Predicate => Key (Key_Type);
-   --  subtype Lock_Type is Schematic_Type with
-   --      Dynamic_Predicate => Lock (Lock_Type);
-   --
-   --  function Key (S : Schematic_Type) return Boolean is
-   --    ((S and 2#1_1111#) = 2#1_1111#);
-   --  function Lock (S : Schematic_Type) return Boolean is
-   --    ((S and 2#1_1111#) = 2#0_0000#);
-   --  function Fit (K : Key_Type; L : Lock_Type) return Boolean is
-   --    ((K xor L) = -1);
-
    type Pin_Height is range 0 .. 5;
    type Pin_Array is array (1 .. 5) of Pin_Height;
    type Schematic_Type is record
@@ -81,18 +63,6 @@ procedure Day_25 is
                raise Constraint_Error;
          end case;
       end;
-
-      --  for I in 0 .. 35 - 1 loop
-      --     Get (File, C);
-      --     case C is
-      --        when '.' =>
-      --           S := S * 2;
-      --        when '#' =>
-      --           S := S * 2 or 1;
-      --        when others =>
-      --           raise Constraint_Error;
-      --     end case;
-      --  end loop;
 
       begin
          Skip_Line (File);

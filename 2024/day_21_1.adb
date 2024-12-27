402e1fb324cdfcba25acebc9be32c126a4438890
Anton Yabchinskiy <arn@bestmx.net>
2024-12-27 13:27:04 +0100

    day_21_1: remove FIXME comments

--- 2024/day_21_1.adb
+++ 2024/day_21_1.adb
@@ -12,47 +12,14 @@ begin
            Numeric.Get_Code (Standard_Input);
          N    : constant Natural               := Numeric.To_Number (Code);
          Keys : constant Directional.Key_Array :=
-           Translate (Translate (Translate (Code)));
-
-         -- FIXME: Multiple translations of "379A", each of these
-         -- translations may have multiple translations, these
-         -- translations may have different lengths.
-         --
-         -- "379A"
-         -- "^A<<^^A>>AvvvA"
-         -- "<A>A<v<AA>^AA>AvAA^A<vAAA>^A"
-         -- "<A>A<v<AA>^AA>AvAA^A<vAAA^>A"
-         -- "<A>A<v<AA>^AA>AvAA^Av<AAA>^A"
-         -- "<A>A<v<AA>^AA>AvAA^Av<AAA^>A"
-         -- "<A>Av<<AA>^AA>AvAA^A<vAAA>^A"
-         -- "<A>Av<<AA>^AA>AvAA^A<vAAA^>A"
-         -- "<A>Av<<AA>^AA>AvAA^Av<AAA>^A"
-         -- "<A>Av<<AA>^AA>AvAA^Av<AAA^>A"
-         --
-         -- "379A"
-         -- "^A^^<<A>>AvvvA"
-         -- "<A>A<AAv<AA>>^AvAA^Av<AAA>^A"
-         -- "v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A"
-
-         -- "37" 2
-         -- "^A<<^^A" 7
-         -- "<A>Av<<AA>^AA>A" 15
-         -- "v<<A>>^AvA^A<vA<AA>>^AAvA^<A>AAvA^A" 35
-
-         -- "37" 2
-         -- "^A^^<<A" 7
-         -- "<A>A<AAv<AA>>^A" 15
-         -- "v<<A>>^AvA^Av<<A>>^AA<vA<A>>^AAvAA^<A>A" 39
-
-         -- 3 7
-         -- ^A <<^^A
-         -- <A >A v<<A A >^A A >A
-         -- v<<A >>^A vA ^A <vA <A A >>^A A vA ^<A >A A vA ^A
-
-         -- 3 7
-         -- ^A ^^<<A
-         -- <A >A <A A v<A A >>^A
-         -- v<<A >>^A vA ^A v<<A >>^A A <vA <A >>^A A vA A ^<A >A
+         --  You
+         Translate (
+         --  Robot
+         Translate (
+         --  Robot
+         Translate (
+         --  Robot
+         Code)));
       begin
          if Debug then
             Put_Line (Standard_Error, "N " & N'Image);

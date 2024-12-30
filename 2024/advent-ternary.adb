dabc2fa504bfdb29f0b29e029d6d568776435f82
Anton Yabchinskiy <arn@bestmx.net>
2024-12-30 02:27:58 +0100

    Advent.Ternary: import package

---
+++ 2024/advent-ternary.adb
@@ -0,0 +1,99 @@
+package body Advent.Ternary is
+   function Array_Binary_Operator (A, B : Ternary_Array) return Ternary_Array
+   is
+   begin
+      return R : Ternary_Array (0 .. A'Length - 1) do
+         for I in R'Range loop
+            R (I) := Operator (A (A'First + I), B (B'First + I));
+         end loop;
+      end return;
+   end Array_Binary_Operator;
+
+   function Array_Unary_Operator (A : Ternary_Array) return Ternary_Array is
+   begin
+      return R : Ternary_Array (0 .. A'Length - 1) do
+         for I in R'Range loop
+            R (I) := Operator (A (A'First + I));
+         end loop;
+      end return;
+   end Array_Unary_Operator;
+
+   function AND_Array_Operator is new Array_Binary_Operator ("and");
+   function NOT_Array_Operator is new Array_Unary_Operator ("not");
+   function OR_Array_Operator is new Array_Binary_Operator ("or");
+   function Unify_Array is new Array_Binary_Operator (Unify);
+   function XOR_Array_Operator is new Array_Binary_Operator ("xor");
+
+   function "and" (A, B : Ternary_Array) return Ternary_Array renames
+     AND_Array_Operator;
+   function "not" (A : Ternary_Array) return Ternary_Array renames
+     NOT_Array_Operator;
+   function "or" (A, B : Ternary_Array) return Ternary_Array renames
+     OR_Array_Operator;
+   function "xor" (A, B : Ternary_Array) return Ternary_Array renames
+     XOR_Array_Operator;
+   function Unify (A, B : Ternary_Array) return Ternary_Array renames
+     Unify_Array;
+
+   function Shift_Left
+     (A : Ternary_Array; N : Natural; Shift_In : Ternary := False)
+      return Ternary_Array
+   is
+   begin
+      return R : Ternary_Array (0 .. A'Length - 1) do
+         if N = 0 then
+            R := A (A'First .. A'Last);
+         elsif N >= A'Length then
+            R := [others => Shift_In];
+         else
+            --  Least significant at index 'First (0), most
+            --  significant at index 'Last.
+            R (R'First + N .. R'Last)      := A (A'First .. A'Last - N);
+            R (R'First .. R'First + N - 1) := [others => Shift_In];
+         end if;
+      end return;
+   end Shift_Left;
+
+   function Shift_Right
+     (A : Ternary_Array; N : Natural; Shift_In : Ternary := False)
+      return Ternary_Array
+   is
+   begin
+      return R : Ternary_Array (0 .. A'Length - 1) do
+         if N = 0 then
+            R := A;
+         elsif N >= A'Length then
+            R := [others => Shift_In];
+         else
+            R (R'First .. R'Last - N)    := A (A'First + N .. A'Last);
+            R (R'Last - N + 1 .. R'Last) := [others => Shift_In];
+         end if;
+      end return;
+   end Shift_Right;
+
+   function To_String (A : Ternary_Array) return String is
+   begin
+      return S : String (1 .. A'Length) do
+         for I in 0 .. A'Length - 1 loop
+            S (S'Last - I) := To_Character (A (A'First + I));
+         end loop;
+      end return;
+   end To_String;
+
+   function To_Ternary_Array (S : String) return Ternary_Array is
+   begin
+      return To_Ternary_Array (S, S'Length);
+   end To_Ternary_Array;
+
+   function To_Ternary_Array
+     (S : String; N : Positive; Shift_In : Ternary := False)
+      return Ternary_Array
+   is
+   begin
+      return A : Ternary_Array (0 .. N - 1) := [others => Shift_In] do
+         for I in 0 .. S'Length - 1 loop
+            A (A'First + I) := To_Ternary (S (S'Last - I));
+         end loop;
+      end return;
+   end To_Ternary_Array;
+end Advent.Ternary;
---
+++ 2024/advent-ternary.ads
@@ -0,0 +1,92 @@
+package Advent.Ternary is
+   pragma Pure;
+
+   type Ternary is (False, True, Unknown);
+
+   --  Least significant digit is at 'First, and the most significant
+   --  one at 'Last.
+   type Ternary_Array is array (Natural range <>) of Ternary;
+   pragma Pack (Ternary_Array);
+
+   function "and" (A, B : Ternary) return Ternary;
+   function "not" (A : Ternary) return Ternary;
+   function "or" (A, B : Ternary) return Ternary;
+   function "xor" (A, B : Ternary) return Ternary;
+
+   function "and" (A, B : Ternary_Array) return Ternary_Array;
+   function "not" (A : Ternary_Array) return Ternary_Array;
+   function "or" (A, B : Ternary_Array) return Ternary_Array;
+   function "xor" (A, B : Ternary_Array) return Ternary_Array;
+
+   function Shift_Left
+     (A : Ternary_Array; N : Natural; Shift_In : Ternary := False)
+      return Ternary_Array;
+
+   function Shift_Right
+     (A : Ternary_Array; N : Natural; Shift_In : Ternary := False)
+      return Ternary_Array;
+
+   function To_Character (A : Ternary) return Character is
+     (case A is when False => '0', when True => '1', when Unknown => '_');
+   function To_String (A : Ternary_Array) return String;
+   function To_Ternary (C : Character) return Ternary is
+     (case C is when '0' | 'F' | '.' => False, when '1' | 'T' | '#' => True,
+        when '?' | '-' | '_' => Unknown,
+        when others => raise Constraint_Error);
+   function To_Ternary_Array (S : String) return Ternary_Array;
+   function To_Ternary_Array
+     (S : String; N : Positive; Shift_In : Ternary := False)
+      return Ternary_Array with
+     Pre => N >= S'Length;
+
+   Not_Unifiable_Error : exception;
+
+   function Unify (A, B : Ternary) return Ternary is
+     (case A is
+        when False =>
+          (case B is when False | Unknown => False,
+             when True => raise Not_Unifiable_Error),
+        when True =>
+          (case B is when False => raise Not_Unifiable_Error,
+             when True | Unknown => True),
+        when Unknown =>
+          (case B is when False => False, when True => True,
+             when Unknown => Unknown));
+   function Unify (A, B : Ternary_Array) return Ternary_Array;
+
+   pragma Inline ("and", "not", "or", "xor", To_Character);
+
+private
+   AND_Table : constant array (Ternary, Ternary) of Ternary :=
+     [False  => [others => False],
+     True    => [False => False, True => True, Unknown => Unknown],
+     Unknown => [False => False, True => Unknown, Unknown => Unknown]];
+
+   NOT_Table : constant array (Ternary) of Ternary :=
+     [False => True, True => False, Unknown => Unknown];
+
+   OR_Table : constant array (Ternary, Ternary) of Ternary :=
+     [False  => [False => False, True => True, Unknown => Unknown],
+     True    => [others => True],
+     Unknown => [False => Unknown, True => True, Unknown => Unknown]];
+
+   XOR_Table : constant array (Ternary, Ternary) of Ternary :=
+     [False  => [False => False, True => True, Unknown => Unknown],
+     True    => [False => True, True => False, Unknown => Unknown],
+     Unknown => [others => Unknown]];
+
+   function "and" (A, B : Ternary) return Ternary is (AND_Table (A, B));
+   function "not" (A : Ternary) return Ternary is (NOT_Table (A));
+   function "or" (A, B : Ternary) return Ternary is (OR_Table (A, B));
+   function "xor" (A, B : Ternary) return Ternary is (XOR_Table (A, B));
+
+   generic
+      with function Operator (A, B : Ternary) return Ternary;
+   function Array_Binary_Operator
+     (A, B : Ternary_Array) return Ternary_Array with
+     Pre => A'Length = B'Length;
+
+   generic
+      with function Operator (A : Ternary) return Ternary;
+   function Array_Unary_Operator (A : Ternary_Array) return Ternary_Array;
+end Advent.Ternary;

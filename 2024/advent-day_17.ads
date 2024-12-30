8966883a6b70da7b970cdac2c39138d74e6c3b23
Anton Yabchinskiy <arn@bestmx.net>
2024-12-30 01:25:13 +0100

    Advent.Day_17: number of bits in Register type should be multiple of 3

--- 2024/advent-day_17.ads
+++ 2024/advent-day_17.ads
@@ -3,7 +3,7 @@ with Ada.Text_IO; use Ada.Text_IO;
 package Advent.Day_17 is
    type Number is mod 2**3;
    type Number_Array is array (Positive range <>) of Number;
-   type Register is mod 2**64;
+   type Register is mod 2**63;
    type Register_Name is (A, B, C);
    type Register_Set is array (Register_Name) of Register;
 

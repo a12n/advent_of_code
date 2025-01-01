2bcae5615045038b089095dab2e8cf8ccf5a74ff
Anton Yabchinskiy <arn@bestmx.net>
2025-01-01 14:50:17 +0100

    day_21_2: WIP, non-minimal length

--- 2024/day_21_2.adb
+++ 2024/day_21_2.adb
@@ -1,119 +1,22 @@
-with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
-with Ada.Text_IO;         use Ada.Text_IO;
-with Advent.Day_21;       use Advent.Day_21;
-with Advent.Debug;        use Advent.Debug;
+with Ada.Text_IO;   use Ada.Text_IO;
+with Advent.Day_21; use Advent.Day_21;
 
 procedure Day_21_2 is
-   Total : Natural := 0;
+   use Count_Text_IO;
 
-   function Min_Translate_Path
-     (From, To : Numeric.Key_Type) return Directional.Key_Array
-   is
-      use type Directional.Key_Array;
-      use type Numeric.Key_Type;
-
-      Min_Dist : Natural := Natural'Last;
-      Min_Path : Directional.Bounded_Key_Array;
-
-      procedure Iterate (Key : Numeric.Key_Type; Path : Directional.Key_Array)
-      is
-      begin
-         if Key = To then
-            declare
-               Dist : constant Natural :=
-                 Directional.Distance (Translate (Translate (Path & 'A')));
-            begin
-               if Dist < Min_Dist then
-                  Min_Dist := Dist;
-                  Min_Path := Directional.To_Bounded (Path);
-               end if;
-            end;
-            return;
-         end if;
-
-         for Dir in Directional.Move_Key_Type'Range loop
-            begin
-               if Numeric.Distance (Move (Key, Dir), To) <
-                 Numeric.Distance (Key, To)
-               then
-                  Iterate (Move (Key, Dir), Path & Dir);
-               end if;
-            exception
-               when Constraint_Error =>
-                  null;
-            end;
-         end loop;
-      end Iterate;
-   begin
-      Iterate (From, Directional.Key_Array'[]);
-      --  Put_Line
-      --    (Standard_Error,
-      --     "From " & From'Image & ", to " & To'Image & ", path " &
-      --     Directional.To_String (Min_Path.Elements (1 .. Min_Path.Length))'
-      --       Image);
-      return Min_Path.Elements (1 .. Min_Path.Length);
-   end Min_Translate_Path;
-
-   use type Numeric.Key_Type;
+   Total      : Count_Type := 0;
+   Translator : Translator_Type (25);
 begin
-   --  Put_Line (Standard_Error, "(case From is");
-   --  for From in Numeric.Key_Type'Range loop
-   --     Put_Line (Standard_Error, "when " & From'Image & " => (case To is");
-   --
-   --     for To in Numeric.Key_Type'Range loop
-   --        Put
-   --          (Standard_Error,
-   --           Character'Val (9) & "when " & To'Image & " => " &
-   --           Directional.To_String (Min_Translate_Path (From, To))'Image);
-   --
-   --        if To < Numeric.Key_Type'Last then
-   --           Put (Standard_Error, ',');
-   --        end if;
-   --        New_Line (Standard_Error);
-   --     end loop;
-   --
-   --     if From < Numeric.Key_Type'Last then
-   --        Put_Line (Standard_Error, "),");
-   --     else
-   --        Put_Line (Standard_Error, ")");
-   --     end if;
-   --  end loop;
-   --  Put_Line (Standard_Error, ")");
    loop
       declare
-         Code : constant Numeric.Code_Type     :=
+         Code   : constant Numeric.Code_Type     :=
            Numeric.Get_Code (Standard_Input);
-         N    : constant Natural               := Numeric.To_Number (Code);
-         Keys : constant Directional.Key_Array :=
-         --  You (directional keypad)
-         Translate (
-         --  Robot 1 (directional keypad)
-         Translate (
-         --  Robot 2 (directional keypad)
-         Translate (
-         --  Robot 3 (directional keypad)
-         Translate (
-         --  Robot 4 (directional keypad)
-         Translate (
-         --  Robot 5 (directional keypad)
-         Translate (
-         --  Robot 6 (directional keypad)
-         Translate (
-         --  Robot 7 (directional keypad)
-         Translate (
-         --  Robot 8 (directional keypad)
-         Translate (
-         --  Robot 9 (directional keypad)
-         Translate (
-         --  Robot 10 (directional keypad)
-         Translate (
-         --  Robot (numeric keypad)
-         Code)))))))))));
+         N      : constant Natural               := Numeric.To_Number (Code);
+         Keys   : constant Directional.Key_Array := Translate (Code);
+         Length : constant Count_Type            :=
+           Translate_Length (Translator, Keys, 25);
       begin
-         if Debug_Enabled then
-            Put_Line (Standard_Error, "N " & N'Image);
-         end if;
-         Total := Total + N * Keys'Length;
+         Total := Total + Count_Type (N) * Length;
       end;
    end loop;
 exception

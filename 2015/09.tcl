c7ce670ec80f10ef6632bf067ffc8612397ab05b
Anton Yabchinskiy <arn@bestmx.net>
2025-01-06 02:32:28 +0100

    09-1: initial solution

--- 2015/09.tcl
+++ 2015/09.tcl
@@ -18,4 +18,55 @@ while {[gets stdin line] >= 0} {
 
 puts stderr $distances
 
-# TODO
+proc pathsFrom {distances path unvisited} {
+    if {$unvisited eq {}} {
+        return [list $path]
+    }
+
+    set from [lindex $path end]
+    set result {}
+
+    # puts stderr "pathsFrom: path $path, from $from, unvisited $unvisited"
+
+    foreach {to dist} [dict get $distances $from] {
+        set i [lsearch -exact $unvisited $to]
+
+        if {$i == -1} {
+            # Already visited, skip
+            continue
+        }
+
+        set path2 [linsert $path end $to]
+        set unvisited2 [lreplace $unvisited $i $i]
+
+        lappend result {*}[pathsFrom $distances $path2 $unvisited2]
+    }
+
+    return $result
+}
+
+proc paths distances {
+    set result {}
+    set unvisited [dict keys $distances]
+
+    foreach from $unvisited {
+        set i [lsearch -exact $unvisited $from]
+        set unvisited2 [lreplace $unvisited $i $i]
+
+        lappend result {*}[pathsFrom $distances $from $unvisited2]
+    }
+
+    return $result
+}
+
+proc distance {distances path} {
+    set total 0
+
+    foreach from [lrange $path 0 end-1] to [lrange $path 1 end] {
+        incr total [dict get $distances $from $to]
+    }
+
+    return $total
+}
+
+puts [tcl::mathfunc::min {*}[lmap path [paths $distances] { distance $distances $path }]]

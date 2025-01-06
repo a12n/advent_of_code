27047517b7a6834953d68ee8c7f4c0a93cac5711
Anton Yabchinskiy <arn@bestmx.net>
2025-01-06 19:35:21 +0100

    04: separate prefixZeroes proc

--- 2015/04.tcl
+++ 2015/04.tcl
@@ -2,18 +2,20 @@
 
 package require md5
 
+proc prefixZeroes {key n {start 0} {step 1}} {
+    set zeroes [string repeat 0 $n]
+
+    for {set i $start} {1} {incr i $step} {
+        if {[string equal -length $n [md5::md5 -hex "$key$i"] $zeroes]} {
+            return $i
+        }
+    }
+}
+
+set key [gets stdin]
 set n [switch $puzzle(part) {
     1 { expr 5 }
     2 { expr 6 }
 }]
-set prefix [string repeat 0 $n]
 
-set key [gets stdin]
-
-# XXX
-for {set i 0} {1} {incr i} {
-    if {[string equal -length $n [md5::md5 -hex "$key$i"] $prefix]} {
-        puts $i
-        return
-    }
-}
+puts [prefixZeroes $key $n]

9e0b7070b81a2012c70f5927aa398d6b6cc8b706
Anton Yabchinskiy <arn@bestmx.net>
2025-01-06 19:57:09 +0100

    04: threads

--- 2015/04.tcl
+++ 2015/04.tcl
@@ -1,16 +1,6 @@
 #!/usr/bin/env tclsh
 
-package require md5
-
-proc prefixZeroes {key n {start 0} {step 1}} {
-    set zeroes [string repeat 0 $n]
-
-    for {set i $start} {1} {incr i $step} {
-        if {[string equal -length $n [md5::md5 -hex "$key$i"] $zeroes]} {
-            return $i
-        }
-    }
-}
+package require Thread
 
 set key [gets stdin]
 set n [switch $puzzle(part) {
@@ -18,4 +8,25 @@ set n [switch $puzzle(part) {
     2 { expr 6 }
 }]
 
-puts [prefixZeroes $key $n]
+for {set i 0} {$i < 8} {incr i} {
+    set id [thread::create {
+        package require md5
+
+        proc prefixZeroes {key n {start 0} {step 1}} {
+            set zeroes [string repeat 0 $n]
+            for {set i $start} {1} {incr i $step} {
+                if {[string equal -length $n [md5::md5 -hex "$key$i"] $zeroes]} {
+                    return $i
+                }
+            }
+        }
+
+        thread::wait
+    }]
+
+    thread::send -async $id [subst {prefixZeroes $key $n $i 8}] result
+}
+
+vwait result
+
+puts $result

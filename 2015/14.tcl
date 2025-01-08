f4ca030c1c47033eb5b746478e9b3233d9df96cc
Anton Yabchinskiy <arn@bestmx.net>
2025-01-08 21:31:01 +0100

    14-2: there may be multiple lead indices

--- 2015/14.tcl
+++ 2015/14.tcl
@@ -34,11 +34,15 @@ for {set t 1} {$t <= $tEnd} {incr t} {
     set distances [lmap reindeer $reindeers { distance $reindeer $t }]
     set lead 0
     for {set i 1} {$i < $n} {incr i} {
-        if {[lindex $distances $i] > [lindex $distances $lead]} {
+        if {[lindex $distances $i] > [lindex $distances [lindex $lead 0]]} {
             set lead $i
+        } elseif {[lindex $distances $i] == [lindex $distances [lindex $lead 0]]} {
+            lappend lead $i
         }
     }
-    lset points $lead [expr {[lindex $points $lead] + 1}]
+    foreach i $lead {
+        lset points $i [expr {[lindex $points $i] + 1}]
+    }
     puts stderr "$t: points $points"
 }
 

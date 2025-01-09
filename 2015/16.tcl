ac651fc9e2d7a971262b846f29f5d86bbf5950db
Anton Yabchinskiy <arn@bestmx.net>
2025-01-10 00:55:45 +0100

    16: separate unifyExact and unifyRange functions

--- 2015/16.tcl
+++ 2015/16.tcl
@@ -4,6 +4,31 @@ proc normalize record {
     lsort -index 0 -stride 2 $record
 }
 
+proc unifyExact {reference record} {
+    foreach {key value} $record {
+        set refValue [dict get $reference $key]
+        if {$value != $refValue} {
+            return no
+        }
+    }
+    return yes
+}
+
+proc unifyRange {reference record} {
+    foreach {key value} $record {
+        set refValue [dict get $reference $key]
+
+        if {$key in {cats trees} && $value <= $refValue} {
+            return no
+        } elseif {$key in {goldfish pomeranians} && $value >= $refValue} {
+            return no
+        # } elseif {$value != $refValue} {
+        #     return no
+        }
+    }
+    return yes
+}
+
 proc unifies {reference record} {
     foreach {key value} $record {
         if {[dict get $reference $key] > $value} {
@@ -43,29 +68,37 @@ while {[gets stdin line] >= 0} {
     lappend notes [normalize [string map { , {} : {} } $line]]
 }
 
+switch $puzzle(part) {
+    1 { set unify unifyExact }
+    2 { set unify unifyRange }
+}
+
 foreach note $notes {
     set id [dict get $note Sue]
     set record [dict remove $note Sue]
 
-    if {![unifies $sample $record]} {
-        puts stderr "$id: record $record not unifies"
+    if {![$unify $sample $record]} {
+        puts stderr "$id: record $record: not unify"
         continue
     }
 
     set dist [distance $sample $record]
 
-    puts stderr "$id: record $record distance $dist"
+    puts stderr "$id: record $record: distance $dist"
 
     if {[info exists leastDistance]} {
         if {$dist < $leastDistance} {
             set idBest $id
             set leastDistance $dist
+            puts stderr "$id: new leastDistance $leastDistance"
         } elseif {$dist == $leastDistance} {
             lappend idBest $id
+            puts stderr "$id: same leastDistance $leastDistance"
         }
     } else {
         set idBest $id
         set leastDistance $dist
+        puts stderr "$id: initial leastDistance $leastDistance"
     }
 }
 

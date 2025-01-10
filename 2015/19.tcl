55586ceb1b26fc2036d42035cd9abfe720482216
Anton Yabchinskiy <arn@bestmx.net>
2025-01-10 23:34:48 +0100

    19-2: initial implementation of fabricate

--- 2015/19.tcl
+++ 2015/19.tcl
@@ -32,8 +32,42 @@ proc calibrate {replacements molecule} {
 }
 
 proc fabricate {replacements start finish} {
-    # TODO
-    return 0
+    set queue [list [list 0 $start]]
+    set seen [dict create]
+
+    while {$queue ne {}} {
+        set queue [lassign $queue elt]
+        lassign $elt dist molecule
+
+        puts stderr "fabricate: dist $dist molecule \"$molecule\" queue [llength $queue]"
+
+        # TODO: keep only "changes" in the queue, not the whole strings?
+        # TODO: A* with some edit distance heuristic?
+        if {$molecule eq $finish} {
+            return $dist
+        }
+
+        dict for {from toList} $replacements {
+            set n [string length $from]
+            set first 0
+            while {[set first [string first $from $molecule $first]] != -1} {
+                set last [expr {$first + $n - 1}]
+                foreach to $toList {
+                    set dist2 [expr {$dist + 1}]
+                    set molecule2 [string replace $molecule $first $last $to]
+                    if {![dict exists $seen $molecule2]} {
+                        lappend queue [list $dist2 $molecule2]
+                        dict set seen $molecule2 yes
+                    }
+                }
+                incr first
+            }
+        }
+
+        set queue [lsort -index 0 -integer $queue]
+    }
+
+    error "fabricate: no path from $start to $finish"
 }
 
 switch $puzzle(part) {

26206362553e08f511069505dae68ed0b002c737
Anton Yabchinskiy <arn@bestmx.net>
2025-01-10 22:58:42 +0100

    19: use dict for replacements and replaced

--- 2015/19.tcl
+++ 2015/19.tcl
@@ -1,28 +1,30 @@
 #!/usr/bin/env tclsh
 
-array set replacements {}
+set replacements [dict create]
 while {[gets stdin line] > 0} {
     lassign [string map {" => " " "} $line] from to
-    lappend replacements($from) $to
+    dict update replacements $from toList {
+        lappend toList $to
+    }
 }
 
 set molecule [gets stdin]
 
-puts stderr "replacements [array get replacements] [array size replacements], molecule $molecule [string length $molecule]"
+puts stderr "replacements $replacements [dict size $replacements], molecule $molecule [string length $molecule]"
 
-array set replaced {}
-foreach {from toList} [array get replacements] {
+set replaced [dict create]
+dict for {from toList} $replacements {
     set n [string length $from]
     set first 0
     while {[set first [string first $from $molecule $first]] != -1} {
         set last [expr {$first + $n - 1}]
         foreach to $toList {
-            set replaced([string replace $molecule $first $last $to]) yes
+            dict set replaced [string replace $molecule $first $last $to] yes
         }
         incr first
     }
 }
 
-puts stderr "replaced [lsort -index 0 -stride 2 [array get replaced]] [array size replaced]"
+puts stderr "replaced [lsort -index 0 -stride 2 $replaced] [dict size $replaced]"
 
-puts [array size replaced]
+puts [dict size $replaced]

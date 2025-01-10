750c2fc94b5372093fd8c08bb86ea073380631c9
Anton Yabchinskiy <arn@bestmx.net>
2025-01-10 23:34:13 +0100

    19: separate calibrate and fabricate commands

--- 2015/19.tcl
+++ 2015/19.tcl
@@ -12,19 +12,31 @@ set molecule [gets stdin]
 
 puts stderr "replacements $replacements [dict size $replacements], molecule $molecule [string length $molecule]"
 
-set replaced [dict create]
-dict for {from toList} $replacements {
-    set n [string length $from]
-    set first 0
-    while {[set first [string first $from $molecule $first]] != -1} {
-        set last [expr {$first + $n - 1}]
-        foreach to $toList {
-            dict set replaced [string replace $molecule $first $last $to] yes
+proc calibrate {replacements molecule} {
+    set replaced [dict create]
+    dict for {from toList} $replacements {
+        set n [string length $from]
+        set first 0
+        while {[set first [string first $from $molecule $first]] != -1} {
+            set last [expr {$first + $n - 1}]
+            foreach to $toList {
+                dict set replaced [string replace $molecule $first $last $to] yes
+            }
+            incr first
         }
-        incr first
     }
+
+    puts stderr "replaced [lsort -index 0 -stride 2 $replaced] [dict size $replaced]"
+
+    return [dict size $replaced]
 }
 
-puts stderr "replaced [lsort -index 0 -stride 2 $replaced] [dict size $replaced]"
+proc fabricate {replacements start finish} {
+    # TODO
+    return 0
+}
 
-puts [dict size $replaced]
+switch $puzzle(part) {
+    1 { puts [calibrate $replacements $molecule] }
+    2 { puts [fabricate $replacements e $molecule] }
+}

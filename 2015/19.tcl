5e5d32c3f5e39973dff7f8b82f7304b6730f9fb4
Anton Yabchinskiy <arn@bestmx.net>
2025-01-10 21:37:53 +0100

    19-1: WIP

--- 2015/19.tcl
+++ 2015/19.tcl
@@ -9,3 +9,16 @@ while {[gets stdin line] > 0} {
 set molecule [gets stdin]
 
 puts stderr "replacements [array get replacements], molecule $molecule"
+
+set replaced {}
+foreach {from toList} [array get replacements] {
+    set pos 0
+    while {[set pos [string first $from $molecule $pos]] != -1} {
+        foreach to $toList {
+            dict set replaced [list $pos $to] yes
+        }
+        incr pos
+    }
+}
+
+puts stderr "replaced $replaced"

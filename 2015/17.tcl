c18b38d1f4de80895f46fdbabdbf1c03b5080cbd
Anton Yabchinskiy <arn@bestmx.net>
2025-01-11 02:05:49 +0100

    17: initial solution

--- 2015/17.tcl
+++ 2015/17.tcl
@@ -6,3 +6,25 @@ puts stderr "liters $liters"
 
 set capacities [lsort -decreasing -integer [read stdin]]
 puts stderr "capacities $capacities"
+
+proc combinations {capacities liters} {
+    puts stderr "combinations: capacities $capacities liters $liters"
+
+    if {$liters == 0} {
+        return 1
+    } elseif {$capacities eq {}} {
+        return 0
+    }
+
+    set capacities2 [lassign $capacities cap]
+    set sum 0
+
+    if {$liters >= $cap} {
+        incr sum [combinations $capacities2 [expr {$liters - $cap}]]
+    }
+    incr sum [combinations $capacities2 $liters]
+
+    return $sum
+}
+
+puts [combinations $capacities $liters]

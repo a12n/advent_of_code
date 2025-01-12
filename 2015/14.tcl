18d23aa148026185cda1dc13003ad4872b5519c8
Anton Yabchinskiy <arn@bestmx.net>
2025-01-12 12:19:00 +0100

    14: proc 1 and proc 2

--- 2015/14.tcl
+++ 2015/14.tcl
@@ -1,9 +1,5 @@
 #!/usr/bin/env tclsh
 
-# Total time limit.
-set tEnd 2503
-catch { set tEnd $env(DURATION) }
-
 # Absolute position offset of the reindeer after t seconds.
 proc distance {reindeer t} {
     lassign $reindeer name speed fly rest
@@ -28,6 +24,43 @@ proc stops {reindeer duration} {
     return $times
 }
 
+proc 1 {reindeers tEnd} {
+    puts [tcl::mathfunc::max {*}[lmap reindeer $reindeers { distance $reindeer $tEnd }]]
+}
+
+proc 2 {reindeers tEnd} {
+    set n [llength $reindeers]
+    set scores [lrepeat $n 0]
+
+    # TODO: Evaluate only at stop time instants?
+    for {set t 1} {$t <= $tEnd} {incr t} {
+        set distances [lmap reindeer $reindeers { distance $reindeer $t }]
+        set leaders 0
+
+        for {set i 1} {$i < $n} {incr i} {
+            set reindeer [lindex $reindeers $i]
+            set best [lindex $distances [lindex $leaders 0]]
+            set this [lindex $distances $i]
+
+            if {$this > $best} {
+                set leaders $i
+            } elseif {$this == $best} {
+                lappend leaders $i
+            }
+        }
+
+        foreach i $leaders {
+            lset scores $i [expr {[lindex $scores $i] + 1}]
+        }
+    }
+
+    puts [tcl::mathfunc::max {*}$scores]
+}
+
+# Total time limit.
+set tEnd 2503
+catch { set tEnd $env(DURATION) }
+
 while {[gets stdin line] >= 0} {
     lassign [string map {
         " can fly" ""
@@ -41,36 +74,4 @@ while {[gets stdin line] >= 0} {
     lappend reindeers [list $name $speed $fly $rest]
 }
 
-switch $puzzle(part) {
-    1 {
-        puts [tcl::mathfunc::max {*}[lmap reindeer $reindeers { distance $reindeer $tEnd }]]
-    }
-    2 {
-        set n [llength $reindeers]
-        set scores [lrepeat $n 0]
-
-        # TODO: Evaluate only at stop time instants?
-        for {set t 1} {$t <= $tEnd} {incr t} {
-            set distances [lmap reindeer $reindeers { distance $reindeer $t }]
-            set leaders 0
-
-            for {set i 1} {$i < $n} {incr i} {
-                set reindeer [lindex $reindeers $i]
-                set best [lindex $distances [lindex $leaders 0]]
-                set this [lindex $distances $i]
-
-                if {$this > $best} {
-                    set leaders $i
-                } elseif {$this == $best} {
-                    lappend leaders $i
-                }
-            }
-
-            foreach i $leaders {
-                lset scores $i [expr {[lindex $scores $i] + 1}]
-            }
-        }
-
-        puts [tcl::mathfunc::max {*}$scores]
-    }
-}
+$puzzle(part) $reindeers $tEnd

2e8e8b262380b016cb803f653b1f453ff87d5c6f
Anton Yabchinskiy <arn@bestmx.net>
2025-04-17 01:57:20 +0200

    io_ext: add error_exit/4

--- 2020/io_ext.m
+++ 2020/io_ext.m
@@ -5,9 +5,14 @@
 :- import_module io.
 :- import_module list.
 
+:- pred error_exit(int::in, string::in, io::di, io::uo) is det.
 :- pred read_lines_as_strings(io.res(list(string))::out, io::di, io::uo) is det.
 
 :- implementation.
+
+error_exit(Status, Message, !IO) :-
+    set_exit_status(Status, !IO),
+    write_string(stderr_stream, Message, !IO).
 
 read_lines_as_strings(Result, !IO) :-
     read_lines_as_strings([], Result, !IO).

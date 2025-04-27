2d0fa24faa136922c3bb5ff96149b97aea16951d
Anton Yabchinskiy <arn@bestmx.net>
2025-04-27 18:02:28 +0200

    day_03: add num_trees/{3,4}

--- 2020/day_03.m
+++ 2020/day_03.m
@@ -11,6 +11,9 @@
 
 :- pred trees_input(res({tree_set, extent})::out, io::di, io::uo) is det.
 
+:- func num_trees(tree_set, extent, slope) = int.
+:- func num_trees(tree_set, extent, slope, pos) = int.
+
 :- implementation.
 
 trees_input(Result, !IO) :-
@@ -32,4 +35,19 @@ trees_input(pos(Row, Col) @ Pos, MaxCol, Trees, Result, !IO) :-
       Result = ok({Trees, extent(Row, MaxCol + 1)})
     ; ReadResult = error(Error),
       Result = error(Error)
+    ).
+
+num_trees(Trees, Extent, Slope) = Num:-
+    Num = num_trees(Trees, Extent, Slope, pos(0, 0)).
+
+num_trees(Trees, Extent, Slope, Pos) = Num :-
+    Num = num_trees(Trees, Extent, Slope, Pos, 0).
+
+:- func num_trees(tree_set, extent, slope, pos, int) = int.
+num_trees(Trees, extent(Rows, Cols) @ Extent, slope(Y, X) @ Slope, pos(Row, Col) @ Pos, Accum) = Num :-
+    ( Row < Rows ->
+      NextPos = pos(Row + Y, (Col + X) mod Cols),
+      NextAccum = (member(Pos, Trees) -> Accum + 1; Accum),
+      Num = num_trees(Trees, Extent, Slope, NextPos, NextAccum)
+    ; Num = Accum
     ).

54ec44cadd79e5b6c4a252df2c8904b4e13a4f65
Anton Yabchinskiy <arn@bestmx.net>
2025-04-27 17:44:14 +0200

    day_03: define tree_set == set(pos)

--- 2020/day_03.m
+++ 2020/day_03.m
@@ -2,19 +2,21 @@
 :- module day_03.
 :- interface.
 
-:- import_module char, int, io, map.
+:- import_module char, int, io, map, set.
 
 :- type extent ---> extent(int, int).
 :- type pos ---> pos(int, int).
 :- type slope ---> slope(int, int).
 :- type tile ---> empty; tree.
 :- type grid == map(pos, tile).
+:- type tree_set == set(pos).
 
 :- pred tile_char(tile, char).
 :- mode tile_char(in, out) is det.
 :- mode tile_char(out, in) is semidet.
 
 :- pred grid_input(res({grid, extent})::out, io::di, io::uo) is det.
+:- pred trees_input(res({tree_set, extent})::out, io::di, io::uo) is det.
 
 :- implementation.
 
@@ -36,6 +38,27 @@ grid_input(pos(Row, Col) @ Pos, MaxCol, Grid, Result, !IO) :-
       )
     ; ReadResult = eof,
       Result = ok({Grid, extent(Row, MaxCol + 1)})
+    ; ReadResult = error(Error),
+      Result = error(Error)
+    ).
+
+trees_input(Result, !IO) :-
+    trees_input(pos(0, 0), 0, init, Result, !IO).
+
+:- pred trees_input(pos::in, int::in, tree_set::in, res({tree_set, extent})::out, io::di, io::uo) is det.
+trees_input(pos(Row, Col) @ Pos, MaxCol, Trees, Result, !IO) :-
+    read_char_unboxed(stdin_stream, ReadResult, Char, !IO),
+    ( ReadResult = ok,
+      ( Char = '\n' ->
+        trees_input(pos(Row + 1, 0), MaxCol, Trees, Result, !IO)
+      ; Char = '#' ->
+        trees_input(pos(Row, Col + 1), max(MaxCol, Col), insert(Trees, Pos), Result, !IO)
+      ; Char = ('.') ->
+        trees_input(pos(Row, Col + 1), max(MaxCol, Col), Trees, Result, !IO)
+      ; Result = error(make_io_error("Invalid character"))
+      )
+    ; ReadResult = eof,
+      Result = ok({Trees, extent(Row, MaxCol + 1)})
     ; ReadResult = error(Error),
       Result = error(Error)
     ).

16c301053b5bc7311a95d772d17ad7a5760e9dfe
Anton Yabchinskiy <arn@bestmx.net>
2025-05-04 20:04:14 +0200

    day_07_1: WIP

--- 2020/day_07_1.m
+++ 2020/day_07_1.m
@@ -8,7 +8,8 @@
 
 :- implementation.
 
-:- import_module solutions.
+:- import_module int, list, solutions, string.
+:- import_module io_ext.
 
 :- pred contain(string, string).
 :- mode contain(in, out) is nondet.
@@ -41,6 +42,44 @@ eventually_contain(X, Y) :-
 eventually_contain_shiny_gold(Y) :-
     eventually_contain(Y, "shiny gold").
 
+:- pred preprocessed(string, string).
+:- mode preprocessed(in, out) is det.
+preprocessed(String0, String) :-
+    String1 = replace_all(String0, " bags contain ", ":"),
+    String2 = replace_all(String1, " bags", ""),
+    String3 = replace_all(String2, " bag", ""),
+    String4 = replace_all(String3, "no other", ""),
+    String5 = replace_all(String4, ".", ""),
+    String = replace_all(String5, ", ", ",").
+
+:- pred contain_string(string::in, string::out, list({int, string})::out) is semidet.
+contain_string(String, Bag, ContainBags) :-
+    [Bag, String1] = split_at_char(':', String),
+    filter_map((pred(String2::in, {N, ChildBag}::out) is semidet :-
+             replace(String2, " ", "=", String3),
+             [NumString, ChildBag] = split_at_char('=', String3),
+             to_int(NumString, N)
+        ), split_at_char(',', String1), ContainBags).
+
 main(!IO) :-
-    solutions(eventually_contain_shiny_gold, Bags),
-    print_line(Bags, !IO).
+    %% %% solutions(eventually_contain_shiny_gold, Bags),
+    %% %% print_line(Bags, !IO).
+    read_line_as_string(ReadResult, !IO),
+    ( ReadResult = ok(Line),
+      String0 = chomp(Line),
+      preprocessed(String0, String1),
+      format("\"%s\" -> \"%s\"\n", [s(String0), s(String1)], !IO),
+      ( contain_string(String1, Bag, ContainBags) ->
+        format("Bag \"%s\" ", [s(Bag)], !IO),
+        print_line(ContainBags, !IO)
+      ; print_line("Couldn't parse string", !IO)
+      ),
+      %% ( [Bag, Other] = split_at_string(" bags contain ", String0) ->
+      %%   format("Bag \"%s\", Other \"%s\"\n", [s(Bag), s(Other)], !IO)
+      %% ; format("Couldn't split string \"%s\"\n", [s(String0)], !IO)
+      %% ),
+      main(!IO)
+    ; ReadResult = eof
+    ; ReadResult = error(Error),
+      error_exit(1, error_message(Error), !IO)
+    ).

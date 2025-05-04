1e60b1d49f0572b75be0f22865a4af94dd0ba5bc
Anton Yabchinskiy <arn@bestmx.net>
2025-05-04 03:02:08 +0200

    day_06_1: solution

---
+++ 2020/day_06_1.m
@@ -0,0 +1,37 @@
+%% -*- mode: mercury -*-
+:- module day_06_1.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module int, list, maybe, string, uint32.
+:- import_module day_06, io_ext.
+
+:- pred main_loop(maybe(string)::in, int::in, int::out, answer_set::in, answer_set::out) is semidet.
+main_loop(MaybeLine, !Sum, !Group) :-
+    trace [run_time(env("TRACE")), io(!IO)] (
+        print(MaybeLine, !IO),
+        format(", Sum %d, Group %#0x\n", [i(!.Sum), u32(!.Group)], !IO)
+    ),
+    ( (MaybeLine = yes("\n"); MaybeLine = no) ->
+      %% Group separator. Count number of answers in the current group, start new group.
+	  !:Sum   = !.Sum + num_ones(!.Group),
+      !:Group = 0u32
+    ; MaybeLine = yes(Line) ->
+      string_answers(chomp(Line), Answers),
+      !:Sum   = !.Sum,
+      !:Group = !.Group \/ Answers
+    ; fail
+    ).
+
+main(!IO) :-
+    lines_foldl2(main_loop, 0, 0u32, Result, !IO),
+    ( Result = ok(Sum),
+      write_int(Sum, !IO), nl(!IO)
+    ; Result = error(_, Error),
+      error_exit(1, error_message(Error), !IO)
+    ).

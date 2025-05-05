c8746c93e79b86e28246ca2accf40a821789bb19
Anton Yabchinskiy <arn@bestmx.net>
2025-05-05 10:00:28 +0200

    day_08: result output variable for run_program/6

--- 2020/day_08.m
+++ 2020/day_08.m
@@ -2,7 +2,7 @@
 :- module day_08.
 :- interface.
 
-:- import_module array, io.
+:- import_module array, bool, io.
 
 :- type opcode ---> acc; jmp; nop.
 :- type instruction ---> {opcode, int}.
@@ -17,7 +17,7 @@
 :- pred program_input(res::out, program::array_uo, io::di, io::uo) is det.
 
 :- pred run_instruction(instruction::in, int::in, int::out, int::in, int::out) is det.
-:- pred run_program(program::in, int::in, int::out, int::in, int::out) is semidet.
+:- pred run_program(program::in, bool::out, int::in, int::out, int::in, int::out) is det.
 
 :- implementation.
 
@@ -55,17 +55,17 @@ run_instruction({acc, Arg}, !Acc, !IP) :- !:Acc = !.Acc + Arg, !:IP = !.IP + 1.
 run_instruction({jmp, Arg}, !Acc, !IP) :- !:IP = !.IP + Arg.
 run_instruction({nop, _}, !Acc, !IP) :- !:IP = !.IP + 1.
 
-run_program(Program, !Acc, !IP) :-
-    run_program(Program, empty, !Acc, !IP).
+run_program(Program, Loop, !Acc, !IP) :-
+    run_program(Program, Loop, empty, !Acc, !IP).
 
-:- pred run_program(program::in, ranges::in, int::in, int::out, int::in, int::out) is semidet.
-run_program(Program, Seen0, !Acc, !IP) :-
+:- pred run_program(program::in, bool::out, ranges::in, int::in, int::out, int::in, int::out) is det.
+run_program(Program, Loop, Seen0, !Acc, !IP) :-
     ( member(!.IP, Seen0) ->
-      %% Loop found, stop.
-      true
-    ; %% Run next instruction.
-      semidet_lookup(Program, !.IP, Instruction),
+      Loop = yes
+    ; !.IP < size(Program) ->
+      lookup(Program, !.IP, Instruction),
       insert(!.IP, Seen0, Seen),
       run_instruction(Instruction, !Acc, !IP),
-      run_program(Program, Seen, !Acc, !IP)
+      run_program(Program, Loop, Seen, !Acc, !IP)
+    ; Loop = no
     ).
--- 2020/day_08_1.m
+++ 2020/day_08_1.m
@@ -8,14 +8,16 @@
 
 :- implementation.
 
+:- import_module bool.
 :- import_module day_08, io_ext.
 
 main(!IO) :-
     program_input(ReadResult, Program, !IO),
     ( ReadResult = ok,
-      ( run_program(Program, 0, Acc, 0, _) ->
+      run_program(Program, Loop, 0, Acc, 0, _),
+      ( Loop = yes ->
         write_int(Acc, !IO), nl(!IO)
-      ; error_exit(1, "Program error", !IO)
+      ; error_exit(1, "Program terminates", !IO)
       )
     ; ReadResult = error(Error),
       error_exit(1, error_message(Error), !IO)

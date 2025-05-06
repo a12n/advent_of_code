a1a3bb690fbd08d40382c76512a6aff274d108c9
Anton Yabchinskiy <arn@bestmx.net>
2025-05-07 01:51:06 +0200

    day_07_1: remove unused

--- 2020/day_07_1.m
+++ 2020/day_07_1.m
@@ -8,39 +8,8 @@
 
 :- implementation.
 
-:- import_module int, list, multi_map, solutions, string.
+:- import_module list, multi_map, solutions, string.
 :- import_module io_ext.
-
-:- pred contain(string, string).
-:- mode contain(in, out) is nondet.
-:- mode contain(out, in) is nondet.
-contain("light red", "bright white").
-contain("light red", "muted yellow").
-contain("dark orange", "bright white").
-contain("dark orange", "muted yellow").
-contain("bright white", "shiny gold").
-contain("muted yellow", "shiny gold").
-contain("muted yellow", "faded blue").
-contain("shiny gold", "dark olive").
-contain("shiny gold", "vibrant plum").
-contain("dark olive", "faded blue").
-contain("dark olive", "dotted black").
-contain("vibrant plum", "faded blue").
-contain("vibrant plum", "dotted black").
-%% contain("faded blue", _).
-%% contain("dotted black", _).
-
-:- pred eventually_contain(string, string).
-:- mode eventually_contain(out, in) is nondet.
-eventually_contain(X, Y) :-
-    ( contain(X, Y)
-    ; contain(Z, Y), eventually_contain(X, Z)
-    ).
-
-:- pred eventually_contain_shiny_gold(string).
-:- mode eventually_contain_shiny_gold(out) is nondet.
-eventually_contain_shiny_gold(Y) :-
-    eventually_contain(Y, "shiny gold").
 
 :- pred simplified(string, string).
 :- mode simplified(in, out) is det.

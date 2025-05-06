c9331c2ac6a5c30f7e5189fe27de40caafee982c
Anton Yabchinskiy <arn@bestmx.net>
2025-05-07 01:18:33 +0200

    day_07_1: WIP

--- 2020/day_07_1.m
+++ 2020/day_07_1.m
@@ -8,7 +8,7 @@
 
 :- implementation.
 
-:- import_module int, list, map, solutions, string.
+:- import_module int, list, multi_map, solutions, string.
 :- import_module io_ext.
 
 :- pred contain(string, string).
@@ -52,43 +52,51 @@ simplified(!String) :-
     replace_all(!.String, ".", "", !:String),
     replace_all(!.String, ", ", ",", !:String).
 
-:- pred contain_string(string::in, string::out, list({int, string})::out) is semidet.
-contain_string(String, Bag, ContainBags) :-
+:- pred contain_string(string::in, string::out, list(string)::out) is semidet.
+contain_string(String, Bag, Contains) :-
     [Bag, String1] = split_at_char(':', String),
-    map((pred(String2::in, {N, ChildBag}::out) is semidet :-
+    map((pred(String2::in, ChildBag::out) is semidet :-
              replace(String2, " ", "=", String3),
-             [NumString, ChildBag] = split_at_char('=', String3),
-             to_int(NumString, N)
+             [_, ChildBag] = split_at_char('=', String3)
         ),
         negated_filter(is_empty, split_at_char(',', String1)),
-        ContainBags
+        Contains
        ).
 
-:- pred main_loop(map(string, string)::in, io::di, io::uo) is det.
+:- pred contained(multi_map(string, string)::in, string::in, string::out) is nondet.
+contained(Parents, Bag, Result) :-
+    nondet_search(Parents, Bag, Parent),
+    ( Result = Parent
+    ; contained(Parents, Parent, Result)
+    ).
+
+:- pred main_loop(multi_map(string, string)::in, io::di, io::uo) is det.
 main_loop(Mapping, !IO) :-
     read_line_as_string(ReadResult, !IO),
     ( ReadResult = ok(Line),
       String0 = chomp(Line),
       simplified(String0, String1),
       format("\"%s\" -> \"%s\"\n", [s(String0), s(String1)], !IO),
-      ( contain_string(String1, Bag, ContainBags) ->
+      ( contain_string(String1, Bag, Contains) ->
         format("Bag \"%s\" ", [s(Bag)], !IO),
-        print_line(ContainBags, !IO),
+        print_line(Contains, !IO),
 
-        map((pred({_, ChildBag}::in, ChildBag::out) is det), ContainBags, ChildBags),
-        foldl(set(Bag), ChildBags, Mapping, Mapping2),
+        foldl((pred(UpLevel::in, Map0::in, Map::out) is det :-
+                   add(UpLevel, Bag, Map0, Map)),
+              Contains, Mapping, Mapping2),
 
         main_loop(Mapping2, !IO)
       ; error_exit(1, "Couldn't parse string", !IO)
       )
     ; ReadResult = eof,
       to_assoc_list(Mapping, AssocList),
-      print_line(AssocList, !IO)
+      print_line(AssocList, !IO),
+      solutions(contained(Mapping, "shiny gold"), Solutions),
+      length(Solutions, N),
+      print_line(N, !IO)
     ; ReadResult = error(Error),
       error_exit(1, error_message(Error), !IO)
     ).
 
 main(!IO) :-
-    %% %% solutions(eventually_contain_shiny_gold, Bags),
-    %% %% print_line(Bags, !IO).
     main_loop(init, !IO).

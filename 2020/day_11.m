83ddbc89ffee8de497846371099429a353385088
Anton Yabchinskiy <arn@bestmx.net>
2025-05-12 01:53:00 +0200

    day_11: implementation of simulate/2

--- 2020/day_11.m
+++ 2020/day_11.m
@@ -10,15 +10,18 @@
 :- type seat_layout == array2d(maybe(seat)).
 
 :- pred input_seat_layout(res(seat_layout)::out, io::di, io::uo) is det.
-:- pred simulate(seat_layout::array2d_di, seat_layout::array2d_uo) is det.
+:- pred simulate(seat_layout::in, seat_layout::array2d_uo) is det.
 
 :- implementation.
 
+:- import_module bool.
 :- import_module char.
+:- import_module int.
 :- import_module list.
-:- import_module string.
 
 :- import_module array2d_ext.
+:- import_module grid.
+:- import_module grid.plane.
 :- import_module io_ext.
 
 :- pred seat_char(char, maybe(seat)).
@@ -39,11 +42,25 @@ input_seat_layout(Result, !IO) :-
       Result = error(Error)
     ).
 
-simulate(!Seats) :-
+simulate(!Seats) :- simulate(bounds(!.Seats), !Seats).
+
+:- pred simulate(extent::in, seat_layout::in, seat_layout::out) is det.
+simulate(Extent, !Seats) :-
     map_foldl(
-        (pred(Row::in, Col::in, !.Elt::in, !:Elt::out, !.N::in, !:N::out) is det :-
-             %% TODO
-             true
-        ), !Seats, 0, N
+        (pred(Row::in, Col::in, !.Elt::in, !:Elt::out, !.Changed::in, !:Changed::out) is det :-
+             NeighborPos = filter(in_bounds(Extent), moore_neighbors(pos(Col, Row))),
+             Num = length(filter(unify(yes(occupied)), map(lookup(!.Seats), NeighborPos))),
+             ( !.Elt = yes(empty),    Num = 0 -> !:Elt = yes(occupied), !:Changed = yes
+             ; !.Elt = yes(occupied), Num > 3 -> !:Elt = yes(empty),    !:Changed = yes
+             ; true
+             )
+        ), !Seats, no, Changed
     ),
-    ( N \= 0 -> simulate(!Seats); true ).
+    ( Changed = yes -> simulate(Extent, !Seats); true ).
+
+:- func lookup(seat_layout, pos) = maybe(seat).
+lookup(Array, pos(Col, Row)) = array2d.lookup(Array, Row, Col).
+
+:- func bounds(seat_layout) = extent.
+bounds(Array) = extent(pos(0, 0), pos(NumCols, NumRows)) :-
+    array2d.bounds(Array, NumRows, NumCols).

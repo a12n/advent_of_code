bc05420e1d44c30c412f3ce7b4168b8fb7c0ed07
Anton Yabchinskiy <arn@bestmx.net>
2025-05-11 16:11:01 +0200

    day_11_1: simulate and print number of occupied seats

---
+++ 2020/day_11_1.m
@@ -0,0 +1,26 @@
+%% -*- mode: mercury -*-
+:- module day_11_1.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module array2d.
+:- import_module list.
+:- import_module maybe.
+:- import_module require.
+
+:- import_module day_11.
+
+main(!IO) :-
+    input_seat_layout(Result, !IO),
+    ( Result = ok(Seats),
+      simulate(Seats, FinalSeats),
+      N = length(filter(unify(yes(occupied)), condense(lists(FinalSeats)))),
+      write_int(N, !IO), nl(!IO)
+    ; Result = error(Error),
+      error(error_message(Error))
+    ).

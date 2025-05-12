5f1aab70aaaa1598bed331231a7a068d139d76e0
Anton Yabchinskiy <arn@bestmx.net>
2025-05-12 02:04:18 +0200

    grid.plane: add foldl/4

--- 2020/grid.plane.m
+++ 2020/grid.plane.m
@@ -11,6 +11,11 @@
 %% Grid extent as [Begin, End) positions.
 :- type extent ---> extent(pos, pos).
 
+%% Iterate over all extent positions.
+:- pred foldl(pred(pos, A, A), extent, A, A).
+:- mode foldl(pred(in, in, out) is det, in, in, out) is det.
+:- mode foldl(pred(in, in, out) is semidet, in, in, out) is semidet.
+
 %% True if position is within grid extent.
 :- pred in_bounds(extent::in, pos::in) is semidet.
 
@@ -23,6 +28,18 @@
 :- implementation.
 
 :- import_module int.
+
+foldl(Pred, extent(Pos, _) @ Extent, !Accum) :-
+    foldl(Pred, Extent, Pos, !Accum).
+
+:- pred foldl(pred(pos, A, A), extent, pos, A, A).
+:- mode foldl(in(pred(in, in, out) is det), in, in, in, out) is det.
+:- mode foldl(in(pred(in, in, out) is semidet), in, in, in, out) is semidet.
+foldl(Pred, extent(pos(X0, _), pos(Xn, Yn)) @ Extent, pos(X, Y) @ Pos, !Accum) :-
+    ( Y = Yn -> true
+    ; X = Xn -> foldl(Pred, Extent, pos(X0, Y + 1), !Accum)
+    ; Pred(Pos, !Accum), foldl(Pred, Extent, pos(X + 1, Y), !Accum)
+    ).
 
 in_bounds(extent(pos(X0, Y0), pos(Xn, Yn)), pos(X, Y)) :-
     X >= X0, X < Xn,

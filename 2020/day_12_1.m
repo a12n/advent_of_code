95ebbd246cf53954bf3fd711092c4d46b6ff25c1
Anton Yabchinskiy <arn@bestmx.net>
2025-05-14 01:54:35 +0200

    day_12_1: rename ShipPos and ShipDir

--- 2020/day_12_1.m
+++ 2020/day_12_1.m
@@ -18,16 +18,16 @@
 main(!IO) :- main(pos(0, 0), _, '→', _, !IO).
 
 :- pred main(pos::in, pos::out, dir::in, dir::out, io::di, io::uo) is det.
-main(!Pos, !Dir, !IO) :-
+main(!ShipPos, !ShipDir, !IO) :-
     read_line_as_string(ReadResult, !IO),
     ( ReadResult = ok(Line),
       ( command_string(Command, chomp(Line)) ->
-        basic_navigation(Command, !Pos, !Dir),
-        main(!Pos, !Dir, !IO)
+        basic_navigation(Command, !ShipPos, !ShipDir),
+        main(!ShipPos, !ShipDir, !IO)
       ; error("Invalid input")
       )
     ; ReadResult = eof,
-      write_int(taxicab_dist(!.Pos), !IO), nl(!IO)
+      write_int(taxicab_dist(!.ShipPos), !IO), nl(!IO)
     ; ReadResult = error(Error),
       error(error_message(Error))
     ).

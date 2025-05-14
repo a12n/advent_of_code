f66221126b02ba2cb32fc7a4c209e995b30a9f9b
Anton Yabchinskiy <arn@bestmx.net>
2025-05-15 00:21:20 +0200

    day_13_1: rewrite with >>

--- 2020/day_13_1.m
+++ 2020/day_13_1.m
@@ -14,22 +14,21 @@
 :- import_module pair.
 :- import_module require.
 
+:- import_module advent.
 :- import_module day_13.
-
-%% Next bus of ID=19 after timestamp=939:
-%% math.ceil(939/19) * 19
 
 main(!IO) :-
     input(Result, !IO),
     ( Result = ok(Arrive - MaybeBuses),
       filter_map(maybe_is_yes, MaybeBuses, Buses),
-      map((pred(Bus::in, Wait::out) is det :-
-               Wait = (Bus - (departure(Arrive, Bus) - Arrive))
-          ), Buses, WaitTimes),
-      ( reduce((func((_ - WaitTimeA) @ A, (_ - WaitTimeB) @ B) =
-                ( WaitTimeA < WaitTimeB -> A ; B )
-               ), WaitTimes, EarliestBus) ->
-        write_int(fst(EarliestBus) * snd(EarliestBus), !IO), nl(!IO)
+      ( (BusID - WaitTime) = (
+            Buses >>
+            map(departure(Arrive)) >>
+            map(plus(-Arrive)) >>
+            map_corresponding(pair, Buses) >>
+            reduce(min_wait_time)
+        ) ->
+        write_int(BusID * WaitTime, !IO), nl(!IO)
       ; error("No solution")
       )
     ; Result = error(Error),
@@ -42,5 +41,8 @@ departure(Arrive, Bus) = Bus * ceil_div(Arrive, Bus).
 :- func ceil_div(int, int) = int.
 ceil_div(N, M) = N // M + ( N rem M \= 0 -> 1; 0 ).
 
-:- pred reduce((func(T, T) = T)::in, list(T)::in, T::out) is semidet.
-reduce(F, [H | T], Ans) :- Ans = foldl(F, T, H).
+:- func reduce((func(T, T) = T), list(T)) = T is semidet.
+reduce(F, [H | T]) = foldl(F, T, H).
+
+:- func min_wait_time(pair(int, int), pair(int, int)) = pair(int, int).
+min_wait_time((_ - WaitTimeA) @ A, (_ - WaitTimeB) @ B) = (WaitTimeA < WaitTimeB -> A; B).

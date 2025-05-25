63660f790aca8fad8d24ae7513dc3925cd29ab00
Anton Yabchinskiy <arn@bestmx.net>
2025-05-25 19:17:26 +0200

    day_14_2: solution

---
+++ 2020/day_14_2.m
@@ -0,0 +1,31 @@
+%% -*- mode: mercury -*-
+:- module day_14_2.
+:- interface.
+
+:- import_module io.
+
+:- pred main(io::di, io::uo) is det.
+
+:- implementation.
+
+:- import_module require.
+
+:- import_module day_14.
+
+main(!IO) :- main(init_mask, _, init_memory, _, !IO).
+
+:- pred main(mask::in, mask::out, memory::in, memory::out, io::di, io::uo) is det.
+main(!Mask, !Memory, !IO) :-
+    read_line_as_string(ReadResult, !IO),
+    ( ReadResult = ok(Line),
+      ( instr_string(Line, Instr) ->
+        decoder_chip_floating(Instr, !Mask, !Memory),
+        main(!Mask, !Memory, !IO)
+      ; error("Invalid input")
+      )
+    ; ReadResult = eof,
+      %% write_uint(!.Memory^sum, !IO), nl(!IO)
+      write_uint(memory_sum(!.Memory), !IO), nl(!IO)
+    ; ReadResult = error(Error),
+      error(error_message(Error))
+    ).

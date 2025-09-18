f8aaa70fdc8aa1dffa3b9fb6d495e046e22cbf0f
Anton Yabchinskiy <arn@bestmx.net>
2025-09-18 09:37:22 +0200

    2019: solution for day 01

---
+++ 2019/01.cpp
@@ -0,0 +1,44 @@
+#include "advent.hpp"
+
+namespace {
+
+using mass_t = long long;
+
+mass_t fuel_required(mass_t mass)
+{
+    return mass / 3 - 2;
+}
+
+mass_t total_fuel_required(mass_t mass)
+{
+    mass_t total_fuel = 0;
+
+    while (true) {
+        if ((mass = fuel_required(mass)) <= 0) {
+            break;
+        }
+        total_fuel += mass;
+    }
+
+    return total_fuel;
+}
+
+} // namespace
+
+int main()
+{
+    mass_t mass, total_fuel = 0;
+
+    while (cin >> mass) {
+        total_fuel +=
+#if PART == 1
+            fuel_required(mass)
+#elif PART == 2
+            total_fuel_required(mass)
+#endif // PART
+            ;
+    }
+    cout << total_fuel << endl;
+
+    return 0;
+}
---
+++ 2019/advent.hpp
@@ -0,0 +1,26 @@
+#ifndef ADVENT_HPP
+#define ADVENT_HPP
+
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::function;
+using std::invalid_argument;
+using std::max;
+using std::min;
+using std::nullopt;
+using std::optional;
+using std::runtime_error;
+using std::string;
+using std::vector;
+
+#endif  // ADVENT_HPP

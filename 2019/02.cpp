af2b2688e9ddb490530338cd0188d6d1df459d94
Anton Yabchinskiy <arn@bestmx.net>
2025-09-19 21:44:03 +0200

    02: solution

---
+++ 2019/02.cpp
@@ -0,0 +1,48 @@
+#include "advent.hpp"
+
+#if PART == 2
+namespace {
+
+std::optional<std::pair<intcode::value, intcode::value>> gravity_assist(const intcode::memory& p, intcode::value k)
+{
+    for (intcode::value noun = 0; noun < 100; ++noun) {
+        for (intcode::value verb = 0; verb < 100; ++verb) {
+            intcode::memory t = p;
+            t[1] = noun;
+            t[2] = verb;
+            intcode::run(t, 0);
+            if (t[0] == k) {
+                return { { noun, verb } };
+            }
+        }
+    }
+    return nullopt;
+}
+
+} // namespace
+#endif // PART
+
+int main()
+{
+    intcode::memory p;
+
+    if (!(std::cin >> p)) {
+        return 1;
+    }
+
+#if PART == 1
+    p[1] = 12;
+    p[2] = 2;
+    intcode::run(p, 0);
+    std::cout << p[0] << '\n';
+#elif PART == 2
+    const auto params = gravity_assist(p, 19690720);
+    if (!params) {
+        return 1;
+    }
+    std::cerr << params->first << ' ' << params->second << '\n';
+    std::cout << 100 * params->first + params->second << '\n';
+#endif // PART
+
+    return 0;
+}

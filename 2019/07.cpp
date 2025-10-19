08d60dc387c13e25c0be6df9088a31232cdb02dd
Anton Yabchinskiy <arn@intabs.net>
2025-10-20 01:59:28 +0200

    07-1: add amplifier_controller(), add test()

---
+++ 2019/07.cpp
@@ -0,0 +1,57 @@
+#include "advent.hpp"
+
+namespace {
+
+intcode::value amplifier_controller(
+    const intcode::memory& prog, const std::array<intcode::value, 5>& phase)
+{
+    intcode::pipe_environ env;
+    intcode::value signal = 0;
+
+    for (size_t i = 0; i < phase.size(); ++i) {
+        intcode::memory img = prog;
+        env.output(phase[i]);
+        env.output(signal);
+        intcode::run(img, 0, env);
+        signal = env.input();
+    }
+
+    return signal;
+}
+
+} // namespace
+
+int main()
+{
+    intcode::memory img;
+
+    if (!(std::cin >> img)) {
+        return EX_DATAERR;
+    }
+
+    intcode::value max_signal = 0;
+
+    // TODO
+
+    std::cout << max_signal << '\n';
+
+    return 0;
+}
+
+__attribute__((constructor)) void test()
+{
+#if PART == 1
+    assert(amplifier_controller(
+               { 3, 15, 3, 16, 1002, 16, 10, 16, 1, 16, 15, 15, 4, 15, 99, 0, 0 },
+               { 4, 3, 2, 1, 0 })
+        == 43210);
+    assert(amplifier_controller(
+               { 3, 23, 3, 24, 1002, 24, 10, 24, 1002, 23, -1, 23, 101, 5, 23, 23, 1, 24, 23, 23, 4, 23, 99, 0, 0 },
+               { 0, 1, 2, 3, 4 })
+        == 54321);
+    assert(amplifier_controller(
+               { 3, 31, 3, 32, 1002, 32, 10, 32, 1001, 31, -2, 31, 1007, 31, 0, 33, 1002, 33, 7, 33, 1, 33, 31, 31, 1, 32, 31, 31, 4, 31, 99, 0, 0, 0 },
+               { 1, 0, 4, 3, 2 })
+        == 65210);
+#endif // PART
+}

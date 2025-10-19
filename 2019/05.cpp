ddf0282caeb70150555ed1260a8dde67d7a3a323
Anton Yabchinskiy <arn@intabs.net>
2025-10-19 13:24:30 +0200

    05-2: test from puzzle description

--- 2019/05.cpp
+++ 2019/05.cpp
@@ -36,5 +36,21 @@ __attribute__((constructor)) void test()
         intcode::test(prog, 0, 8, { 8 }, { 8 < 8 });
         intcode::test(prog, 0, 8, { 9 }, { 9 < 8 });
     };
+
+    {
+        // "…example program uses an input instruction to ask for a
+        // single number. The program will then output 999 if the
+        // input value is below 8, output 1000 if the input value is
+        // equal to 8, or output 1001 if the input value is greater
+        // than 8".
+        const intcode::memory prog {
+            3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+            1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+            999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
+        };
+        intcode::test(prog, 0, 46, { 5 }, { 999 }); // 5 < 8
+        intcode::test(prog, 0, 46, { 8 }, { 1000 }); // 8 == 8
+        intcode::test(prog, 0, 46, { 88 }, { 1001 }); // 88 > 8
+    };
 #endif // PART
 }

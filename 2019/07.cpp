bc116fda482a765bf3c44038f016b25a1b0902da
Anton Yabchinskiy <arn@intabs.net>
2025-10-20 02:05:26 +0200

    07-1: WIP on solution

--- 2019/07.cpp
+++ 2019/07.cpp
@@ -31,7 +31,30 @@ int main()
 
     intcode::value max_signal = 0;
 
-    // TODO
+    // FIXME
+    for (int a = 0; a < 5; ++a) {
+        for (int b = 0; b < 5; ++b) {
+            if (b == a) {
+                continue;
+            }
+            for (int c = 0; c < 5; ++c) {
+                if (c == a || c == b) {
+                    continue;
+                }
+                for (int d = 0; d < 5; ++d) {
+                    if (d == a || d == b || d == c) {
+                        continue;
+                    }
+                    for (int e = 0; e < 5; ++e) {
+                        if (e == a || e == b || e == c || e == d) {
+                            continue;
+                        }
+                        max_signal = std::max(max_signal, amplifier_controller(img, { a, b, c, d, e }));
+                    }
+                }
+            }
+        }
+    }
 
     std::cout << max_signal << '\n';
 

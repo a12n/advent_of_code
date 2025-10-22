a75a6c8e58eaec4a33570ce0fb8db1450819a86e
Anton Yabchinskiy <arn@bestmx.net>
2025-10-22 20:20:05 +0200

    ansi: WIP for day 13

---
+++ 2019/ansi.hpp
@@ -0,0 +1,31 @@
+#ifndef ANSI_HPP
+#define ANSI_HPP
+
+#include <ostream>
+
+namespace ansi {
+
+struct erase {
+    enum {
+        end = 0,
+        begin = 1,
+        entire = 2,
+    } what
+        = entire;
+};
+
+struct hide_cursor {
+    bool hide = true;
+};
+
+struct cursor_position {
+    size_t row, col;
+};
+
+std::ostream& operator<<(std::ostream& out, erase cmd);
+std::ostream& operator<<(std::ostream& out, hide_cursor cmd);
+std::ostream& operator<<(std::ostream& out, cursor_position cmd);
+
+} // namespace ansi
+
+#endif  // ANSI_HPP

e4781a59f8579b066a72fffef858a0a1b8de5b04
Anton Yabchinskiy <arn@intabs.net>
2025-11-14 00:01:45 +0100

    16-1: brute force solution

--- 2019/16.cpp
+++ 2019/16.cpp
@@ -1,12 +1,39 @@
+#include <array>
 #include <iostream>
 #include <string>
+#include <string_view>
 
 namespace {
 
-std::string fft(const std::string& s)
+
+const std::array<int, 4> base_pattern = { 0, 1, 0, -1 };
+
+// Index into base pattern.
+//
+// i j>0 1 2 3 4 5 6 7 8 9 A B C D E F 0 1
+// v______________________________________
+// 0 : 0 1 2 3 4 5
+// 1 : 0 0 1 1 2 2 3 3 4 4 5 5
+// 2 : 0 0 0 1 1 1 2 2 2 3 3 3 4 4 4 5 5 5
+//
+// j/(i+1)
+// (j+1)/(i+1)
+inline size_t index(size_t i, size_t j)
 {
-    // TODO
-    return s;
+    return ((j + 1) / (i + 1)) % base_pattern.size();
+}
+
+std::string fft(std::string_view s)
+{
+    std::string t(s.size(), '\0');
+    for (size_t i = 0; i < s.size(); ++i) {
+        int n = 0;
+        for (size_t j = 0; j < s.size(); ++j) {
+            n += (s[j] - '0') * base_pattern[index(i, j)];
+        }
+        t[i] = (std::abs(n) % 10) + '0';
+    }
+    return t;
 }
 
 } // namespace
@@ -22,12 +49,12 @@ int main()
         phases = std::stol(phases_env);
     }
 
-    std::cerr << "signal '" << signal << "' phases " << phases << '\n';
+    std::cerr << "signal " << std::quoted(signal) << " phases " << phases << '\n';
     while (phases-- > 0) {
-        signal=fft(signal);
+        signal = fft(signal);
     }
 
-    std::cerr << signal.substr(0, 8) << '\n';
+    std::cout << signal.substr(0, 8) << '\n';
 
     return 0;
 }

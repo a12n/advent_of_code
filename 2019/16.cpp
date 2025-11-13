d5d461da08e36163dccf67165cc0ab1a10b64b15
Anton Yabchinskiy <arn@intabs.net>
2025-11-13 20:26:19 +0100

    16-1: PHASES env variable

--- 2019/16.cpp
+++ 2019/16.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
 #include <string>
 
+namespace {
+
+std::string fft(const std::string& s)
+{
+    // TODO
+    return s;
+}
+
+} // namespace
+
 int main()
 {
     std::string signal;
+
     std::getline(std::cin, signal);
-    std::cerr << "signal '" << signal << "'\n";
+
+    size_t phases = 100;
+    if (const auto phases_env = getenv("PHASES"); phases_env) {
+        phases = std::stol(phases_env);
+    }
+
+    std::cerr << "signal '" << signal << "' phases " << phases << '\n';
+    while (phases-- > 0) {
+        signal=fft(signal);
+    }
+
+    std::cerr << signal.substr(0, 8) << '\n';
+
     return 0;
 }

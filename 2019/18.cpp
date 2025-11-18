a1d28356233eb5acdf3759aadbbfcc64e15938fc
Anton Yabchinskiy <arn@intabs.net>
2025-11-18 09:48:00 +0100

    18: remove input specialization

--- 2019/18.cpp
+++ 2019/18.cpp
@@ -4,8 +4,6 @@
 #include <string>
 #include <vector>
 
-#include "input.hpp"
-
 namespace {
 
 using distance_map = std::map<char, std::map<char, size_t>>;
@@ -19,32 +17,8 @@ distance_map distances(const vault_map& vault)
 
 } // namespace
 
-template <>
-std::optional<vault_map> input<vault_map>(std::istream& s)
-{
-    vault_map vault;
-    while (true) {
-        vault.emplace_back();
-        if (!std::getline(s, vault.back())) {
-            if (s.eof()) {
-                vault.pop_back();
-                if (vault.empty()) {
-                    return {};
-                } else {
-                    return vault;
-                }
-            } else {
-                throw std::invalid_argument(__func__);
-            }
-        }
-        assert(vault.back().size() == vault.front().size());
-    }
-}
-
 int main()
 {
-    const auto dists = distances(input<vault_map>(std::cin).value());
-
     // TODO
 
     return 0;

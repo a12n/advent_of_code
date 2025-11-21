d369bf4f6a4b9a9031caf582f16ee00cdb7c9c5c
Anton Yabchinskiy <arn@intabs.net>
2025-11-22 00:31:32 +0100

    21-1: move comment inside PART == 1 block

--- 2019/21.cpp
+++ 2019/21.cpp
@@ -1,43 +1,35 @@
 #include "intcode.hpp"
 
-/*
-J ABCD
-0 1111
-0 1110
-1 1101
-0 1100
-1 1011
-_ 1010 X
-1 1001
-_ 1000 X
-1 0111
-_ 0110 X
-1 0101
-_ 0100 X
-1 0011
-_ 0010 X
-1 0001
-_ 0000 X
-
-(!A || !B || !C) && D
-((!A || !B) || !C) && D
-(!(A && B) || !C) && D
-!((A && B) && C) && D
-!(A && B && C) && D
-
-NOT A T
-NOT T T	; T = A
-AND B T	; T = A && B
-AND C T	; T = A && B && C
-NOT T J	; J = !T
-AND D J	; J = D && J
- */
-
 int main(int argc, char* argv[])
 {
     auto img = intcode::load(argc, argv);
     std::string_view script =
 #if PART == 1
+        /*
+        J ABCD
+        0 1111
+        0 1110
+        1 1101
+        0 1100
+        1 1011
+        _ 1010 X
+        1 1001
+        _ 1000 X
+        1 0111
+        _ 0110 X
+        1 0101
+        _ 0100 X
+        1 0011
+        _ 0010 X
+        1 0001
+        _ 0000 X
+
+        (!A || !B || !C) && D
+        ((!A || !B) || !C) && D
+        (!(A && B) || !C) && D
+        !((A && B) && C) && D
+        !(A && B && C) && D
+         */
         "NOT A T\n"
         "NOT T T\n"
         "AND B T\n"

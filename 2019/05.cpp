bac6f395e53ae765bef4867a940066a4ebd5a99e
Anton Yabchinskiy <arn@intabs.net>
2025-11-22 00:11:53 +0100

    05: fix intcode usage

--- 2019/05.cpp
+++ 2019/05.cpp
@@ -2,32 +2,34 @@
 
 int main()
 {
-    auto img = intcode::load(0, nullptr);
-    intcode::run(img);
+    using namespace intcode;
+    auto img = load(0, nullptr);
+    run(img, input_stream_line(std::cin), output_stream_line(std::cout));
     return 0;
 }
 
 int test()
 {
+    using namespace intcode;
 #if PART == 2
     {
         // Position mode, input is equal to 8
-        const intcode::memory prog { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 };
-        intcode::test(prog, 8, { -1 }, { -1 == 8 });
-        intcode::test(prog, 8, { 0 }, { 0 == 8 });
-        intcode::test(prog, 8, { 7 }, { 7 == 8 });
-        intcode::test(prog, 8, { 8 }, { 8 == 8 });
-        intcode::test(prog, 8, { 9 }, { 9 == 8 });
+        const memory prog { 3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8 };
+        assert(8 == run_copy(prog, input_value(-1), output_assert(-1 == 8)));
+        assert(8 == run_copy(prog, input_value(0), output_assert(0 == 8)));
+        assert(8 == run_copy(prog, input_value(7), output_assert(7 == 8)));
+        assert(8 == run_copy(prog, input_value(8), output_assert(8 == 8)));
+        assert(8 == run_copy(prog, input_value(9), output_assert(9 == 8)));
     };
 
     {
         // Immediate mode, input is less than 8
-        const intcode::memory prog { 3, 3, 1107, -1, 8, 3, 4, 3, 99 };
-        intcode::test(prog, 8, { -1 }, { -1 < 8 });
-        intcode::test(prog, 8, { 0 }, { 0 < 8 });
-        intcode::test(prog, 8, { 7 }, { 7 < 8 });
-        intcode::test(prog, 8, { 8 }, { 8 < 8 });
-        intcode::test(prog, 8, { 9 }, { 9 < 8 });
+        const memory prog { 3, 3, 1107, -1, 8, 3, 4, 3, 99 };
+        assert(8 == run_copy(prog, input_value(-1), output_assert(-1 < 8)));
+        assert(8 == run_copy(prog, input_value(0), output_assert(0 < 8)));
+        assert(8 == run_copy(prog, input_value(7), output_assert(7 < 8)));
+        assert(8 == run_copy(prog, input_value(8), output_assert(8 < 8)));
+        assert(8 == run_copy(prog, input_value(9), output_assert(9 < 8)));
     };
 
     {
@@ -36,14 +38,14 @@ int test()
         // input value is below 8, output 1000 if the input value is
         // equal to 8, or output 1001 if the input value is greater
         // than 8".
-        const intcode::memory prog {
+        const memory prog {
             3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
             1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
             999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99
         };
-        intcode::test(prog, 46, { 5 }, { 999 }); // 5 < 8
-        intcode::test(prog, 46, { 8 }, { 1000 }); // 8 == 8
-        intcode::test(prog, 46, { 88 }, { 1001 }); // 88 > 8
+        assert(46 == run_copy(prog, input_value(5), output_assert(999))); // 5 < 8
+        assert(46 == run_copy(prog, input_value(8), output_assert(1000))); // 8 == 8
+        assert(46 == run_copy(prog, input_value(88), output_assert(1001))); // 88 > 8
     };
 #endif // PART
     return 0;

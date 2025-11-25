a1feb80390494a8f9830c37a0c419f38ad08ec6c
Anton Yabchinskiy <arn@intabs.net>
2025-11-25 01:18:34 +0100

    24-2: WIP on solution

--- 2019/24.cpp
+++ 2019/24.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <set>
 
 #include "grid.hpp"
@@ -43,21 +44,20 @@ size_t biodiversity(const bugs_grid& grid)
 
 #elif PART == 2
 // Level and grid position.
-using bug_position = std::tuple<int64_t, position>;
+using bug_position = std::tuple<int16_t, int8_t, int8_t>;
 using bug_set = std::set<bug_position>;
 
 // Next positions on the grid of adjacent level, from the specified
 // direction.
 template <typename func_type>
-void for_each_adjacent_edge(direction dir, int64_t p_level, func_type func)
+void for_each_adjacent_edge(direction dir, int64_t level, func_type func)
 {
     for (int64_t i = 0; i < n; ++i) {
-        const auto p = dir == direction::up ? position{ i, n - 1 }
-            : dir == direction::left            ? position{ n - 1, i }
-            : dir == direction::right           ? position{ 0, i }
-            : dir == direction::down            ? position{ i, 0 }
-                                                : throw std::invalid_argument(__func__);
-        func(bug_position(p_level, p));
+        func(dir == direction::up         ? bug_position(level, i, n - 1)
+                : dir == direction::left  ? bug_position(level, n - 1, i)
+                : dir == direction::right ? bug_position(level, 0, i)
+                : dir == direction::down  ? bug_position(level, i, 0)
+                                          : throw std::invalid_argument(__func__));
     }
 }
 
@@ -66,38 +66,42 @@ void for_each_adjacent_edge(direction dir, int64_t p_level, func_type func)
 template <typename func_type>
 void for_each_adjacent_dir(const bug_position& bug, direction dir, func_type func)
 {
-    const auto [p_level, p] = bug;
+    const auto [level, x, y] = bug;
     if (dir == direction::up) {
-        if (p[1] == 0) {
-            for_each_adjacent_edge(dir, p_level - 1, func);
-        } else if (p[1] == (n / 2) + 1) {
-            for_each_adjacent_edge(dir, p_level + 1, func);
+        if (y == 0) {
+            func(bug_position(level - 1, n / 2, (n / 2) - 1));
+        } else if (x == (n / 2) && y == (n / 2) + 1) {
+            for_each_adjacent_edge(dir, level + 1, func);
         } else {
-            func(bug_position(p_level, p + to_offset(dir)));
+            // func(bug_position(p_level, p + to_offset(dir)));
+            func(bug_position ( level, x, y - 1 ));
         }
     } else if (dir == direction::left) {
-        if (p[0] == 0) {
-            for_each_adjacent_edge(dir, p_level - 1, func);
-        } else if (p[0] == (n / 2) + 1) {
-            for_each_adjacent_edge(dir, p_level + 1, func);
+        if (x == 0) {
+            func(bug_position(level-1,(n/2)-1,n/2));
+        } else if (x == (n / 2) + 1 && y == (n / 2)) {
+            for_each_adjacent_edge(dir, level + 1, func);
         } else {
-            func(bug_position(p_level, p + to_offset(dir)));
+            // func(bug_position(p_level, p + to_offset(dir)));
+            func(bug_position ( level,x - 1, y ));
         }
     } else if (dir == direction::right) {
-        if (p[0] == n - 1) {
-            for_each_adjacent_edge(dir, p_level - 1, func);
-        } else if (p[0] == (n / 2) - 1) {
-            for_each_adjacent_edge(dir, p_level + 1, func);
+        if (x == n - 1) {
+            func(bug_position(level - 1, (n / 2) + 1, (n / 2)));
+        } else if (x == (n / 2) - 1 && y == (n / 2)) {
+            for_each_adjacent_edge(dir, level + 1, func);
         } else {
-            func(bug_position(p_level, p + to_offset(dir)));
+            // func(bug_position(p_level, p + to_offset(dir)));
+            func(bug_position ( level, x + 1, y ));
         }
     } else if (dir == direction::down) {
-        if (p[1] == n - 1) {
-            for_each_adjacent_edge(dir, p_level - 1, func);
-        } else if (p[1] == (n / 2) - 1) {
-            for_each_adjacent_edge(dir, p_level + 1, func);
+        if (y == n - 1) {
+            func(bug_position(level - 1, (n / 2), (n / 2) + 1));
+        } else if (x == (n / 2) && y == (n / 2) - 1) {
+            for_each_adjacent_edge(dir, level + 1, func);
         } else {
-            func(bug_position(p_level, p + to_offset(dir)));
+            // func(bug_position(p_level, p + to_offset(dir)));
+            func(bug_position ( level, x, y + 1 ));
         }
     }
 }
@@ -110,6 +114,18 @@ void for_each_adjacent(const bug_position& bug, func_type func)
     for (const auto dir : { direction::up, direction::left, direction::right, direction::down }) {
         for_each_adjacent_dir(bug, dir, func);
     }
+}
+
+bug_set adjacent(const bug_position& bug)
+{
+    bug_set adj;
+    std::cerr << __func__ << ": {" << std::get<0>(bug) << ' ' << int(std::get<1>(bug)) << ' ' << int(std::get<2>(bug)) << "} = [";
+    for_each_adjacent(bug, [&adj](const bug_position& p) {
+        std::cerr << " {" << std::get<0>(p) << ' ' << int(std::get<1>(p)) << ' ' << int(std::get<2>(p)) << "}";
+        adj.insert(p);
+    });
+    std::cerr << " ]\n";
+    return adj;
 }
 
 bug_set simulate(const bug_set& grid)
@@ -170,6 +186,8 @@ int main()
 
     std::cout << biodiversity(grid) << '\n';
 #elif PART == 2
+    // int test(void);
+    // return test();
     bug_set grid;
 
     for_each(std::cin, [&grid](position p, char c) {
@@ -177,7 +195,8 @@ int main()
             throw std::invalid_argument(__func__);
         }
         if (c == '#') {
-            grid.insert(bug_position(0,p));
+            // grid.insert(bug_position(0,p));
+            grid.insert(bug_position(0, p[0], p[1]));
         }
     });
 
@@ -191,8 +210,52 @@ int main()
         grid = simulate(grid);
     }
 
+    // for (const auto& [p_level, p] : grid) {
+    //     std::cerr << p_level << ' ' << p << '\n';
+    // }
+
     std::cout << grid.size() << '\n';
 #endif // PART
 
+    return 0;
+}
+
+int test()
+{
+    const int16_t l=0;
+#if PART == 2
+    {
+        // Tile 19 has four adjacent tiles: 14, 18, 20, and 24.
+        const bug_set adj = {
+            { l, 3, 2 },
+            { l, 2, 3 },
+            { l, 4, 3 },
+            { l, 3, 4 },
+        };
+        assert(adjacent({ l, 3, 3 }) == adj);
+    }
+
+    {
+        // Tile G has four adjacent tiles: B, F, H, and L.
+        const bug_set adj = {
+            { l + 1, 1, 0 },
+            { l + 1, 0, 1 },
+            { l + 1, 2, 1 },
+            { l + 1, 1, 2 },
+        };
+        assert(adjacent({ l + 1, 1, 1 }) == adj);
+    }
+
+    {
+        // Tile D has four adjacent tiles: 8, C, E, and I.
+        const bug_set adj = {
+            { l, 2, 1 },
+            { l + 1, 2, 0 },
+            { l + 1, 4, 0 },
+            { l + 1, 3, 1 },
+        };
+        assert(adjacent({ l + 1, 3, 0 }) == adj);
+    }
+#endif // PART
     return 0;
 }

30e8ef41c03fcfa1cc12fb509b7e0e0bdecbef15
Anton Yabchinskiy <arn@intabs.net>
2025-11-27 09:50:06 +0100

    25: inline script into the intcode::run() call

--- 2019/25.cpp
+++ 2019/25.cpp
@@ -15,57 +15,56 @@
 int main(int argc, char* argv[])
 {
     auto img = intcode::load(argc, argv);
-    std::string_view script = "south\n"
-                              "south\n"
-                              "take tambourine\n"
-                              "north\n"
-                              "north\n"
-                              // Hull Breach
-                              "west\n"
-                              "south\n"
-                              "take polygon\n"
-                              "north\n"
-                              "east\n"
-                              // Hull Breach
-                              "north\n"
-                              "west\n"
-                              "take boulder\n"
-                              "east\n"
-                              // Navigation
-                              "north\n"
-                              "take manifold\n"
-                              "north\n"
-                              "take hologram\n"
-                              "south\n"
-                              // Observatory
-                              "west\n"
-                              "take fuel cell\n"
-                              "south\n"
-                              "east\n"
-                              "south\n"
-                              "take fixed point\n"
-                              "north\n"
-                              "west\n"
-                              "north\n"
-                              // Holodeck
-                              "north\n"
-                              "take wreath\n"
-                              "east\n"
-                              "east\n"
-                              "inv\n"
-                              "drop boulder\n"
-                              "drop fixed point\n"
-                              "drop fuel cell\n"
-                              "drop hologram\n"
-                              "drop manifold\n"
-                              "drop polygon\n"
-                              "drop tambourine\n"
-                              "drop wreath\n"
-        // Security Checkpoint
-        ;
-    intcode::run(
-        img,
-        intcode::input_string_ascii(script),
+    intcode::run(img,
+        intcode::input_string_ascii(
+            "south\n"
+            "south\n"
+            "take tambourine\n"
+            "north\n"
+            "north\n"
+            // Hull Breach
+            "west\n"
+            "south\n"
+            "take polygon\n"
+            "north\n"
+            "east\n"
+            // Hull Breach
+            "north\n"
+            "west\n"
+            "take boulder\n"
+            "east\n"
+            // Navigation
+            "north\n"
+            "take manifold\n"
+            "north\n"
+            "take hologram\n"
+            "south\n"
+            // Observatory
+            "west\n"
+            "take fuel cell\n"
+            "south\n"
+            "east\n"
+            "south\n"
+            "take fixed point\n"
+            "north\n"
+            "west\n"
+            "north\n"
+            // Holodeck
+            "north\n"
+            "take wreath\n"
+            "east\n"
+            "east\n"
+            "inv\n"
+            "drop boulder\n"
+            "drop fixed point\n"
+            "drop fuel cell\n"
+            "drop hologram\n"
+            "drop manifold\n"
+            "drop polygon\n"
+            "drop tambourine\n"
+            "drop wreath\n"
+            // Security Checkpoint
+            ),
         intcode::output_stream_ascii(std::cout));
     return 0;
 }

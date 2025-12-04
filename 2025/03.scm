a545a328dc79a73940a0141a4205884f7777e308
Anton Yabchinskiy <arn@intabs.net>
2025-12-04 02:03:00 +0100

    03: in largest-joltage, reverse outside of let

--- 2025/03.scm
+++ 2025/03.scm
@@ -15,16 +15,15 @@
 (define (largest-joltage n bank)
   (define (loop m prev-row bank-left)
     (if (= m n) (last prev-row)
-        (let ((next
-               (reverse
-                (fold
-                 (lambda (prev-x left-x ans)
-                   (let ((next-x (+ (* prev-x 10) left-x)))
-                     (if (or (null? ans) (> next-x (car ans)))
-                         (cons next-x ans)
-                         (cons (car ans) ans))))
-                 '() prev-row bank-left))))
-          (loop (+ m 1) next (cdr bank-left)))))
+        (let ((next-rev
+               (fold
+                (lambda (prev-x left-x ans)
+                  (let ((next-x (+ (* prev-x 10) left-x)))
+                    (if (or (null? ans) (> next-x (car ans)))
+                        (cons next-x ans)
+                        (cons (car ans) ans))))
+                '() prev-row bank-left)))
+          (loop (+ m 1) (reverse next-rev) (cdr bank-left)))))
   (loop 0 (circular-list 0) bank))
 
 (define (total-output-joltage n #!optional (sum 0))

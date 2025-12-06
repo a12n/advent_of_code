e0235354d226d2c11296eac2422acc050725b1ad
Anton Yabchinskiy <arn@intabs.net>
2025-12-06 22:12:54 +0100

    06-2: WIP on solution

--- 2025/06.scm
+++ 2025/06.scm
@@ -1,12 +1,17 @@
 #!/usr/bin/env gosh
 
 (import (scheme base)
+        (scheme char)
         (scheme eval)
         (scheme write)
         (srfi 1)
         (srfi 13)
         (advent input)
+        (advent grid)
         (advent main))
+
+;; ---------------------------------------------------------------------------
+;; Part 1
 
 (define (transpose lists)
   (cond
@@ -34,4 +39,62 @@
     (display (fold + 0 answers))
     (newline)))
 
-(main part-1)
+;; ---------------------------------------------------------------------------
+;; Part 2
+
+;; FIXME
+(define (part-2)
+  (let* ((worksheet (lines->grid (read-lines)))
+         (n (grid-rows worksheet))
+         (m (grid-cols worksheet)))
+    (display
+     (let loop ((total 0)
+                (numbers '())
+                (number 0)
+                (col (- m 1))
+                (row 0))
+       (cond
+        ;; Exit condition
+        ((< col 0) total)
+        ;; Done one column
+        ((= row n)
+         (loop total
+               numbers
+               0
+               (- col 1)
+               0))
+        ;; Found digit
+        ((char-numeric? (grid-ref worksheet row col))
+         (loop total
+               numbers
+               (+ (* number 10) (digit-value (grid-ref worksheet row col)))
+               col
+               (+ row 1)))
+        ;; Found space
+        ((char-whitespace? (grid-ref worksheet row col))
+         (loop total
+               (if (zero? number) numbers (cons number numbers))
+               0
+               col
+               (+ row 1)))
+        ;; Found multiplication
+        ((char=? (grid-ref worksheet row col) #\*)
+         (loop (+ total (apply * (if (zero? number) numbers (cons number numbers))))
+               '()
+               0
+               col
+               (+ row 1)))
+        ;; Found addition
+        ((char=? (grid-ref worksheet row col) #\+)
+         (loop (+ total (apply + (if (zero? number) numbers (cons number numbers))))
+               '()
+               0
+               col
+               (+ row 1))))
+       ))
+    (newline)))
+
+;; ---------------------------------------------------------------------------
+;; Main
+
+(main part-1 part-2)

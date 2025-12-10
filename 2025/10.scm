7e6996ad3c65cfb9af642052e396b7d11a6a2b24
Anton Yabchinskiy <arn@intabs.net>
2025-12-10 10:12:59 +0100

    10: joltage-reqs as vector, button bits are specific to part 1

--- 2025/10.scm
+++ 2025/10.scm
@@ -34,22 +34,30 @@
         (error "invalid string" s))
     (map string->number (string-tokenize s (char-set-complement (char-set #\,)) 1 (- n 1)))))
 
-(define (number-list->schematic numbers)
-  (fold (lambda (k schematic)
-          (bitwise-ior schematic (arithmetic-shift 1 k)))
-        0 numbers))
-
 (define (string->machine-descr s)
   (let ((fields (string-tokenize s)))
     (if (< (length fields) 3)
         (error "invalid string" s))
     (let* ((lights (string->lights-diagram (car fields)))
            (number-lists (reverse (map string->number-list (cdr fields))))
-           (joltage-reqs (car number-lists))
-           (schematics (reverse (map number-list->schematic (cdr number-lists)))))
-      (vector lights schematics joltage-reqs))))
+           (joltage-reqs (list->vector (car number-lists)))
+           (buttons (reverse (cdr number-lists))))
+      (vector lights buttons joltage-reqs))))
 
-(define (num-button-presses goal-lights schematics)
+;; ---------------------------------------------------------------------------
+;; Part 1
+
+;; Button is a list of indices (indices of the toggled lights in part
+;; 1, and indices of joltage levels in part 2). Button bits is bitmask
+;; representation of the list.
+(define (button->button-bits button)
+  (fold (lambda (k bits)
+          (bitwise-ior bits (arithmetic-shift 1 k)))
+        0 button))
+
+;; Buttons in the provided buttons list must be in bitmask
+;; representation.
+(define (num-button-presses goal-lights buttons)
   (let loop ((solution #f)
              (states (ideque #(0 0)))
              (seen (set exact-integer-comparator)))
@@ -68,23 +76,22 @@
            ((= lights goal-lights) (loop presses states (set-adjoin seen lights)))
            ;; Transition to the neigbouring states.
            (else (loop solution
-                       (fold (lambda (buttons states)
+                       (fold (lambda (button states)
                                (ideque-add-back states
-                                                (vector (bitwise-xor lights buttons)
+                                                (vector (bitwise-xor lights button)
                                                         (+ presses 1))))
-                             states schematics)
+                             states buttons)
                        (set-adjoin seen lights))))))))
-
-;; ---------------------------------------------------------------------------
-;; Part 1
 
 (define (part-1)
   (display
    (fold-lines
     (lambda (line sum)
-      (let ((machine (string->machine-descr line)))
-        (+ sum (num-button-presses (vector-ref machine 0)
-                                   (vector-ref machine 1)))))
+      (let* ((machine (string->machine-descr line))
+             (lights (vector-ref machine 0))
+             (buttons (vector-ref machine 1))
+             (buttons (map button->button-bits buttons)))
+        (+ sum (num-button-presses lights buttons))))
     0))
   (newline))
 

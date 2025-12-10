4d57ca0cc58f014dbb563267a6fcbd6b80554d02
Anton Yabchinskiy <arn@intabs.net>
2025-12-10 10:00:15 +0100

    10-1: WIP on num-button-presses function

--- 2025/10.scm
+++ 2025/10.scm
@@ -49,9 +49,31 @@
            (schematics (reverse (map number-list->schematic (cdr number-lists)))))
       (vector lights schematics joltage-reqs))))
 
-(define (num-button-presses lights schematics)
-  ;; TODO
-  0)
+(define (num-button-presses goal-lights schematics)
+  (let loop ((solution #f)
+             (states (ideque #(0 0)))
+             (seen (set exact-integer-comparator)))
+    (if (ideque-empty? states) solution
+        (let* ((state (ideque-front states))
+               (states (ideque-remove-front states))
+               (lights (vector-ref state 0))
+               (presses (vector-ref state 1)))
+          (cond
+           ;; There's a solution and current number of presses is
+           ;; already worse, drop this state.
+           ((and solution (> presses solution)) (loop solution states seen))
+           ;; Have already seen this state, drop.
+           ((set-contains? seen lights) (loop solution states seen))
+           ;; Goal condition met, update the solution and inspect rest of the states.
+           ((= lights goal-lights) (loop presses states (set-adjoin seen lights)))
+           ;; Transition to the neigbouring states.
+           (else (loop solution
+                       (fold (lambda (buttons states)
+                               (ideque-add-back states
+                                                (vector (bitwise-xor lights buttons)
+                                                        (+ presses 1))))
+                             states schematics)
+                       (set-adjoin seen lights))))))))
 
 ;; ---------------------------------------------------------------------------
 ;; Part 1
@@ -61,7 +83,8 @@
    (fold-lines
     (lambda (line sum)
       (let ((machine (string->machine-descr line)))
-        (+ sum (num-button-presses (vector-ref machine 0) (vector-ref machine 1)))))
+        (+ sum (num-button-presses (vector-ref machine 0)
+                                   (vector-ref machine 1)))))
     0))
   (newline))
 

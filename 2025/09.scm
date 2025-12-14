34f5bb9489ca1f2c14ac8368e0d2d85abed244d4
Anton Yabchinskiy <arn@intabs.net>
2025-12-14 12:09:11 +0100

    09-2: WIP, filtering out segments

--- 2025/09.scm
+++ 2025/09.scm
@@ -117,6 +117,69 @@
    (else (loop points))))
 
 (define (part-2)
+  (let* ((points (read-input))
+         (segments (points->segments points))
+         ;; XXX: Find the outliers automatically (e.g., compare with
+         ;; mean segment length)?
+         (p (point 94651 48450))
+         (q (point 94651 50319)))
+
+    ;; Remove segments which have an endpoint below the outlier point
+    ;; Q. They can't form a valid rectange with Q.
+    (set! segments
+          (remove
+           (lambda (s)
+             (or (< (point-ref (interval-lower s) Y)
+                    (point-ref q Y))
+                 (< (point-ref (interval-upper s) Y)
+                    (point-ref q Y))))
+           segments))
+
+    ;; Find horizontal segment which intersects with x=Qx line. Remove
+    ;; segments above this found one. Points of these removed segments
+    ;; couldn't form a valid rectange with Q.
+    (let ((r (segment-begin
+              (find
+               (lambda (s)
+                 (and (segment-horiz? s)
+                      (> (point-ref (segment-begin s) Y) (point-ref q Y))
+                      (interval-member? (point-ref q X)
+                                        (make-interval (point-ref (segment-begin s) X)
+                                                       (point-ref (segment-end s) X)))))
+               segments))))
+      (set! segments
+            (remove
+             (lambda (s)
+               (or (> (point-ref (segment-begin s) Y)
+                      (point-ref r Y))
+                   (> (point-ref (segment-end s) Y)
+                      (point-ref r Y))))
+             segments)))
+
+    ;; Remove segments to the right of Q. Segments to the left are
+    ;; clearly better.
+    (set! segments
+          (remove
+           (lambda (s)
+             (or (> (point-ref (segment-begin s) X)
+                    (point-ref q X))
+                 (> (point-ref (segment-end s) X)
+                    (point-ref q X))))
+           segments))
+
+    ;; Print segments.
+    (for-each
+     (lambda (s)
+       (display (vector-ref (car s) 0))
+       (display ",")
+       (display (vector-ref (car s) 1))
+       (display " ")
+       (display (vector-ref (cdr s) 0))
+       (display ",")
+       (display (vector-ref (cdr s) 1))
+       (display "\n"))
+     segments)
+
   ;; TODO
   )
 

10492133ac86e2deb7f9ad608feceb93f3656940
Anton Yabchinskiy <arn@intabs.net>
2026-01-15 20:22:54 +0100

    18-2: Revert "WIP on solution"
    This reverts commit 4622dc1bfa8862b37635efb9bdd96fcb95d2a660.

--- 2019/18.cpp
+++ 2019/18.cpp
@@ -31,28 +31,16 @@ constexpr keys_set door(char c)
 
 using vault_map = dense_grid<char>;
 
-size_t search(const vault_map& vault, const position_set& start, keys_set all_keys)
+size_t search(const vault_map& vault, position start, keys_set all_keys)
 {
     std::set<std::tuple<position, keys_set>> seen;
     std::set<std::tuple<size_t, position, keys_set>> states;
-#if PART == 2
-    std::map<position, size_t> waiting;
-#endif // PART
 
-    for (const auto q : start) {
-        states.insert({ 0, q, all_keys });
-    }
+    states.insert({ 0, start, all_keys });
 
     while (!states.empty()) {
-        auto [p_steps, p, no_keys] = *states.begin();
+        auto [steps, p, no_keys] = *states.begin();
         states.erase(states.begin());
-
-        std::cerr << __func__ << ":"
-                  << " seen " << seen.size()
-                  << " p_steps " << p_steps
-                  << " p " << p
-                  << " no_keys " << no_keys
-                  << '\n';
 
         if (seen.find({ p, no_keys }) != seen.end()) {
             continue;
@@ -63,36 +51,17 @@ size_t search(const vault_map& vault, const position_set& start, keys_set all_ke
             continue;
         } else if (door(c)) {
             if (!(door(c) & ~no_keys)) {
-#if PART == 2
-                std::cerr << __func__ << ": wait:"
-                          << " c " << c
-                          << " p " << p
-                          << " p_steps " << p_steps
-                          << '\n';
-                waiting.insert({ p, p_steps });
-#endif // PART == 2
                 continue;
             }
         } else if (key(c)) {
             no_keys &= ~key(c);
             if (no_keys == 0) {
-                return p_steps;
+                return steps;
             }
-#if PART == 2
-            for (const auto [q, q_steps] : waiting) {
-                std::cerr << __func__ << ": resume:"
-                          << " c " << c
-                          << " q " << q
-                          << " q_steps " << q_steps
-                          << " no_keys " << no_keys
-                          << '\n';
-                states.insert({ p_steps + 1, q, no_keys });
-            }
-#endif // PART == 2
         }
 
         for (const auto dir : { direction::up, direction::left, direction::right, direction::down }) {
-            states.insert({ p_steps + 1, p + to_offset(dir), no_keys });
+            states.insert({ steps + 1, p + to_offset(dir), no_keys });
         }
     }
 
@@ -116,27 +85,7 @@ int main()
         return c;
     });
 
-    const auto start = find<char>(vault, '@').value();
-
-#if PART == 1
-    std::cout << search(vault, { start }, all_keys) << '\n';
-#elif PART == 2
-    for (int dy = -1; dy <= 1; ++dy) {
-        for (int dx = -1; dx <= 1; ++dx) {
-            set(vault, start + offset { dx, dy }, (dx == 0 || dy == 0) ? '#' : '@');
-        }
-    }
-
-    std::cout << search(
-        vault, {
-                   start + offset { -1, -1 },
-                   start + offset { -1, +1 },
-                   start + offset { +1, -1 },
-                   start + offset { +1, +1 },
-               },
-        all_keys)
-              << '\n';
-#endif // PART
+    std::cout << search(vault, find<char>(vault, '@').value(), all_keys) << '\n';
 
     return 0;
 }

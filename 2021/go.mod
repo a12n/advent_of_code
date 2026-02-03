76593f882ebb8ac108bcc0f3c7877d5256b88a7e
Anton Yabchinskiy <arn@intabs.net>
2026-02-04 00:35:05 +0100

    2021: initial files

---
+++ 2021/go.mod
@@ -0,0 +1,3 @@
+module a12n/advent_of_code/2021
+
+go 1.25.1
---
+++ 2021/run.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path"
+	"runtime/pprof"
+)
+
+func run() error {
+	var debug bool
+	var err error
+	var prof string
+
+	flag.BoolVar(&debug, "debug", debug, "enable debug output")
+	flag.StringVar(&prof, "prof", prof, "write CPU profile to file")
+	flag.Parse()
+
+	if !debug {
+		log.SetOutput(io.Discard)
+	}
+
+	if prof != "" {
+		var f *os.File
+
+		if f, err = os.Create(prof); err != nil {
+			return err
+		}
+		defer f.Close()
+
+		if err = pprof.StartCPUProfile(f); err != nil {
+			return err
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	var day, part int
+
+	if _, err = fmt.Sscanf(path.Base(os.Args[0]), "%02d-%1d", &day, &part); err != nil {
+		return err
+	}
+
+	var dispatch = [25][2](func(*bufio.Reader, io.Writer) error){
+	}
+
+	return dispatch[day-1][part-1](bufio.NewReader(os.Stdin), os.Stdout)
+}
+
+func main() {
+	var err error
+	if err = run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}

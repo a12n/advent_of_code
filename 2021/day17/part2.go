6d8ed6ee1679355840aec9e841ab4a992dff4538
Anton Yabchinskiy <arn@intabs.net>
2026-02-06 21:04:24 +0100

    day17/part2: reorder loops in Part2()

--- 2021/day17/part2.go
+++ 2021/day17/part2.go
@@ -39,10 +39,34 @@ func Part2(r *bufio.Reader, w io.Writer) error {
 	log.Println("vxMin", vxMin, "vxMax", vxMax)
 	log.Println("vyMin", vyMin, "vyMax", vyMax)
 
-	for vx = vxMin; vx <= vxMax; vx++ {
-		for vy = vyMin; vy <= vyMax; vy++ {
-			log.Println("vx", vx, "vy", vy)
-			// TODO
+	for vy = vyMin; vy <= vyMax; vy++ {
+		var tMin, tMax [2]int
+
+		log.Println("vy", vy)
+
+		// The target are is always at negative Y, while the
+		// shooting starts at zero. So, the projectile will first
+		// cross the target.Max[Y] and then later will cross the
+		// target.Min[Y] (both negative).
+		roots, _ = Quadratic(-2*float64(vy), 2*float64(target.Max[Y]))
+		tMin[Y] = int(math.Ceil(roots[1]))
+		log.Println("roots", roots)
+		log.Println("tMin[Y]", tMin[Y])
+
+		roots, _ = Quadratic(-2*float64(vy), 2*float64(target.Min[Y]))
+		tMax[Y] = int(math.Floor(roots[1]))
+		log.Println("roots", roots)
+		log.Println("tMax[Y]", tMax[Y])
+
+		if tMin[Y] > tMax[Y] {
+			log.Println("infeasible with vy", vy)
+			continue
+		}
+
+		for vx = vxMin; vx <= vxMax; vx++ {
+			log.Println("vx", vx)
+
+			// TODO: Where X will be from tMin[Y] to tMax[Y]?
 		}
 	}
 

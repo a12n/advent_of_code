e1fe417f11b4cc0f3d87f6db7f65cd862ef2605e
Anton Yabchinskiy <arn@bestmx.net>
2026-04-05 01:11:52 +0200

    08: rename variables

--- 2017/08.lua
+++ 2017/08.lua
@@ -1,12 +1,11 @@
 local regs = {}
-local prog = {}
 
-local actions = {
+local modifiers = {
    ['inc'] = function(r, n) regs[r] = (regs[r] or 0) + n end,
    ['dec'] = function(r, n) regs[r] = (regs[r] or 0) - n end,
 }
 
-local cops = {
+local conds = {
    ['<'] = function(r, n) return (regs[r] or 0) < n end,
    ['>'] = function(r, n) return (regs[r] or 0) > n end,
    ['<='] = function(r, n) return (regs[r] or 0) <= n end,
@@ -15,39 +14,41 @@ local cops = {
    ['!='] = function(r, n) return (regs[r] or 0) ~= n end,
 }
 
-local highest_value = nil
+local instrs = {}
+local max_during = nil
+
 for line in io.lines() do
-   local reg, op, n, creg, cop, cn = string.match(line, '^(%w+)%s+(%w+)%s+(-?%d+)%s+if%s+(%w+)%s*(%p+)%s*(-?%d+)$')
-   assert(reg and op and n and creg and cop and cn)
+   local r, op, n, cr, cop, cn =
+      string.match(line, '^(%w+)%s+(%w+)%s+(-?%d+)%s+if%s+(%w+)%s*(%p+)%s*(-?%d+)$')
+   assert(r and op and n and cr and cop and cn)
    n = tonumber(n)
    cn = tonumber(cn)
-   print(reg, op, n, creg, cop, cn)
-   local condf = cops[cop]
-   local actionf = actions[op]
-   assert(condf and actionf)
-   table.insert(prog,
-                function()
-                   if condf(creg, cn) then
-                      actionf(reg, n)
-                      if not highest_value or (regs[reg] or 0) > highest_value then
-                         highest_value = (regs[reg] or 0)
-                      end
-                   end
-                end)
+   local cond = conds[cop]
+   local modifier = modifiers[op]
+   assert(cond and modifier)
+   local instr = function()
+      if cond(cr, cn) then
+         modifier(r, n)
+         if not max_during or (regs[r] or 0) > max_during then
+            max_during = regs[r]
+         end
+      end
+   end
+   table.insert(instrs, instr)
 end
 
-for i = 1, #prog do
-   prog[i]()
+for i = 1, #instrs do
+   instrs[i]()
 end
 
 if puzzle.part == 1 then
-   local max = nil
+   local max_after = nil
    for r, n in pairs(regs) do
-      if not max or n > max then
-         max = n
+      if not max_after or n > max_after then
+         max_after = n
       end
    end
-   print(max)
+   print(max_after)
 elseif puzzle.part == 2 then
-   print(highest_value)
+   print(max_during)
 end

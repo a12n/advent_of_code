6f038a2d015b113c2ab1a1e9c64c36cbca4fadd7
Anton Yabchinskiy <arn@bestmx.net>
2026-05-07 23:05:17 +0200

    15: rewrite without coroutines

--- 2017/15.lua
+++ 2017/15.lua
@@ -1,52 +1,45 @@
-local a = tonumber(string.match(io.read(), 'A starts with (%d+)'))
-local b = tonumber(string.match(io.read(), 'B starts with (%d+)'))
-
-function lcg(k, n)
-   return (k * n) % 2147483647
+local function input(name)
+   return tonumber(string.match(io.read(), 'Generator ' .. name .. ' starts with (%d+)'))
 end
 
-function lcga(n)
-   return lcg(16807, n)
+local function lcg(k, x)
+   return (k * x) % 2147483647
 end
 
-function lcgb(n)
-   return lcg(48271, n)
+local function lcga(x)
+   return lcg(16807, x)
 end
 
+local function lcgb(x)
+   return lcg(48271, x)
+end
+
+local function judge(n, a, b)
+   return (a & 0xFFFF) == (b & 0xFFFF) and n + 1 or n
+end
+
+local a = input('A')
+local b = input('B')
 local n = 0
+
 if puzzle.part == 1 then
    for i = 1, 40000000 do
-      if (a & 0xFFFF) == (b & 0xFFFF) then
-         n = n + 1
-      end
+      n = judge(n, a, b)
       a = lcga(a)
       b = lcgb(b)
    end
 elseif puzzle.part == 2 then
-   local coa = coroutine.create(function()
-         while true do
-            if a % 4 == 0 then
-               coroutine.yield(a)
-            end
-            a = lcga(a)
-         end
-   end)
-   local cob = coroutine.create(function()
-         while true do
-            if b % 8 == 0 then
-               coroutine.yield(b)
-            end
-            b = lcgb(b)
-         end
-   end)
    for i = 1, 5000000 do
-      local _, aval = coroutine.resume(coa)
-      local _, bval = coroutine.resume(cob)
-      if (aval & 0xFFFF) == (bval & 0xFFFF) then
-         n = n + 1
+      while a % 4 ~= 0 do
+         a = lcga(a)
       end
+      while b % 8 ~= 0 do
+         b = lcgb(b)
+      end
+      n = judge(n, a, b)
+      a = lcga(a)
+      b = lcgb(b)
    end
-   coroutine.close(coa)
-   coroutine.close(cob)
 end
+
 print(n)

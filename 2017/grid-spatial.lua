f1c0df76e04cb4cbbf443ef91ec65bf2f92dd71f
Anton Yabchinskiy <arn@bestmx.net>
2026-05-07 09:47:01 +0200

    grid-spatial: 3D point and vector

---
+++ 2017/grid-spatial.lua
@@ -0,0 +1,41 @@
+point = {}
+point.__index = point
+
+function point.new(x, y, z)
+   return setmetatable({ x = x or 0,
+                         y = y or x or 0,
+                         z = z or y or x or 0 }, point)
+end
+
+function point.__add(p, u)
+   return point.new(p.x + u.x, p.y + u.y, p.z + u.z)
+end
+
+function point.__tostring(p)
+   return p.x .. ',' .. p.y .. ',' .. p.z
+end
+
+vector = {}
+vector.__index = vector
+
+function vector.new(x, y, z)
+   return setmetatable({ x = x or 0,
+                         y = y or x or 0,
+                         z = z or y or x or 0 }, vector)
+end
+
+function vector.__add(u, v)
+   return vector.new(u.x + v.x, u.y + v.y, u.z + v.z)
+end
+
+function vector.__mul(u, n)
+   return vector.new(u.x * n, u.y * n, u.z * n)
+end
+
+function vector.__tostring(u)
+   return u.x .. ',' .. u.y .. ',' .. u.z
+end
+
+function vector.taxicab(u)
+   return math.abs(u.x) + math.abs(u.y) + math.abs(u.z)
+end

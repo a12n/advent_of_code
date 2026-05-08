03fceef294af946aa11fb7fb56ad5e4b18bd59ab
Anton Yabchinskiy <arn@bestmx.net>
2026-05-08 20:06:15 +0200

    19-1: solution

---
+++ 2017/19.lua
@@ -0,0 +1,50 @@
+require('exts')
+require('grid')
+
+local EMPTY = string.byte(' ')
+local VERT = string.byte('|')
+local HORIZ = string.byte('-')
+local TURN = string.byte('+')
+local LETTERA = string.byte('A')
+local LETTERZ = string.byte('Z')
+
+local diagram = grid.new(io.read('a'))
+local pos = point.new(table.find(diagram[1], VERT), 1)
+local dir = vector.new(0, 1)
+local letters = {}
+
+while true do
+   local item = diagram:get(pos)
+   if not item or item == EMPTY then
+      break
+   elseif item == TURN then
+      -- XXX
+      local newdir, newpos, newitem
+      newdir = dir:ccw()
+      newpos = pos + newdir
+      newitem = diagram:get(newpos)
+      if newitem and newitem ~= EMPTY then
+         dir = newdir
+         pos = newpos
+      else
+         newdir = dir:cw()
+         newpos = pos + newdir
+         newitem = diagram:get(newpos)
+         if newitem and newitem ~= EMPTY then
+            dir = newdir
+            pos = newpos
+         else
+            error('invalid turn')
+         end
+      end
+   elseif item >= LETTERA and item <= LETTERZ then
+      table.insert(letters, item)
+      pos = pos + dir
+   elseif item == VERT or item == HORIZ then
+      pos = pos + dir
+   else
+      error('invalid item')
+   end
+end
+
+print(string.char(table.unpack(letters)))

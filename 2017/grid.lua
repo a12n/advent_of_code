b7f8bbf42da753aa3a938189468f76464acded83
Anton Yabchinskiy <arn@bestmx.net>
2026-05-08 20:04:20 +0200

    grid: add `grid` type, create from string

--- 2017/grid.lua
+++ 2017/grid.lua
@@ -39,3 +39,36 @@ end
 function vector.taxicab(u)
    return math.abs(u.x) + math.abs(u.y)
 end
+
+grid = {}
+grid.__index = grid
+
+function grid.new(linesstr)
+   local g = {}
+   local ncols = nil
+   for line in string.gmatch(linesstr, '[^\n]+') do
+      local row = table.pack(string.byte(line, 1, #line))
+      if not ncols then
+         ncols = #row
+      end
+      if #row ~= ncols then
+         error('row length mismatch')
+      end
+      table.insert(g, row)
+   end
+   return setmetatable(g, grid)
+end
+
+function grid.get(g, p)
+   local row = rawget(g, p.y)
+   return row and rawget(row, p.x) or nil
+end
+
+function grid.set(g, p, v)
+   local row = rawget(g, p.y)
+   if not row then
+      row = {}
+      rawset(g, p.y, row)
+   end
+   rawset(row, p.x, v)
+end

6d85d91b5b9f9afc94cb5a3e71d00cf48d96bf6c
Anton Yabchinskiy <arn@bestmx.net>
2026-05-08 23:46:45 +0200

    run: allow symlink to run tests

--- 2017/run.lua
+++ 2017/run.lua
@@ -1,9 +1,10 @@
 #!/usr/bin/env lua
 
 puzzle = {}
-puzzle.day, puzzle.part = string.match(arg[0], '([0-2][0-9])-([12])$')
+puzzle.day, puzzle.part = string.match(arg[0], '([0-2][0-9])-([12t])$')
 if puzzle.day and puzzle.part then
    puzzle.day = tonumber(puzzle.day)
+   puzzle.test = (puzzle.part == 't')
    puzzle.part = tonumber(puzzle.part)
-   dofile(string.gsub(arg[0], '-[12]$', '.lua'))
+   dofile(string.gsub(arg[0], '-[12t]$', '.lua'))
 end

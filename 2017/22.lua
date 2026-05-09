12e50493e5766c74c7b0d1a36fa0dd6ca7df10d6
Anton Yabchinskiy <arn@bestmx.net>
2026-05-09 17:35:12 +0200

    22: solutions

--- 2017/22.lua
+++ 2017/22.lua
@@ -1,34 +1,60 @@
 require('grid')
 
 local CLEAN = string.byte('.')
+local WEAKENED = string.byte('W')
 local INFECTED = string.byte('#')
+local FLAGGED = string.byte('F')
 
-local cluster = grid.new(io.read('a'))
-
-local pos = point.new(cluster:ncols() // 2 + 1, cluster:nrows() // 2 + 1)
-local dir = vector.new(0, -1)
-local infections = 0
-for burst = 1, 10000 do
+local function unevolved(cluster, pos, dir)
+   local infected = false
    local node = cluster:get(pos) or CLEAN
-   -- print('burst', burst)
-   -- print('pos', pos, 'dir', dir)
-   -- print('node', string.char(node))
    if node == INFECTED then
       dir = dir:cw()
       cluster:set(pos, CLEAN)
-      -- print('dir<-', dir)
-      -- print('node<-', string.char(CLEAN))
    elseif node == CLEAN then
       dir = dir:ccw()
       cluster:set(pos, INFECTED)
-      infections = infections + 1
-      -- print('dir<-', dir)
-      -- print('node<-', string.char(INFECTED))
-      -- print('infections<-', infections)
+      infected = true
    else
       error('invalid node')
    end
-   pos = pos + dir
-   -- print()
+   return pos + dir, dir, infected
+end
+
+local function evolved(cluster, pos, dir)
+   local infected = false
+   local node = cluster:get(pos) or CLEAN
+   if node == CLEAN then
+      dir = dir:ccw()
+      cluster:set(pos, WEAKENED)
+   elseif node == WEAKENED then
+      infected = true
+      cluster:set(pos, INFECTED)
+   elseif node == INFECTED then
+      dir = dir:cw()
+      cluster:set(pos, FLAGGED)
+   elseif node == FLAGGED then
+      dir = dir:cw():cw()
+      cluster:set(pos, CLEAN)
+   else
+      error('invalid node')
+   end
+   return pos + dir, dir, infected
+end
+
+local cluster = grid.new(io.read('a'))
+local pos = point.new(cluster:ncols() // 2 + 1, cluster:nrows() // 2 + 1)
+local dir = vector.new(0, -1)
+local infections = 0
+local virus = (puzzle.part == 1 and unevolved or
+               puzzle.part == 2 and evolved)
+
+for burst = 1, (puzzle.part == 1 and 10000 or
+                puzzle.part == 2 and 10000000) do
+   local infected
+   pos, dir, infected = virus(cluster, pos, dir)
+   if infected then
+      infections = infections + 1
+   end
 end
 print(infections)

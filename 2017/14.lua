967e170b1dd831b15373e19562420002c9861c77
Anton Yabchinskiy <arn@bestmx.net>
2026-05-09 12:45:36 +0200

    14-2: WIP

--- 2017/14.lua
+++ 2017/14.lua
@@ -1,21 +1,53 @@
 require('knothash')
 
-local function popcount(k)
-   local n = 0
-   while k ~= 0 do
-      if (k & 1) ~= 0 then
-         n = n + 1
-      end
-      k = k >> 1
-   end
-   return n
-end
-
 local key = io.read()
-local sum = 0
-for i = 1, 128 do
-   for _, byte in ipairs(densehash(knothash(key .. '-' .. tostring(i - 1)))) do
-      sum = sum + popcount(byte)
+
+if puzzle.part == 1 then
+   local function popcount(k)
+      local n = 0
+      while k ~= 0 do
+         if (k & 1) ~= 0 then
+            n = n + 1
+         end
+         k = k >> 1
+      end
+      return n
+   end
+
+   local sum = 0
+
+   for i = 1, 128 do
+      for _, byte in ipairs(densehash(knothash(key .. '-' .. tostring(i - 1)))) do
+         sum = sum + popcount(byte)
+      end
+   end
+
+   print(sum)
+elseif puzzle.part == 2 then
+   local grid = {}
+
+   for rowi = 1, 128 do
+      local row = {}
+      for coli, byte in ipairs(densehash(knothash(key .. '-' .. tostring(rowi - 1)))) do
+         for biti = 1, 8 do
+            if (byte & (1 << (biti - 1))) ~= 0 then
+               row[(coli - 1) * 8 + (biti - 1) + 1] = true
+            end
+         end
+      end
+      grid[rowi] = row
+   end
+
+   -- TODO
+   for rowi = 1, 128 do
+      for coli = 1, 128 do
+         if grid[rowi][coli] then
+            sum = sum + 1
+            io.stderr:write('#')
+         else
+            io.stderr:write('.')
+         end
+      end
+      io.stderr:write('\n')
    end
 end
-print(sum)

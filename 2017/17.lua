a66c1a741e7a0179d32cbbfaacf1a9c2c86565d7
Anton Yabchinskiy <arn@bestmx.net>
2026-05-09 15:46:49 +0200

    17-2: WIP

--- 2017/17.lua
+++ 2017/17.lua
@@ -1,9 +1,22 @@
 local steps = tonumber(io.read())
-local buffer = { 0 }
 local pos = 1
 
-for i = 1, 2017 do
-   pos = (((pos - 1) + steps) % #buffer) + 1 + 1
-   table.insert(buffer, pos, i)
+if puzzle.part == 1 then
+   local buffer = { 0 }
+   for i = 1, 2017 do
+      pos = (((pos - 1) + steps) % #buffer) + 1 + 1
+      table.insert(buffer, pos, i)
+   end
+   print(buffer[pos + 1])
+elseif puzzle.part == 2 then
+   local first = nil
+   local len = 1
+   for i = 1, 50000000 do
+      pos = (((pos - 1) + steps) % len) + 1 + 1
+      if pos == 2 then
+         first = i
+      end
+      len = len + 1
+   end
+   print(first)
 end
-print(buffer[pos + 1])

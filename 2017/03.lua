8c17b64f666668d870393595b566bf8a40fae780
Anton Yabchinskiy <arn@bestmx.net>
2026-05-12 01:07:20 +0200

    03-1: for loop over two sides of the iteration

--- 2017/03.lua
+++ 2017/03.lua
@@ -7,18 +7,12 @@ local len = 1
 if puzzle.part == 1 then
    local index = tonumber(io.read()) - 1
    while index > 0 do
-      local m
-
-      m = math.min(len, index)
-      pos = pos + dir * m
-      dir = dir:ccw()
-      index = index - m
-
-      m = math.min(len, index)
-      pos = pos + dir * m
-      dir = dir:ccw()
-      index = index - m
-
+      for _ = 1, 2 do
+         local m = math.min(len, index)
+         pos = pos + dir * m
+         dir = dir:ccw()
+         index = index - m
+      end
       len = len + 1
    end
    print(vector.taxicab(pos))

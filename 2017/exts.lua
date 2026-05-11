80451ea462dae3327ae538f4591b3f45e52da81b
Anton Yabchinskiy <arn@bestmx.net>
2026-05-12 01:06:04 +0200

    exts: add table.map() and table.reduce() for sequence tables

--- 2017/exts.lua
+++ 2017/exts.lua
@@ -6,3 +6,18 @@ function table.find(list, item)
    end
    return nil
 end
+
+function table.map(list, f)
+   local ans = {}
+   for _, v in ipairs(list) do
+      table.insert(ans, f(v, k))
+   end
+   return ans
+end
+
+function table.reduce(list, f, acc)
+   for _, v in ipairs(list) do
+      acc = f(acc, v, k)
+   end
+   return acc
+end

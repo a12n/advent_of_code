040fd9acc4cf4c441c39ef6ad9b49e4ebc4eb9cd
Anton Yabchinskiy <arn@bestmx.net>
2026-05-12 00:33:02 +0200

    03-1: puzzle.part == 1 section

--- 2017/03.lua
+++ 2017/03.lua
@@ -1,24 +1,25 @@
 require('grid')
 
-local index = tonumber(io.read()) - 1
 local pos = point.new(0, 0)
 local dir = vector.new(1, 0)
+local len = 1
 
-local n = 1
-while index > 0 do
-   local m
+if puzzle.part == 1 then
+   local index = tonumber(io.read()) - 1
+   while index > 0 do
+      local m
 
-   m = math.min(n, index)
-   pos = pos + dir * m
-   dir = dir:ccw()
-   index = index - m
+      m = math.min(len, index)
+      pos = pos + dir * m
+      dir = dir:ccw()
+      index = index - m
 
-   m = math.min(n, index)
-   pos = pos + dir * m
-   dir = dir:ccw()
-   index = index - m
+      m = math.min(len, index)
+      pos = pos + dir * m
+      dir = dir:ccw()
+      index = index - m
 
-   n = n + 1
+      len = len + 1
+   end
+   print(vector.taxicab(pos))
 end
-
-print(vector.taxicab(pos))

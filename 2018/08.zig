d907bad103fa3fc4bc6cf4b120194b5f892db367
Anton Yabchinskiy <arn@bestmx.net>
2026-05-19 23:01:10 +0200

    08-2: solution

--- 2018/08.zig
+++ 2018/08.zig
@@ -22,13 +22,37 @@ fn firstCheck(reader: *std.Io.Reader) !usize {
     return sum;
 }
 
+fn secondCheck(gpa: std.mem.Allocator, reader: *std.Io.Reader) !usize {
+    const n_children = try takeUnsigned(reader);
+    const n_metadata = try takeUnsigned(reader);
+
+    var sum: usize = 0;
+    var child_sum = try gpa.alloc(usize, n_children);
+    defer gpa.free(child_sum);
+
+    if (n_children != 0) {
+        for (0..n_children) |i| {
+            child_sum[i] = try secondCheck(gpa, reader);
+        }
+        for (0..n_metadata) |_| {
+            const ref = try takeUnsigned(reader) - 1;
+            if (ref < n_children) {
+                sum += child_sum[ref];
+            }
+        }
+    } else {
+        for (0..n_metadata) |_| {
+            sum += try takeUnsigned(reader);
+        }
+    }
+
+    return sum;
+}
+
 pub fn part1(_: std.process.Init, stdin: *std.Io.Reader, stdout: *std.Io.Writer) !void {
     try stdout.print("{d}\n", .{try firstCheck(stdin)});
 }
 
 pub fn part2(init: std.process.Init, stdin: *std.Io.Reader, stdout: *std.Io.Writer) !void {
-    _ = init;
-    _ = stdin;
-    _ = stdout;
-    // TODO
+    try stdout.print("{d}\n", .{try secondCheck(init.gpa, stdin)});
 }

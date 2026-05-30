b31921268c22237323cf4438a160b41aa1278362
Anton Yabchinskiy <arn@bestmx.net>
2026-05-30 17:01:27 +0200

    aoc: add run() to prepare the environment and call the puzzle function

--- 2018/aoc.zig
+++ 2018/aoc.zig
@@ -199,3 +199,30 @@ test "spiral point iterator" {
     try expectEqual(Point{ x - 2, y - 2 }, iter.next().?);
     try expectEqual(Point{ x - 2, y - 1 }, iter.next().?);
 }
+
+pub fn run(
+    init: std.process.Init,
+    func: *const fn (std.process.Init, *std.Io.Reader, *std.Io.Writer, u1) anyerror!void,
+) !void {
+    var args_iter = init.minimal.args.iterate();
+    const stem = std.fs.path.stem(args_iter.next() orelse return error.InvalidExecName);
+    if (stem.len != "00-0".len) {
+        return error.InvalidExecName;
+    }
+
+    const day = std.fmt.parseUnsigned(u8, stem[0..2], 10) catch return error.InvalidPuzzle;
+    const part = std.fmt.parseUnsigned(u8, stem[3..4], 10) catch return error.InvalidPuzzle;
+    if (day < 1 or day > 25 or part < 1 or (day == 25 and part > 1) or part > 2) {
+        return error.InvalidPuzzle;
+    }
+
+    var stdin_buf: [3584]u8 = undefined;
+    var stdin_reader = std.Io.File.stdin().reader(init.io, &stdin_buf);
+    const stdin = &stdin_reader.interface;
+
+    var stdout_buf: [512]u8 = undefined;
+    var stdout_writer = std.Io.File.stdout().writer(init.io, &stdout_buf);
+    const stdout = &stdout_writer.interface;
+
+    try func(init, stdin, stdout, @intCast(part - 1));
+}
